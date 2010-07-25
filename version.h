2b8775bc56a1016198010e50a0329db82144c1cc
Simon <simon@hardwired.org.uk>
2010-07-25 15:03:39 +0100

    Current source - 38.0

---
+++ version.h
@@ -0,0 +1,31 @@
+/**
+ * Powder Toy - Version (Header)
+ *
+ * Copyright (c) 2008 - 2010 Stanislaw Skowronek.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
+ */
+#ifndef VERSION_H
+#define VERSION_H
+
+#define SAVE_VERSION 38
+#define MINOR_VERSION 0
+#define IDENT_VERSION "S" //Change this if you're not Simon! It should be a single letter.
+
+#define MENUV3
+//#define BETA
+#define HEAT_ENABLE
+
+#endif
