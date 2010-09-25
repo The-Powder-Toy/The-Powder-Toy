c7dab50b51f51d8c386123b33d2a726740d03483
Simon <simon@hardwired.org.uk>
2010-09-25 19:11:25 +0100

    Add Anti-Matter

--- powder.c
+++ powder.c
@@ -1260,7 +1260,7 @@ void update_particles_i(pixel *vid, int start, int inc)
                 for(nx=-1; nx<2; nx++)
                     for(ny=-1; ny<2; ny++)
                         if(x+nx>=0 && y+ny>0 &&
-                                x+nx<XRES && y+ny<YRES && (nx || ny))
+						   x+nx<XRES && y+ny<YRES && (nx || ny))
                         {
                             r = pmap[y+ny][x+nx];
                             if((r>>8)>=NPART || !r)
@@ -1288,6 +1288,35 @@ void update_particles_i(pixel *vid, int start, int inc)
                                     parts[i].life = 10;
                                 }
                             }
+                        }
+            }
+            else if(t==PT_AMTR)
+            {
+                for(nx=-1; nx<2; nx++)
+                    for(ny=-1; ny<2; ny++)
+                        if(x+nx>=0 && y+ny>0 &&
+						   x+nx<XRES && y+ny<YRES && (nx || ny))
+                        {
+                            r = pmap[y+ny][x+nx];
+                            if((r>>8)>=NPART || !r)
+                                continue;
+                            rt = parts[r>>8].type;
+							if((r&0xFF)!=PT_AMTR && (r&0xFF)!=PT_DMND && (r&0xFF)!=PT_CLNE && (r&0xFF)!=PT_NONE && (r&0xFF)!=PT_PHOT && (r&0xFF)!=PT_VOID && (r&0xFF)!=PT_BHOL)           
+							{
+								t = parts[i].life++;
+								if(parts[i].life==3)
+								{
+									parts[i].type = PT_NONE;
+									kill_part(i);
+								}
+								parts[r>>8].life = 0;
+								parts[r>>8].type = PT_NONE;
+								kill_part(r>>8);
+								if(2>(rand()/(RAND_MAX/100)))
+									create_part(r>>8, x+nx, y+ny, PT_PHOT);
+								pv[y/CELL][x/CELL] -= 5.0f;
+								continue;
+							}
                         }
             }
 			else if(t==PT_FIRW){
