b501e45705b6819560fc7e40b47a92d0ed92db29
Felix Wallin <nibbler.v1@gmail.com>
2010-10-02 19:47:43 +0200

    supposed fix for photon heat

--- src/powder.c
+++ src/powder.c
@@ -73,7 +73,7 @@ int try_move(int i, int x, int y, int nx, int ny)
     if(!e) {
         if(!legacy_enable) {
             if((r >> 8) < PT_NUM)
-                |                parts[r>>8].temp = restrict_flt(parts[r>>8].temp+parts[i].temp/2, MIN_TEMP, MAX_TEMP);;
+				 parts[i].temp = parts[r>>8].temp = restrict_flt(parts[r>>8].temp+parts[i].temp/2, MIN_TEMP, MAX_TEMP);
         }
         return 0;
     }
