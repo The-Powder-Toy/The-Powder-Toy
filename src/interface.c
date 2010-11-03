53eaaec77af41cecf5180a5e3f7e4fd48fbcdbb2
Simon <simon@hardwired.org.uk>
2010-11-03 18:18:53 +0000

    Another possible fix for the search bug

--- src/interface.c
+++ src/interface.c
@@ -2253,6 +2253,7 @@ int search_ui(pixel *vid_buf)
 
         if((b && !bq && mp!=-1 && !st && !uih) || do_open==1)
         {
+			active = 0;
             if(open_ui(vid_buf, search_ids[mp], search_dates[mp]?search_dates[mp]:NULL)==1) {
                 goto finish;
             }
