761ea03882fd779cc77ac7cb49ff59bbfe94b51e
Cracker64 <cracker642@gmail.com>
2010-12-27 17:29:05 -0500

    added a basic coldflame explosive, ignites from CFLM or something cold.

--- src/powder.c
+++ src/powder.c
@@ -2792,7 +2792,28 @@ void update_particles_i(pixel *vid, int start, int inc)
 					}
 				}				
 			}
-		else if(t==PT_BOMB)
+	    else if(t==PT_C5)
+            {
+                for(nx=-2; nx<3; nx++)
+                    for(ny=-2; ny<3; ny++)
+                        if(x+nx>=0 && y+ny>0 && x+nx<XRES && y+ny<YRES && (nx || ny))
+                        {
+                            r = pmap[y+ny][x+nx];
+                            if((r>>8)>=NPART || !r)
+                                continue;
+                            if((parts[r>>8].temp<100 && parts[r>>8].type!=PT_C5)||parts[r>>8].type==PT_HFLM)
+                            {
+				if(1>rand()%6)
+				{
+					t = parts[i].type = PT_HFLM;
+					parts[r>>8].temp = parts[i].temp = 0;
+					parts[i].life = rand()%150+50;
+					pv[y/CELL][x/CELL] += 1.5;
+				}
+                            }
+                        }
+            }
+	    else if(t==PT_BOMB)
 	    {
 		    int nb;
 			if(parts[i].tmp==1){
