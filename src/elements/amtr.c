f32dd71e0afeeedff4cc7177f8fd16910687399c
AntB <Withheld>
2011-03-27 23:56:28 +0100

    AMTR updates, releases LESS pressure, generates MORE photons and lifes that little bit longer.

--- src/elements/amtr.c
+++ src/elements/amtr.c
@@ -12,16 +12,16 @@ int update_AMTR(UPDATE_FUNC_ARGS) {
 				if ((r&0xFF)!=PT_AMTR && (r&0xFF)!=PT_DMND && (r&0xFF)!=PT_CLNE && (r&0xFF)!=PT_PCLN && (r&0xFF)!=PT_NONE && (r&0xFF)!=PT_PHOT && (r&0xFF)!=PT_VOID && (r&0xFF)!=PT_BHOL && (r&0xFF)!=PT_PRTI && (r&0xFF)!=PT_PRTO)
 				{
 					parts[i].life++;
-					if (parts[i].life==3)
+					if (parts[i].life==4)
 					{
 						kill_part(i);
 						return 1;
 					}
-					if (2>(rand()/(RAND_MAX/100)))
+					if (10>(rand()/(RAND_MAX/100)))
 						create_part(r>>8, x+rx, y+ry, PT_PHOT);
 					else
 						kill_part(r>>8);
-					pv[y/CELL][x/CELL] -= 5.0f;
+					pv[y/CELL][x/CELL] -= 2.0f;
 				}
 			}
 	return 0;
