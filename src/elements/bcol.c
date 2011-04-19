743b7dffce48c6892472de9aa306a4a498b2bceb
Simon <simon@hardwired.org.uk>
2011-04-19 18:09:36 +0100

    bcol Catelite merge

--- src/elements/bcol.c
+++ src/elements/bcol.c
@@ -23,6 +23,13 @@ int update_BCOL(UPDATE_FUNC_ARGS) {
 						parts[i].life = 99;
 					}
 				}
+				if ((r&0xFF)==PT_LAVA && 1>(rand()%500))
+				{
+					if (parts[r>>8].ctype == 76) {
+						parts[r>>8].ctype = 14;
+						kill_part(i);
+					}
+				}
 			}
 	return 0;
 }
