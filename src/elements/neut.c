603844f97f5ba22c1bc0ce6f4e6f51aa9af1ebfb
jacksonmj <mj-pt@jacksonmj.co.uk>
2011-05-17 18:37:13 +0800

    Fix DEUT explosion

--- src/elements/neut.c
+++ src/elements/neut.c
@@ -1,8 +1,11 @@
 #include <element.h>
 
 
+#if defined(WIN32) && !defined(__GNUC__)
+_inline int create_n_parts(int n, int x, int y, float vx, float vy, int t)
+#else
 inline int create_n_parts(int n, int x, int y, float vx, float vy, int t)//testing a new deut create part
-
+#endif
 {
 	int i, c;
 	n = (n/50);
@@ -80,7 +83,7 @@ int update_NEUT(UPDATE_FUNC_ARGS) {
 #ifdef SDEUT
 				else if ((r&0xFF)==PT_DEUT && (pressureFactor+1+(parts[i].life/100))>(rand()%1000))
 				{
-					//create_n_parts(parts[r>>8].life, x+rx, y+ry, parts[i].vx, parts[i].vy, PT_NEUT);
+					create_n_parts(parts[r>>8].life, x+rx, y+ry, parts[i].vx, parts[i].vy, PT_NEUT);
 					kill_part(r>>8);
 				}
 #else
