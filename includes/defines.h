239e55b80281a80e367047a4cb28fcd953b488ba
me4502 <mnmiller1@hotmail.com>
2011-05-17 17:51:12 +1000

    forgot beta...

--- includes/defines.h
+++ includes/defines.h
@@ -12,7 +12,7 @@
 #define SAVE_VERSION 48
 #define MINOR_VERSION 0
 #define IDENT_VERSION "G" //Change this if you're not Simon! It should be a single letter.
-//#define BETA
+#define BETA
 
 //#define SERVER "powdertoy.co.uk"
 #define SERVER "powdertoy.co.uk"
