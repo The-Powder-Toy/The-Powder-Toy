48254635159a4ec109a35a882f1723f8ca3351e6
TheEyes <otherthing6@gmail.com>
2011-08-02 12:28:39 +0100

    Removed unneeded Python stuff

--- src/python/stdlib/json/tests/test_default.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-import json
-
-class TestDefault(TestCase):
-    def test_default(self):
-        self.assertEqual(
-            json.dumps(type, default=repr),
-            json.dumps(repr(type)))
