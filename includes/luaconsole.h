ef671ced128f2389ecddeaa0c1f5ac33cadde716
me4502 <mnmiller1@hotmail.com>
2011-08-19 02:42:36 +1000

    Get zoom via lua part 2

--- includes/luaconsole.h
+++ includes/luaconsole.h
@@ -67,4 +67,5 @@ int luatpt_heat(lua_State* l);
 int luatpt_setfire(lua_State* l);
 int luatpt_setdebug(lua_State* l);
 int luatpt_setfpscap(lua_State* l);
+int luatpt_getzoom(lua_State* l);
 #endif
