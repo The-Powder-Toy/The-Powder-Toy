f35be3db02f1d3115a77d1b921d50c1e19d25af1
Simon Robertshaw <simon@hardwired.org.uk>
2011-09-03 14:46:48 +0100

    Update contributors

--- src/main.c
+++ src/main.c
@@ -153,7 +153,7 @@ static const char *it_msg =
     "\n"
     "Contributors: \bgStanislaw K Skowronek (\brhttp://powder.unaligned.org\bg, \bbirc.unaligned.org #wtf\bg),\n"
     "\bgSimon Robertshaw, Skresanov Savely, cracker64, Catelite, Bryan Hoyle, Nathan Cousins, jacksonmj,\n"
-	"\bgLieuwe Mosch, Anthony Boot\n"
+	"\bgLieuwe Mosch, Anthony Boot, Matthew Miller\n"
     "\n"
     "\bgTo use online features such as saving, you need to register at: \brhttp://powdertoy.co.uk/Register.html"
     ;
