592fe333ea611bb62c76829fd1eb7e4d08cc0f66
Simon Robertshaw <simon@hardwired.org.uk>
2011-09-21 12:38:47 +0100

    Fix missing header changes from cracker's colour tools commit

--- includes/interface.h
+++ includes/interface.h
@@ -255,7 +255,7 @@ void menu_ui(pixel *vid_buf, int i, int *sl, int *sr);
 
 void menu_ui_v3(pixel *vid_buf, int i, int *sl, int *sr, int *dae, int b, int bq, int mx, int my);
 
-int color_menu_ui(pixel *vid_buf, int i, int *cr, int *cg, int *cb, int b, int bq, int mx, int my);
+int color_menu_ui(pixel *vid_buf, int i, int *cr, int *cg, int *cb, int b, int bq, int mx, int my, int * tool);
 
 int sdl_poll(void);
 
