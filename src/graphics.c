05eb16e14c679f92d766ace0b421e793f73faf69
jacksonmj <mj-pt@jacksonmj.co.uk>
2011-10-22 19:27:32 +0800

    Fix LIGH overwriting fire_alpha

--- src/graphics.c
+++ src/graphics.c
@@ -3393,7 +3393,7 @@ void draw_parts(pixel *vid)
                         int rx, ry;
 						for (rx=-1; rx<2; rx++)
                         for (ry=-1; ry<2; ry++)
-                        if (x+rx>=0 && y+ry>0 && x+rx<XRES && y+ry<YRES && (rx || ry))
+                        if (x+rx>=0 && y+ry>=0 && x+rx<XRES/CELL && y+ry<YRES/CELL && (rx || ry))
                         {
                             cr = R*(a-0.3)/6;
                             cg = G*(a-0.3)/6;
