e46ef289e142982d7bd592faa7b0f85470364c01
Simon Robertshaw <simon@hardwired.org.uk>
2011-10-26 14:50:50 +0100

    Remove Python console stuff

--- src/python/stdlib/distutils/debug.py
+++
@@ -1,7 +0,0 @@
-import os
-
-__revision__ = "$Id: debug.py 68943 2009-01-25 22:09:10Z tarek.ziade $"
-
-# If DISTUTILS_DEBUG is anything other than the empty string, we run in
-# debug mode.
-DEBUG = os.environ.get('DISTUTILS_DEBUG')
