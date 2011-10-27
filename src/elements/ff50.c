92e74e31b4c024375ee58d76885677b3b5df9d37
Merbo <carrethan02@gmail.com>
2011-10-27 16:33:54 -0400

    FF50 explosion is now a massive nuke

--- src/elements/ff50.c
+++ src/elements/ff50.c
@@ -35,17 +35,44 @@ int update_FF50(UPDATE_FUNC_ARGS) {
 				parts[i].life = parts[r>>8].life;
 		   }
 		   if (parts[i].life == 1) {
+			   int ra = rand()%2 + 1; 
+			   if (ra == 1) {
+					parts[i].life = 10000;
+					parts[i].temp = 0;
+					parts[i].type = PT_DEUT;
+			   }
+			   else if (ra == 2) {
+					parts[i].life = 10000;
+					parts[i].temp = 9273.15;
+					parts[i].type = PT_NEUT;
+			   }
+			   /*
 			    parts[i].life = 100000;
 				parts[i].temp = 9000 + 273.15f;
 				parts[i].type = PT_PLSM;
+				*/
 				//active = 2;
 		   }
-		   if (parts[r>>8].type == PT_PLSM && parts[i].life <= 30) {
+		   if (parts[r>>8].type == PT_DEUT || parts[r>>8].type == PT_NEUT && parts[i].life <= 30) {
+			   int ra = rand()%2 + 1; 
+			   if (ra == 1) {
+					parts[i].life = 10000;
+					parts[i].temp = 9273.15;
+					parts[i].type = PT_DEUT;
+			   }
+			   else if (ra == 2) {
+					parts[i].life = 10000;
+					parts[i].temp = 9273.15;
+					parts[i].type = PT_NEUT;
+			   }
+			   /*
 			    parts[i].temp = 9000 + 273.15f;
 			    parts[i].life = 100000;
 			    parts[i].type = PT_PLSM;
+				*/
 				//active = 2;
 		   }
+
 
 	   }
 	   return 0;
