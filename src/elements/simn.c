4389139e95823e0ab78079b318948757b00a5751
Merbo <carrethan02@gmail.com>
2011-10-28 18:09:35 -0400

    Added SIMN. "Cornflower Blue" liquid which is slightly affected by ACID, and turns NOOB to DUST.

---
+++ src/elements/simn.c
@@ -0,0 +1,14 @@
+#include <element.h>
+
+int update_SIMN(UPDATE_FUNC_ARGS) {
+	int r, rx, ry;
+	for(rx=-1; rx<2; rx++)
+		for(ry=-1; ry<2; ry++)
+			if(x+rx>=0 && y+ry>0 && x+rx<XRES && y+ry<YRES && pmap[y+ry][x+rx] && (pmap[y+ry][x+rx]&0xFF)!=0xFF) {
+				r = pmap[y+ry][x+rx];
+				if (parts[r>>8].type == PT_NOOB) {
+					parts[r>>8].type = PT_DUST;
+				}
+			}
+	return 0;
+}
