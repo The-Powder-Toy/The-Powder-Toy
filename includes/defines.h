358a7a68332f4c984e00187c51ad4e4bf28a7d9a
chaos.powdertoy.co.uk <admin@powdertoy.co.uk>
2011-12-30 14:59:45 +0000

    Version Increment

--- includes/defines.h
+++ includes/defines.h
@@ -8,9 +8,10 @@
 #endif
  
 //VersionInfoStart
-#define SAVE_VERSION 70
-#define MINOR_VERSION 1
-#define BUILD_NUM 132
+#define SAVE_VERSION 71
+#define MINOR_VERSION 0
+#define BETA
+#define BUILD_NUM 133
 //VersionInfoEnd
 
 #define IDENT_VERSION "G" //Change this if you're not Simon! It should be a single letter
