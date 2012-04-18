c5a8615e892ff0009b10e918fe46421b298bc8e0
Simon Robertshaw <simon@hardwired.org.uk>
2012-04-18 19:06:56 +0100

    TPT: Fix portals being ugly and PRTO effects not being random at first 94b8ed193f

--- src/elements/prti.cpp
+++ src/elements/prti.cpp
@@ -53,8 +53,8 @@ int update_PRTI(UPDATE_FUNC_ARGS) {
 	if (fe) {
 		int orbd[4] = {0, 0, 0, 0};	//Orbital distances
 		int orbl[4] = {0, 0, 0, 0};	//Orbital locations
-		if (!parts[i].life) parts[i].life = rand();
-		if (!parts[i].ctype) parts[i].ctype = rand();
+		if (!sim->parts[i].life) parts[i].life = rand()*rand()*rand();
+		if (!sim->parts[i].ctype) parts[i].ctype = rand()*rand()*rand();
 		sim->orbitalparts_get(parts[i].life, parts[i].ctype, orbd, orbl);
 		for (r = 0; r < 4; r++) {
 			if (orbd[r]>1) {
