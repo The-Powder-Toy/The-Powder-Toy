c112715ea7f32dab8713dba9d31cdcea1ad37b76
Simon Robertshaw <simon@hardwired.org.uk>
2012-05-23 19:31:40 +0100

    Better element positioning in ErrorMessage

--- src/dialogues/ErrorMessage.cpp
+++ src/dialogues/ErrorMessage.cpp
@@ -12,13 +12,13 @@
 ErrorMessage::ErrorMessage(std::string title, std::string message):
 	ui::Window(ui::Point(-1, -1), ui::Point(200, 75))
 {
-	ui::Label * titleLabel = new ui::Label(ui::Point(2, 1), ui::Point(Size.X-4, 16), title);
+	ui::Label * titleLabel = new ui::Label(ui::Point(4, 5), ui::Point(Size.X-8, 16), title);
 	titleLabel->SetTextColour(ui::Colour(200, 100, 50));
 	titleLabel->Appearance.HorizontalAlign = ui::Appearance::AlignLeft;
-	titleLabel->Appearance.VerticalAlign = ui::Appearance::AlignBottom;
+	titleLabel->Appearance.VerticalAlign = ui::Appearance::AlignMiddle;
 	AddComponent(titleLabel);
 
-	ui::Label * messageLabel = new ui::Label(ui::Point(4, 18), ui::Point(Size.X-8, 60), message);
+	ui::Label * messageLabel = new ui::Label(ui::Point(4, 24), ui::Point(Size.X-8, 60), message);
 	messageLabel->Appearance.HorizontalAlign = ui::Appearance::AlignLeft;
 	messageLabel->Appearance.VerticalAlign = ui::Appearance::AlignTop;
 	AddComponent(messageLabel);
@@ -36,8 +36,8 @@ ErrorMessage::ErrorMessage(std::string title, std::string message):
 	};
 
 	ui::Button * okayButton = new ui::Button(ui::Point(0, Size.Y-16), ui::Point(Size.X, 16), "Dismiss");
-	okayButton->Appearance.HorizontalAlign = ui::Appearance::AlignRight;
-	okayButton->Appearance.VerticalAlign = ui::Appearance::AlignBottom;
+	okayButton->Appearance.HorizontalAlign = ui::Appearance::AlignLeft;
+	okayButton->Appearance.VerticalAlign = ui::Appearance::AlignMiddle;
 	okayButton->Appearance.BorderInactive = ui::Colour(200, 200, 200);
 	okayButton->SetActionCallback(new DismissAction(this));
 	AddComponent(okayButton);
