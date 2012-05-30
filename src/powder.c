10e553be0393bbeb7692168d935b2d42507cfdfd
jacksonmj <mj-pt@jacksonmj.co.uk>
2012-05-30 22:05:16 +0100

    Fix PROP_NEUTPENETRATE, particles weren't being moved into empty space

--- src/powder.c
+++ src/powder.c
@@ -384,7 +384,7 @@ int try_move(int i, int x, int y, int nx, int ny)
 		if (parts[i].type==PT_NEUT) {
 			// target material is NEUTPENETRATE, meaning it gets moved around when neutron passes
 			unsigned s = pmap[y][x];
-			if (!(ptypes[s&0xFF].properties&PROP_NEUTPENETRATE))
+			if (s && !(ptypes[s&0xFF].properties&PROP_NEUTPENETRATE))
 				return 1; // if the element currently underneath neutron isn't NEUTPENETRATE, don't move anything except the neutron
 			// if nothing is currently underneath neutron, only move target particle
 			if (s)
