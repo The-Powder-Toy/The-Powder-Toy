29f3500ba3ea82bef6ddbffb90420ffdd2b70fd9
Savely Skresanov <savask@yandex.ru>
2012-06-05 16:03:39 +0700

    Put ifs into switch in create_part. Delete useless PSv saving function.

--- includes/save.h
+++ includes/save.h
@@ -36,8 +36,6 @@ int parse_save_OPS(void *save, int size, int replace, int x0, int y0, unsigned c
 //Old save parsers, readers
 pixel *prerender_save_PSv(void *save, int size, int *width, int *height);
 
-void *build_save_PSv(int *size, int orig_x0, int orig_y0, int orig_w, int orig_h, unsigned char bmap[YRES/CELL][XRES/CELL], float fvx[YRES/CELL][XRES/CELL], float fvy[YRES/CELL][XRES/CELL], sign signs[MAXSIGNS], void* partsptr);
-
 int parse_save_PSv(void *save, int size, int replace, int x0, int y0, unsigned char bmap[YRES/CELL][XRES/CELL], float fvx[YRES/CELL][XRES/CELL], float fvy[YRES/CELL][XRES/CELL], sign signs[MAXSIGNS], void* partsptr, unsigned pmap[YRES][XRES]);
 
 #endif
