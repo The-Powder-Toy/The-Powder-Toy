fe39fdb0e5e91e3c09fe18d6bd1af72db4059ef7
Catelite <liclockwork@yahoo.com>
2012-06-05 16:51:19 -0400

    Oops. +2 files.

---
+++ src/elements/exot.c
@@ -0,0 +1,44 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <element.h>
+
+int update_EXOT(UPDATE_FUNC_ARGS) {
+
+
+	parts[i].tmp--;	
+	if (parts[i].tmp<1) {
+		parts[i].tmp = 250;
+	}
+		
+	return 0;
+}
+int graphics_EXOT(GRAPHICS_FUNC_ARGS)
+{
+	*colr = cpart->tmp / 2 + 0x08;
+	*colg = cpart->tmp * 2 + 0x28;
+	*colb = cpart->tmp / 2 + 0x08;
+	*cola = cpart->tmp / 6;	
+	*firea = *cola;
+	*firer = *colr;
+	*fireg = *colg;
+	*fireb = *colb;
+	*pixel_mode |= FIRE_ADD;
+	*pixel_mode |= PMODE_BLUR;
+
+
+
+	return 0;
+}
