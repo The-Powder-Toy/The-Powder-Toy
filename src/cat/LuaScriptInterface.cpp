3b020b315148e5b065071b5bb8a4caf1321c730c
Simon Robertshaw <simon@hardwired.org.uk>
2012-06-12 19:45:19 +0100

    TPT: fix crash, spark reset set type without checking if it was a valid number

--- src/cat/LuaScriptInterface.cpp
+++ src/cat/LuaScriptInterface.cpp
@@ -1128,8 +1128,13 @@ int luatpt_reset_spark(lua_State* l)
 	{
 		if (luacon_sim->parts[i].type==PT_SPRK)
 		{
-			luacon_sim->parts[i].type = luacon_sim->parts[i].ctype;
-			luacon_sim->parts[i].life = 4;
+			if (luacon_sim->parts[i].ctype >= 0 && luacon_sim->parts[i].ctype < PT_NUM)
+			{
+				luacon_sim->parts[i].type = luacon_sim->parts[i].ctype;
+				luacon_sim->parts[i].life = 0;
+			}
+			else
+				luacon_sim->kill_part(i);
 		}
 	}
 	return 0;
