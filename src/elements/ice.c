3e1d3cd5a6382f1ad7dacee48cfee04188601670
jacksonmj <mj-pt@jacksonmj.co.uk>
2012-07-09 19:20:52 +0100

    Salt should only melt ice that is above saltwater freezing temperature

--- src/elements/ice.c
+++ src/elements/ice.c
@@ -28,7 +28,7 @@ int update_ICEI(UPDATE_FUNC_ARGS) { //currently used for snow as well
 				r = pmap[y+ry][x+rx];
 				if (!r)
 					continue;
-				if (((r&0xFF)==PT_SALT || (r&0xFF)==PT_SLTW) && 1>(rand()%1000))
+				if (((r&0xFF)==PT_SALT || (r&0xFF)==PT_SLTW) && parts[i].temp > ptransitions[PT_SLTW].tlv && 1>(rand()%1000))
 				{
 					part_change_type(i,x,y,PT_SLTW);
 					part_change_type(r>>8,x+rx,y+ry,PT_SLTW);
