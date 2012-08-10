c333e9bb99dfd53d5c2364be4614e84bf50ec3bb
Simon Robertshaw <simon@hardwired.org.uk>
2012-08-10 14:20:00 +0100

    Correct position of cancel button in local save dialogue as per #80

--- src/save/LocalSaveActivity.cpp
+++ src/save/LocalSaveActivity.cpp
@@ -47,7 +47,7 @@ LocalSaveActivity::LocalSaveActivity(SaveFile save) :
 	filenameField->Appearance.HorizontalAlign = ui::Appearance::AlignLeft;
 	AddComponent(filenameField);
 
-	ui::Button * cancelButton = new ui::Button(ui::Point(0, Size.Y-16), ui::Point(Size.X-50, 16), "Cancel");
+	ui::Button * cancelButton = new ui::Button(ui::Point(0, Size.Y-16), ui::Point(Size.X-75, 16), "Cancel");
 	cancelButton->Appearance.HorizontalAlign = ui::Appearance::AlignLeft;
 	cancelButton->Appearance.VerticalAlign = ui::Appearance::AlignMiddle;
 	cancelButton->Appearance.BorderInactive = ui::Colour(200, 200, 200);
