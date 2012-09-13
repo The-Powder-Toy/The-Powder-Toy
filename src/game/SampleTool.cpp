f7f51d5045a25a331b4cec3844f98584934d827e
Simon Robertshaw <simon@hardwired.org.uk>
2012-09-13 16:41:35 +0100

    Make sample tool sample different life types. Fixes #184

--- src/game/SampleTool.cpp
+++ src/game/SampleTool.cpp
@@ -30,16 +30,38 @@ void SampleTool::Draw(Simulation * sim, Brush * brush, ui::Point position)
 	else
 	{
 		int particleType = 0;
+		int particleCtype = 0;
 		if(sim->pmap[position.Y][position.X])
+		{
 			particleType = sim->parts[sim->pmap[position.Y][position.X]>>8].type;
+			particleCtype = sim->parts[sim->pmap[position.Y][position.X]>>8].ctype;
+		}
 		else if(sim->photons[position.Y][position.X])
+		{
 			particleType = sim->parts[sim->photons[position.Y][position.X]>>8].type;
+			particleCtype = sim->parts[sim->pmap[position.Y][position.X]>>8].ctype;
+		}
 
 		if(particleType)
 		{
-			Tool * elementTool = gameModel->GetElementTool(particleType);
-			if(elementTool)
-				gameModel->SetActiveTool(0, elementTool);
+			if(particleType == PT_LIFE)
+			{
+				Menu * lifeMenu = gameModel->GetMenuList()[SC_LIFE];
+				std::vector<Tool*> elementTools = lifeMenu->GetToolList();
+
+				for(std::vector<Tool*>::iterator iter = elementTools.begin(), end = elementTools.end(); iter != end; ++iter)
+				{
+					Tool * elementTool = *iter;
+					if(elementTool && elementTool->GetToolID() == particleCtype)
+						gameModel->SetActiveTool(0, elementTool);
+				}
+			}
+			else
+			{
+				Tool * elementTool = gameModel->GetElementTool(particleType);
+				if(elementTool)
+					gameModel->SetActiveTool(0, elementTool);
+			}
 		}
 	}
 }
