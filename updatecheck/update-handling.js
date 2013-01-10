e3d3b1af290b1fb957390916b513fd3ef6391caa
boxmein <johannes12321@windowslive.com>
2013-01-10 20:06:35 +0200

    Added update handler script for Heroku. Also has a sub-repo

---
+++ updatecheck/update-handling.js
@@ -0,0 +1,16 @@
+/*
+Format: 
+	Receives:
+	Action
+		- CheckVersion (returns changelog) SAFE VALUES FOR BOTH: Unknown
+		- Download (returns update)
+	Sends: 
+	sample changelog:
+	4 20 3  Buildversion MajorVersion MinorVersion
+	List
+	of
+	changes
+	with \n
+	in
+	between
+*/ 
