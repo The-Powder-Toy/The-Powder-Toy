b2663aaad50376ffb6ff8acc125c4fa2ee497ab7
boxmein <johannes12321@windowslive.com>
2013-01-12 13:33:32 +0200

    Added a base node handler. Also commented specification parts.

--- updatecheck/update-handling.js
+++ updatecheck/update-handling.js
@@ -4,6 +4,10 @@ Format:
 	Action
 		- CheckVersion (returns changelog) SAFE VALUES FOR BOTH: Unknown
 		- Download (returns update)
+	Architecture
+		- Contains the build architecture
+	InstructionSet 
+		- Contains the instruction set. :O
 	Sends: 
 	sample changelog:
 	4 20 3  Buildversion MajorVersion MinorVersion
@@ -14,3 +18,17 @@ Format:
 	in
 	between
 */ 
+var headers = {
+	"Content-Type": "text/html",
+	"X-Powered-By": "lolcats",
+
+};
+				// Build, major version, minor version
+var send = 20 + " " + 84 + " " + 3;
+var http = require("http");
+http.createServer(function(request, response) {
+	response.writeHead(200, headers);
+	response.end(JSON.stringify(send));
+
+
+}).listen(80, "localhost");
