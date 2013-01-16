183d2633616439ae6496082708e13846e31c837f
Bryan Hoyle <starfoxprime@gmail.com>
2013-01-16 11:11:25 -0500

    Formatting

--- src/Sound.cpp
+++ src/Sound.cpp
@@ -7,39 +7,39 @@ int note_len[MAX_NOTE];
 int note_smp[MAX_NOTE];
 void add_note(float f)
 {
-  int i;
-  for(i=0;i<MAX_NOTE;i++)
-    {
-      if(!note_len[i])
-        {
-	  note_freq[i]=f;
-	  note_len[i]=22050*3;
-	  note_smp[i]=0;
-	  break;
-        }
-    }
+	int i;
+	for(i=0;i<MAX_NOTE;i++)
+	{
+		if(!note_len[i])
+		{
+			note_freq[i]=f;
+			note_len[i]=22050*3;
+			note_smp[i]=0;
+			break;
+		}
+	}
 }
 void create_tone(void *userdata, Uint8 *stream, int l)
 {
-  int i,j;
-  for(j=0;j<l;j++)
-    {
-      for(i=0;i<MAX_NOTE;i++)
+	int i,j;
+	for(j=0;j<l;j++)
 	{
-	  if(note_len[i])
-	    {
-	      float tmpvar=0;
-	      tmpvar+=sin(PI*note_freq[i]/22050.0f*note_smp[i])*3.0f/8.0f;
-	      tmpvar+=sin(2.0f*PI*note_freq[i]/22050.0f*note_smp[i])*1.0f/8.0f;
-	      tmpvar+=sin(4.0f*PI*note_freq[i]/22050.0f*note_smp[i])*3.0f/8.0f;
-	      tmpvar+=sin(6.0f*PI*note_freq[i]/22050.0f*note_smp[i])*1.0f/16.0f;
-	      tmpvar+=sin(0.5f*PI*note_freq[i]/22050.0f*note_smp[i])*1.0f/16.0f;
-	      *stream+=(tmpvar*16);
-	      note_smp[i]++;
-	      if(note_len[i]==note_smp[i])
-		note_len[i]=0;
-	    }
+		for(i=0;i<MAX_NOTE;i++)
+		{
+			if(note_len[i])
+			{
+				float tmpvar=0;
+				tmpvar+=sin(PI*note_freq[i]/22050.0f*note_smp[i])*3.0f/8.0f;
+				tmpvar+=sin(2.0f*PI*note_freq[i]/22050.0f*note_smp[i])*1.0f/8.0f;
+				tmpvar+=sin(4.0f*PI*note_freq[i]/22050.0f*note_smp[i])*3.0f/8.0f;
+				tmpvar+=sin(6.0f*PI*note_freq[i]/22050.0f*note_smp[i])*1.0f/16.0f;
+				tmpvar+=sin(0.5f*PI*note_freq[i]/22050.0f*note_smp[i])*1.0f/16.0f;
+				*stream+=(tmpvar*16);
+				note_smp[i]++;
+				if(note_len[i]==note_smp[i])
+					note_len[i]=0;
+			}
+		}
+		stream++;
 	}
-      stream++;
-    }
 }
