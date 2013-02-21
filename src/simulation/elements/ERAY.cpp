eddfdb055d91acdd9357fb7030d4065dec186e72
boxmein <johannes12321@windowslive.com>
2013-02-21 20:27:01 +0200

    Finished element ray

--- src/simulation/elements/ERAY.cpp
+++ src/simulation/elements/ERAY.cpp
@@ -3,7 +3,7 @@
 Element_ERAY::Element_ERAY()
 {
     Identifier = "DEFAULT_PT_ERAY";
-    Name = "PRAY";
+    Name = "ERAY";
     Colour = PIXPACK(0xFFF400);
     MenuVisible = 1;
     MenuSection = SC_ELEC;
@@ -31,7 +31,7 @@ Element_ERAY::Element_ERAY()
     Description = "Property Ray. Will edit element values like a ray element would.";
     
     State = ST_SOLID;
-    Properties = TYPE_SOLID|PROP_LIFE_DEC;
+    Properties = TYPE_SOLID;
     
     LowPressure = IPL;
     LowPressureTransition = NT;
@@ -42,7 +42,7 @@ Element_ERAY::Element_ERAY()
     HighTemperature = ITH;
     HighTemperatureTransition = NT;
     
-    Update = &Element_CRAY::update;
+    Update = &Element_ERAY::update;
     
 }
 
@@ -64,16 +64,54 @@ int Element_ERAY::update(UPDATE_FUNC_ARGS)
 						if (!(x+nxi+nxx<XRES && y+nyi+nyy<YRES && x+nxi+nxx >= 0 && y+nyi+nyy >= 0)) {
 							break;
 						}
+						
 						r = pmap[y+nyi+nyy][x+nxi+nxx];
-						if (r) {
-							if (parts[i].ctype == PT_BIZR) {
-								// BIZR sets dcolour
-								parts[i].ctype--;
+						if (!r || (r&0xFF) == PT_BRAY) {
+							int nr = sim->create_part(-1, x+nxi+nxx, y+nyi+nyy, PT_BRAY);
+							parts[nr].dcolour = 0x00022110;
+							parts[nr].life = 5;
+						}
+						else if (r) {
+							switch (parts[i].ctype) {
+								// ctype dcolour flags life pavg temp tmp tmp2 type vx vy x y
+							case PT_CLNE: // CLNE sets ctype.
+								parts[r>>8].ctype = parts[i].tmp;
+								break;
+							case PT_BIZR: // BIZR sets dcolour
+								parts[r>>8].dcolour = parts[i].dcolour;
+								break;
+							// Don't touch flags
+							case PT_LIFE: // LIFE sets ... you've guessed it! life!
+								parts[r>>8].life = parts[i].tmp;
+								break;
+							case PT_FIRE: // FIRE sets temp. 
+								parts[r>>8].temp = parts[i].tmp % MAX_TEMP;
+								break;
+							case PT_ACEL: // ACEL sets tmp
+								parts[r>>8].tmp = parts[i].tmp;
+								break;
+							case PT_DCEL: // DCEL sets tmp2
+								parts[r>>8].tmp2 = parts[i].tmp;
+								break;
+							case PT_PSCN: // PSCN sets vx
+								parts[r>>8].vx = parts[i].tmp;
+								break;
+							case PT_NSCN: // NSCN sets vy
+								parts[r>>8].vy = parts[i].tmp;
+								break;
+							case PT_CONV: // CONV sets type. 
+								parts[r>>8].type = parts[i].tmp % PT_NUM;
+								break;
+							default: // Anything else destroys the particle underneath.
+								sim->kill_part(r>>8);
+								break;
 							}
+							docontinue = 0;
 						}
 					}
 				}
 			}
+	return 0;
 }
 
-Element_CRAY::~Element_CRAY() {}
+Element_ERAY::~Element_ERAY() {}
