b617b8b6565f7c87e2bd1e984f9a17d49743634c
jacob1 <jfu614@gmail.com>
2013-03-16 14:00:11 -0400

    fix compiling error

--- src/client/requestbroker/APIRequest.h
+++ src/client/requestbroker/APIRequest.h
@@ -7,7 +7,7 @@ public:
 	APIResultParser * Parser;
 	std::string URL;
 	void * HTTPContext;
-	APIRequest(std::string url, APIResultParser * parser, ListenerHandle listener = ListenerHandle(0, 0));
+	APIRequest(std::string url, APIResultParser * parser, ListenerHandle listener = ListenerHandle(0, (RequestListener*)NULL));
 	virtual RequestBroker::ProcessResponse Process(RequestBroker & rb);
 	virtual ~APIRequest();
 	virtual void Cleanup();
