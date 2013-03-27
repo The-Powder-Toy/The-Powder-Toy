8bdb053c3cea8d06a00a1e1ea4ead3cdea2f22fd
mniip <mniip@yandex.ru>
2013-03-27 19:46:26 +0400

    finally, somewhat nice sound

--- src/Sound.cpp
+++ src/Sound.cpp
@@ -19,7 +19,8 @@ void add_note(float f, int l)
 		}
 	}
 }
-#define NOTE(F,L,S) (sin(2.0f*PI*(F)*(S)/44100.0f)*(pow(1.0f-((float)(S))/(L),2)))
+#define SINE(F,S) (sinf(2.0f*PI*(F)*(S)/44100.0f))
+#define DECAY(F,S,L) (SINE(F,S)*(1.0f-(S)/(float)(L)))
 void create_tone(void *userdata, Uint8 *stream, int l)
 {
 	int i,k;
@@ -32,10 +33,11 @@ void create_tone(void *userdata, Uint8 *stream, int l)
 			if(note_len[i])
 			{
 				a++;
-				float n=0.0f;
-				n=NOTE(note_freq[i],note_len[i],note_smp[i])*1024.0f;
-				n+=NOTE(note_freq[i]*1.5225f,note_len[i],note_smp[i])*288.0f;
-				smp+=n;
+				smp+=DECAY(note_freq[i],note_smp[i],note_len[i])*1024.0f;
+				smp+=DECAY(note_freq[i]*2.0f,note_smp[i],note_len[i])*512.0f;
+				smp+=DECAY(note_freq[i]*3.0f,note_smp[i],note_len[i])*341.3f;
+				smp+=DECAY(note_freq[i]*4.0f,note_smp[i],note_len[i])*256.0f;
+				smp+=DECAY(note_freq[i]*5.0f,note_smp[i],note_len[i])*204.0f;
 				note_smp[i]++;
 				if(note_len[i]==note_smp[i])
 					note_len[i]=0;
@@ -48,11 +50,7 @@ void create_tone(void *userdata, Uint8 *stream, int l)
 		}
 		smp=sqrt(smp);
 		if(smp>127.0f)
-		{
-			printf("overrun\n");
-			return;
 			*stream=127;
-		}
 		else if(smp<-128.0f)
 			*stream=-128;
 		else
