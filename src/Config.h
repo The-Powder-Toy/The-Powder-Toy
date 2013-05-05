fea71ec69b48e6756a6b5fde58ae98ee4348b61f
Simon Robertshaw <simon@hardwired.org.uk>
2013-05-05 14:33:08 +0100

    Update version number

--- src/Config.h
+++ src/Config.h
@@ -16,11 +16,11 @@
 #endif
 
 #ifndef MINOR_VERSION
-#define MINOR_VERSION 0
+#define MINOR_VERSION 1
 #endif
 
 #ifndef BUILD_NUM
-#define BUILD_NUM 267
+#define BUILD_NUM 270
 #endif
 
 #ifndef SNAPSHOT_ID
