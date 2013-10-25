aa09c0e7536214f20e978fd4136c418732ae38d9
Simon Robertshaw <simon@hardwired.org.uk>
2013-10-25 20:40:45 +0100

    Reject {b:1|blah} signs

--- src/Misc.cpp
+++ src/Misc.cpp
@@ -628,14 +628,16 @@ int splitsign(const char* str, char * type)
 	if (str[0]=='{' && (str[1]=='c' || str[1]=='t' || str[1]=='b'))
 	{
 		const char* p=str+2;
-		if(str[2]==':' && str[3]>='0' && str[3]<='9')
-		{
-			p=str+4;
-			while (*p>='0' && *p<='9')
-				p++;
+		if(str[1] != 'b') {
+			if(str[2]==':' && str[3]>='0' && str[3]<='9')
+			{
+				p=str+4;
+				while (*p>='0' && *p<='9')
+					p++;
+			}
+			else
+				return 0;
 		}
-		else if(str[1] != 'b')
-			return 0;
 
 		if (*p=='|')
 		{
