418ac14f003a1eccbff19f71238994bfecc82526
jBot42 <Rick@MacBook-Pro.local>
2013-11-01 18:28:05 -0700

    Made Proton Lasers Quite a Bit Easier

--- src/simulation/Simulation.cpp
+++ src/simulation/Simulation.cpp
@@ -2216,13 +2216,13 @@ int Simulation::try_move(int i, int x, int y, int nx, int ny)
 				part_change_type(i, x, y, PT_ELEC);
 				parts[i].ctype = 0;
 			}
-			else if ((r&0xFF) == PT_H2 && pv[y/CELL][x/CELL] < 45.0f && parts[i].temp < 3000)
+			else if ((r&0xFF) == PT_VRSS && pv[y/CELL][x/CELL] < 45.0f && parts[i].temp < 3000)
 			{
 				part_change_type(i, x, y, PT_PROT);
 				parts[i].ctype = 0;
 				parts[i].tmp2 = 0x1;
 
-				create_part(r>>8, x, y, PT_ELEC);
+				//create_part(r>>8, x, y, PT_ELEC);
 				return 1;
 			}
 		}
