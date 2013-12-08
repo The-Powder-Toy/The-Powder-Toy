4f81cf55f39b04be3e3388634986cf0e5b60813f
jacob1 <jfu614@gmail.com>
2013-12-08 14:11:45 -0500

    Update version number (89.0.274 -> 89.1.278)

--- src/Config.h
+++ src/Config.h
@@ -16,11 +16,11 @@
 #endif
 
 #ifndef MINOR_VERSION
-#define MINOR_VERSION 0
+#define MINOR_VERSION 1
 #endif
 
 #ifndef BUILD_NUM
-#define BUILD_NUM 274
+#define BUILD_NUM 278
 #endif
 
 #ifndef SNAPSHOT_ID
