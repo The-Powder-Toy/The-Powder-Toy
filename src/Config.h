b889c62bee87f808635610f10861e40f8f9d9888
jacob1 <jfu614@gmail.com>
2014-08-03 23:21:35 -0400

    Update version number -> 90.0.316

--- src/Config.h
+++ src/Config.h
@@ -12,15 +12,15 @@
 
 //VersionInfoStart
 #ifndef SAVE_VERSION
-#define SAVE_VERSION 89
+#define SAVE_VERSION 90
 #endif
 
 #ifndef MINOR_VERSION
-#define MINOR_VERSION 2
+#define MINOR_VERSION 0
 #endif
 
 #ifndef BUILD_NUM
-#define BUILD_NUM 283
+#define BUILD_NUM 316
 #endif
 
 #ifndef SNAPSHOT_ID
