859fdece6a8438f1817b38ab133b5b718fc3afe0
jacob1 <jfu614@gmail.com>
2014-08-06 11:49:57 -0400

    prevent PROT from rapidly changing portal temps

--- src/simulation/elements/PROT.cpp
+++ src/simulation/elements/PROT.cpp
@@ -92,7 +92,7 @@ int Element_PROT::update(UPDATE_FUNC_ARGS)
 	if (under)
 	{
 		//now changed so that PROT goes through portal, so only the WIFI part applies
-		if ((under&0xFF) == PT_WIFI || (under&0xFF) == PT_PRTI || (under&0xFF) == PT_PRTO)
+		if ((under&0xFF) == PT_WIFI/* || (under&0xFF) == PT_PRTI || (under&0xFF) == PT_PRTO*/)
 		{
 			float change;
 			if (parts[i].temp<173.15f) change = -1000.0f;
