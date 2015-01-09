5e868995659a3b06aa63b972f897196a62d5adff
39bit <cosmoconsole@gmail.com>
2015-01-10 00:47:03 +0200

    new reactions: direction with INST, NEUT->PROT, AMTR, cold fusion

--- src/simulation/elements/NTRI.cpp
+++ src/simulation/elements/NTRI.cpp
@@ -50,6 +50,8 @@ Element_NTRI::Element_NTRI()
 int Element_NTRI::update(UPDATE_FUNC_ARGS)
 {
 	int under = pmap[y][x];
+    if (!under)
+        under = sim->photons[y][x];
 	if ((under&0xFF) == PT_DEUT)
 	{
 		if ((-((int)sim->pv[y/CELL][x/CELL]-4)+(parts[under>>8].life/100)) > rand()%200)
@@ -61,17 +63,12 @@ int Element_NTRI::update(UPDATE_FUNC_ARGS)
     if (under)
 	{
 		if ((under&0xFF) == PT_HSWC)
-		{
             parts[i].temp = parts[under>>8].temp;
-		}
 		else if ((under&0xFF) == PT_FILT)
-        {
             parts[under>>8].temp = parts[i].temp;
-        }
 		else
-		{
-			if (sim->elements[(under&0xFF)].HeatConduct) parts[i].temp = restrict_flt(parts[i].temp+(parts[under>>8].temp-parts[i].temp)/16.0f, MIN_TEMP, MAX_TEMP);
-		}
+			if (sim->elements[(under&0xFF)].HeatConduct && ((under&0xFF) != PT_H2)) 
+                parts[i].temp = restrict_flt(parts[i].temp+(parts[under>>8].temp-parts[i].temp)/16.0f, MIN_TEMP, MAX_TEMP);
 	}
 	else if (parts[i].life)
 	{
@@ -89,42 +86,71 @@ int Element_NTRI::update(UPDATE_FUNC_ARGS)
         sim->pv[y/CELL][x/CELL] -= 1.0f * CFDS;
         break;
     case PT_DUST:
-        if (3>(rand()%200))
+        if (3>(rand()%20))
             sim->part_change_type(under>>8,x,y,PT_GUNP);
         break;
     case PT_YEST:
-        if (3>(rand()%200))
+        if (3>(rand()%20))
             sim->part_change_type(under>>8,x,y,PT_DYST);
+        break;
+    case PT_NEUT:
+        if (3>(rand()%5))
+            sim->part_change_type(under>>8,x,y,PT_PROT);
+        break;
+    case PT_H2:
+        if ((int(parts[i].temp)>>6)>(rand()%10000)) {
+            // well here we go
+            parts[under>>8].temp = 9999;
+            sim->pv[y/CELL][x/CELL] += 300.0F;
+            sim->gravmap[(y/CELL)*(XRES/CELL)+(x/CELL)] -= 30.0F;
+            sim->kill_part(i);
+        }
         break;
     case PT_DYST:
         sim->part_change_type(under>>8,x,y,PT_YEST);
         break;
     case PT_GOO:
-        if (3>(rand()%200))
+        if (3>(rand()%20))
             sim->part_change_type(under>>8,x,y,PT_PLEX);
         break;
     case PT_DESL:
-        if (3>(rand()%200))
+        if (3>(rand()%20))
             sim->part_change_type(under>>8,x,y,PT_NITR);
         break;
     case PT_GAS:
-        if (3>(rand()%200))
+        if (3>(rand()%20))
             sim->part_change_type(under>>8,x,y,rand()%2 ? PT_DESL : PT_OIL);
         break;
     case PT_WOOD:
-        if (3>(rand()%200))
+        if (3>(rand()%20))
             sim->part_change_type(under>>8,x,y,rand()%2 ? PT_COAL : PT_PLNT);
         if (parts[under>>8].type == PT_COAL) {
             parts[under>>8].tmp = 50;
             parts[under>>8].life = 110;
         }
         break;
+    case PT_INST: 
+        {
+            if (parts[under>>8].tmp == 1) {
+                if (abs(parts[i].vy) > abs(parts[i].vx))
+                    parts[i].vx = 0;
+                else
+                    parts[i].vy = 0;
+            } else {
+                if (!(parts[i].vx == 0 || parts[i].vy == 0)) {
+                    float wr = abs(parts[i].vx / parts[i].vy);
+                    if (wr >= 2.0) parts[i].vy = 0;
+                    if (wr <= 0.5) parts[i].vx = 0;
+                }
+            }
+        }
+        break;
     case PT_FWRK:
-        if (!(rand()%20))
+        if (!(rand()%5))
             sim->part_change_type(under>>8, x, y, PT_DUST);
         break;
     case PT_ISOZ:
-        if (!(rand()%20))
+        if (!(rand()%5))
             sim->create_part(under>>8, x, y, PT_ACID);
         break;
     default:
@@ -142,6 +168,18 @@ int Element_NTRI::DeutImplosion(Simulation * sim, int n, int x, int y, float tem
 		n = 1;
 	else if (n>340)
 		n = 340;
+    int z = 1;
+    
+    int r, rx, ry;
+	for (rx=-1; rx<2; rx++)
+		for (ry=-1; ry<2; ry++)
+			if (BOUNDS_CHECK)
+			{
+				r = sim->photons[y+ry][x+rx];
+                if ((r&0xFF)==PT_PROT) {
+                    z = -1;
+                }
+            }
 
 	for (int c=0; c<n; c++)
 	{
@@ -150,9 +188,9 @@ int Element_NTRI::DeutImplosion(Simulation * sim, int n, int x, int y, float tem
 			sim->parts[i].temp = temp;
 
 		if (sim->grav->ngrav_enable)
-            sim->gravmap[(y/CELL)*(XRES/CELL)+(x/CELL)] = restrict_flt(sim->gravmap[(y/CELL)*(XRES/CELL)+(x/CELL)] + (4.0f * CFDS),-120,120);
+            sim->gravmap[(y/CELL)*(XRES/CELL)+(x/CELL)] = restrict_flt(sim->gravmap[(y/CELL)*(XRES/CELL)+(x/CELL)] + (z * 4.0f * CFDS),-120,120);
         else
-            sim->pv[y/CELL][x/CELL] += 5.0f * CFDS;
+            sim->pv[y/CELL][x/CELL] += 5.0f * z * CFDS;
 	}
 	return 0;
 }
