01bdcff628503b4b7a49a42cf3a63850e8322a75
BuildTools <unconfigured@null.spigotmc.org>
2016-03-03 13:51:33 +0200

    Added Elements

---
+++ src/simulation/elements/COPR.cpp
@@ -0,0 +1,47 @@
+#include "simulation/Elements.h"
+//#TPT-Directive ElementClass Element_COPR PT_COPR 514
+Element_COPR::Element_COPR()
+{
+	Identifier = "DEFAULT_PT_COPR";
+	Name = "COPR";
+	Colour = PIXPACK(0x404060);
+	MenuVisible = 1;
+	MenuSection = SC_ELEC;
+	Enabled = 1;
+
+	Advection = 0.0f;
+	AirDrag = 0.00f * CFDS;
+	AirLoss = 0.90f;
+	Loss = 0.00f;
+	Collision = 0.0f;
+	Gravity = 0.0f;
+	Diffusion = 0.00f;
+	HotAir = 0.000f	* CFDS;
+	Falldown = 0;
+
+	Flammable = 0;
+	Explosive = 0;
+	Meltable = 1;
+	Hardness = 1;
+
+	Weight = 100;
+
+	Temperature = R_TEMP+0.0f	+273.15f;
+	HeatConduct = 500;
+	Description = "Copper. Very good heat conduit";
+
+	Properties = TYPE_SOLID|PROP_CONDUCTS|PROP_LIFE_DEC|PROP_HOT_GLOW;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = IPH;
+	HighPressureTransition = NT;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = 1273.0f;
+	HighTemperatureTransition = PT_LAVA;
+
+	Update = NULL;
+}
+
+Element_COPR::~Element_COPR() {}
