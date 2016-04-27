1c15b426d5f308ae1e0ffa515f0211c4eb3b784f
jacob1 <jfu614@gmail.com>
2016-04-26 23:15:33 -0400

    Fix ctrl+MIX not actually doing anything, and shift+MIX being too powerful

--- src/simulation/simtools/Mix.cpp
+++ src/simulation/simtools/Mix.cpp
@@ -17,7 +17,7 @@ int Tool_Mix::Perform(Simulation * sim, Particle * cpart, int x, int y, float st
 	if(rand() % 100 != 0)
 		return 0;
 
-	int distance = strength * 10;
+	int distance = (int)(std::pow(strength, .5) * 10);
 
 	if(!(sim->elements[thisPart&0xFF].Properties & TYPE_PART))
 		return 0;
@@ -42,6 +42,8 @@ int Tool_Mix::Perform(Simulation * sim, Particle * cpart, int x, int y, float st
 	sim->pmap[newY][newX] = thisPart;
 	sim->parts[thisPart>>8].x = newX;
 	sim->parts[thisPart>>8].y = newY;
+
+	return 1;
 }
 
 Tool_Mix::~Tool_Mix() {}
