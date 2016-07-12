5b525852b49f0e4d339f8e1122b52182dee19169
Simon Robertshaw <simon@hardwired.org.uk>
2016-07-12 22:34:31 +0100

    More OS X compatibility fixes with OpenGL

--- src/graphics/OpenGLHeaders.h
+++ src/graphics/OpenGLHeaders.h
@@ -2,6 +2,9 @@
 
 #if __MAC_OS_X_VERSION_MAX_ALLOWED >= __MAC_OS_X_VERSION_10_9
 #include <OpenGL/glu.h>
+#ifndef GL_RGBA32F
+#define GL_RGBA32F GL_RGBA32F_ARB
+#endif
 #elif __MAC_OS_X_VERSION_MAX_ALLOWED >= 1070
 #include <OpenGL/gl3.h>
 #include <OpenGL/glu.h>
