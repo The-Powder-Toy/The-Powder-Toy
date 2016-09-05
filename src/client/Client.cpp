b405674d3d7d9c382cafa06c4e938f20b7ae18c5
jacob1 <jfu614@gmail.com>
2016-09-04 20:17:35 -0400

    minor updates to .desktop files

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -384,7 +384,7 @@ bool Client::DoInstallation()
 "Comment=Physics sandbox game\n"
 "MimeType=x-scheme-handler/ptsave;\n"
 "NoDisplay=true\n"
-"Categories=Game\n";
+"Categories=Game;Simulation\n";
 	std::stringstream protocolfiledata;
 	protocolfiledata << protocolfiledata_tmp << "Exec=" << filename <<" ptsave %u\nPath=" << pathname << "\n";
 	f = fopen("powdertoy-tpt-ptsave.desktop", "wb");
@@ -401,7 +401,7 @@ bool Client::DoInstallation()
 "Comment=Physics sandbox game\n"
 "MimeType=application/vnd.powdertoy.save;\n"
 "NoDisplay=true\n"
-"Categories=Game\n";
+"Categories=Game;Simulation\n";
 	std::stringstream desktopfiledata;
 	desktopfiledata << desktopfiledata_tmp << "Exec=" << filename <<" open %f\nPath=" << pathname << "\n";
 	f = fopen("powdertoy-tpt.desktop", "wb");
