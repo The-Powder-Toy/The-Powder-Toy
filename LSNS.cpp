a00bb6d3fdd145cf6c8a1df0135065bc0d9677d6
nope <1337h4x0rname@gmail.com>
2016-11-21 17:21:50 -0500

    le LSNS
    "Updated."

--- LSNS.cpp
+++ LSNS.cpp
@@ -42,33 +42,46 @@
  
  	Update = &Element_LSNS::update;
  }
- int Element_LSNS::update(UPDATE_FUNC_ARGS)
-   {
-   int sensorLife=parts[i].temp;
-   if(sensorLife<0)
-   {
-       sensorLife=0;
-   }
- 	int r,rx,ry,rt=parts[i].tmp2;
- 	for (rx=-1; rx<2; rx++)
- 		for (ry=-1; ry<2; ry++)
- 			if (BOUNDS_CHECK && (rx || ry))
- 			{
- 				r = pmap[y+ry][x+rx];
- 				if (!r)
- 					continue;
-                        rt=(r&0xFF);
- 			if ((parts[r>>8].life) > sensorLife)
- 			{
-                            if(sim->elements[rt].Properties&PROP_CONDUCTS)
-                            {
- 				    sim->part_change_type(r>>8,x+rx,y+ry,PT_SPRK);
-                            }
- 			};
- 			};
- 	};
-         return 0;
- }
- 
- 
- Element_LSNS::~Element_LSNS() {}
+int Element_LSNS::update(UPDATE_FUNC_ARGS)
+{
+	int r, rx, ry, rt, rd = parts[i].tmp2;
+	if (rd > 25) parts[i].tmp2 = rd = 25;
+	if (parts[i].life)
+	{
+		parts[i].life = 0;
+		for (rx=-2; rx<3; rx++)
+			for (ry=-2; ry<3; ry++)
+				if (BOUNDS_CHECK && (rx || ry))
+				{
+					r = pmap[y+ry][x+rx];
+					if (!r)
+						continue;
+					rt = r&0xFF;
+					if (sim->parts_avg(i,r>>8,PT_INSL) != PT_INSL)
+					{
+						if ((sim->elements[rt].Properties&PROP_CONDUCTS) && !(rt==PT_WATR||rt==PT_SLTW||rt==PT_NTCT||rt==PT_PTCT||rt==PT_INWR) && parts[r>>8].life==0)
+						{
+							parts[r>>8].life = 4;
+							parts[r>>8].ctype = rt;
+							sim->part_change_type(r>>8,x+rx,y+ry,PT_SPRK);
+						}
+					}
+				}
+	}
+	for (rx=-rd; rx<rd+1; rx++)
+		for (ry=-rd; ry<rd+1; ry++)
+			if (x+rx>=0 && y+ry>=0 && x+rx<XRES && y+ry<YRES && (rx || ry))
+			{
+				r = pmap[y+ry][x+rx];
+				if(!r)
+					r = sim->photons[y+ry][x+rx];
+				if(!r)
+					continue;
+				if ((r&0xFF)!=PT_LSNS && (r&0xFF)!=PT_METL && parts[r>>8].temp > parts[i].temp)
+					parts[i].life = 1;
+			}
+	return 0;
+}
+
+
+Element_LSNS::~Element_LSNS() {}
