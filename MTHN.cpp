0aefabe0a02b1bc4b23b474dd10cee9f6e085cde
nope <1337h4x0rname@gmail.com>
2016-11-25 22:17:06 -0500

    Methane.
    Methane. Burns in contact with OXYG. Suggested in id:762472

---
+++ MTHN.cpp
@@ -0,0 +1,64 @@
+#include "simulation/Elements.h"
+Element_MTHN::Element_MTHN()
+{
+	Identifier = "DEFAULT_PT_MTHN";
+	Name = "MTHN";
+	Colour = PIXPACK(0x003399);
+	MenuVisible = 1;
+	MenuSection = SC_GAS;
+	Enabled = 1;
+
+	Advection = 1.0f;
+	AirDrag = 0.01f * CFDS;
+	AirLoss = 0.99f;
+	Loss = 0.30f;
+	Collision = -0.1f;
+	Gravity = 0.0f;
+	Diffusion = 0.75f;
+	HotAir = 0.001f	* CFDS;
+	Falldown = 0;
+
+	Flammable = 0;
+	Explosive = 0;
+	Meltable = 0;
+	Hardness = 1;
+
+	Weight = 1;
+
+	Temperature = R_TEMP+2.0f	+273.15f;
+	HeatConduct = 106;
+	Description = "Methane. Diffuses quickly. Burns in contact with OXYG"
+
+	Properties = TYPE_GAS|PROP_CONDUCTS|PROP_LIFE_DEC;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = IPH;
+	HighPressureTransition = NT;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = 500.0f;
+	HighTemperatureTransition = PT_FIRE;
+
+	Update = &Element_MTHN::update;
+}
+
+int Element_MTHN::update(UPDATE_FUNC_ARGS)
+{
+ int r,rx,ry;
+	for (rx=-2; rx<3; rx++)
+		for (ry=-2; ry<3; ry++)
+			if (BOUNDS_CHECK && (rx || ry))
+			{
+				r = pmap[y+ry][x+rx];
+				if (!r)
+					continue;
+
+				if((r&0xFF)==PT_OXYG)
+				{
+				 sim->part_change_type(i,x,y,PT_FIRE);
+				}
+			}
+}
+
+Element_MTHN::~Element_MTHN() {}
