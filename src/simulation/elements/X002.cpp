9fd4acfdd014e00c439d55e480093fd6775687ec
My main account <sf2164229838@sina.com>
2017-02-15 18:47:44 -0600

    Add files via upload

---
+++ src/simulation/elements/X002.cpp
@@ -0,0 +1,81 @@
+#include "simulation/Elements.h"
+//#TPT-Directive ElementClass Element_X002 PT_X002 186
+Element_X002::Element_X002()
+{
+	Identifier = "DEFAULT_PT_X002";
+	Name = "X002";
+	Colour = PIXPACK(0xDFEFFF);
+	MenuVisible = 0;
+	MenuSection = SC_NUCLEAR;
+#if defined(DEBUG) || defined(SNAPSHOT)
+	Enabled = 1;
+#else
+	Enabled = 0;
+#endif
+
+	Advection = 0.0f;
+	AirDrag = 0.00f * CFDS;
+	AirLoss = 1.00f;
+	Loss = 1.00f;
+	Collision = -0.99f;
+	Gravity = 0.0f;
+	Diffusion = 0.00f;
+	HotAir = 0.000f	* CFDS;
+	Falldown = 0;
+
+	Flammable = 0;
+	Explosive = 0;
+	Meltable = 0;
+	Hardness = 0;
+
+	Weight = -1;
+
+	Temperature = R_TEMP+200.0f+273.15f;
+	HeatConduct = 251;
+	Description = "X002trons. Sparks X002tronics, reacts with NEUT and WATR.";
+
+	Properties = TYPE_ENERGY|PROP_LIFE_DEC|PROP_LIFE_KILL_DEC;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = IPH;
+	HighPressureTransition = NT;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = ITH;
+	HighTemperatureTransition = NT;
+
+	Update = &Element_X002::update;
+	Graphics = &Element_X002::graphics;
+}
+
+//#TPT-Directive ElementHeader Element_X002 static int update(UPDATE_FUNC_ARGS)
+int Element_X002::update(UPDATE_FUNC_ARGS)
+{
+	if (!(rand()%60))
+	{
+		s = sim->create_part(-3, x, y, PT_ELEC);
+		if(s >= 0) {
+			parts[i].temp += 400.0f;
+			parts[s].temp = parts[i].temp;
+			sim->pv[y/CELL][x/CELL] += 1.5f;
+		}
+	}
+	return 0;
+}
+
+//#TPT-Directive ElementHeader Element_X002 static int graphics(GRAPHICS_FUNC_ARGS)
+int Element_X002::graphics(GRAPHICS_FUNC_ARGS)
+
+{
+	*firea = 70;
+	*firer = *colr;
+	*fireg = *colg;
+	*fireb = *colb;
+
+	*pixel_mode |= FIRE_ADD;
+	return 0;
+}
+
+
+Element_X002::~Element_X002() {}
