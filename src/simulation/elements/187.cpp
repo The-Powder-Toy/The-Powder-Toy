86ae7359e8bc2e992ec19011c4417fbc28091c61
My main account <sf2164229838@sina.com>
2017-02-17 15:18:52 +0800

    Add files via upload

--- src/simulation/elements/187.cpp
+++ src/simulation/elements/187.cpp
@@ -46,46 +46,82 @@ Element_E187::Element_E187()
 	HighTemperatureTransition = NT;
 
 	Update = &Element_E187::update;
+	Graphics = &Element_E187::graphics;
 }
 
 //#TPT-Directive ElementHeader Element_E187 static int update(UPDATE_FUNC_ARGS)
 int Element_E187::update(UPDATE_FUNC_ARGS)
 { // for both ISZS and ISOZ
-	int r, s;
+	int r, s, rx, ry;
 	float r2, r3;
 	const int cooldown = 15;
-	if (!parts[i].life)
-	{
-		if (!(rand()%10000) && !parts[i].tmp)
+	switch (parts[i].ctype) {
+	case 1:
+		for (rx=-2; rx<3; rx++)
+			for (ry=-2; ry<3; ry++)
+				if (BOUNDS_CHECK && (rx || ry))
+				{
+					r = pmap[y+ry][x+rx];
+					if ((r & 0xFF) == PT_E187 && !(rand()%200))
+					{
+						parts[r>>8].tmp = 0;
+						sim->pv[y/CELL][x/CELL] += 3.0f;
+					}
+				}
+		break;
+	default:
+		if (!parts[i].life)
 		{
-			s = sim->create_part(-3, x, y, PT_PHOT);
-			r2 = (rand()%128+128)/127.0f;
-			r3 = (rand()%360)*3.1415926f/180.0f;
-			parts[s].vx = r2*cosf(r3);
-			parts[s].vy = r2*sinf(r3);
-			parts[i].life = cooldown;
-			parts[i].tmp = 1;
-			parts[s].life = rand()%480+480;
-			parts[s].tmp = 0x1;
-			parts[s].temp = parts[i].temp + 20;
+			if (!(rand()%10000) && !parts[i].tmp)
+			{
+				s = sim->create_part(-3, x, y, PT_PHOT);
+				if(s >= 0) {
+					r2 = (rand()%128+128)/127.0f;
+					r3 = (rand()%360)*3.1415926f/180.0f;
+					parts[s].vx = r2*cosf(r3);
+					parts[s].vy = r2*sinf(r3);
+					parts[i].life = cooldown;
+					parts[i].tmp = 1;
+					parts[s].life = rand()%480+480;
+					parts[s].tmp = 0x1;
+					parts[s].temp = parts[i].temp + 20;
+				}
+			}
+			r = sim->photons[y][x];
+			if ((r & 0xFF) == PT_PHOT && !(rand()%100))
+			{
+				if(s >= 0) {
+					s = sim->create_part(-3, x, y, PT_PHOT);
+					r2 = (rand()%128+128)/127.0f;
+					r3 = (rand()%360)*3.1415926f/180.0f;
+					parts[s].vx = r2*cosf(r3);
+					parts[s].vy = r2*sinf(r3);
+					parts[i].life = cooldown;
+					parts[i].tmp = 1;
+					parts[s].life = rand()%480+480;
+					parts[s].tmp = 0x1;
+					parts[s].temp = parts[i].temp + 20;
+				}
+			}
 		}
-		r = sim->photons[y][x];
-		if ((r & 0xFF) == PT_PHOT && !(rand()%100))
-		{
-			s = sim->create_part(-3, x, y, PT_PHOT);
-			r2 = (rand()%128+128)/127.0f;
-			r3 = (rand()%360)*3.1415926f/180.0f;
-			parts[s].vx = r2*cosf(r3);
-			parts[s].vy = r2*sinf(r3);
-			parts[i].life = cooldown;
-			parts[i].tmp = 1;
-			parts[s].life = rand()%480+480;
-			parts[s].tmp = 0x1;
-			parts[s].temp = parts[i].temp + 20;
-		}
+		break;
 	}
 	return 0;
 }
 
+int Element_E187::graphics(GRAPHICS_FUNC_ARGS)
+{
+	switch(cpart->ctype) {
+	case 1:
+		*colr = 0xFF;
+		*colg = 0xD0;
+		*colb = 0x60;
+		break;
+	default:
+		break;
+	}
+
+	return 0;
+}
 
 Element_E187::~Element_E187() {}
