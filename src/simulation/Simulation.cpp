45b20098f8c2cf8b39831a37335f81ce2f6434d2
My main account <sf2164229838@sina.com>
2017-02-18 10:50:48 +0800

    Add files via upload

--- src/simulation/Simulation.cpp
+++ src/simulation/Simulation.cpp
@@ -2066,10 +2066,7 @@ void Simulation::init_can_move()
 	// TODO: replace with property
 	for (destinationType = 0; destinationType < PT_NUM; destinationType++)
 	{
-		if (destinationType == PT_GLAS || destinationType == PT_PHOT || destinationType == PT_FILT || destinationType == PT_INVIS
-		 || destinationType == PT_WATR || destinationType == PT_DSTW || destinationType == PT_SLTW || destinationType == PT_GLOW
-		 || destinationType == PT_ISOZ || destinationType == PT_ISZS || destinationType == PT_QRTZ || destinationType == PT_PQRT
-		 || destinationType == PT_H2 || elements[destinationType].Properties&PROP_TRANSPARENT)
+		if (destinationType == PT_PHOT || destinationType == PT_INVIS || destinationType == PT_H2 || elements[destinationType].Properties&PROP_TRANSPARENT)
 			can_move[PT_PHOT][destinationType] = 2;
 		if (destinationType != PT_DMND && destinationType != PT_INSL && destinationType != PT_VOID && destinationType != PT_PVOD && destinationType != PT_VIBR && destinationType != PT_BVBR && destinationType != PT_PRTI && destinationType != PT_PRTO && destinationType != PT_E187 && destinationType != PT_E188)
 		{
