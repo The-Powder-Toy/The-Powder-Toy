ba1dcfababdbe1655dbd8fc966870409a0ad9f86
My main account <sf2164229838@sina.com>
2017-02-23 13:22:39 +0800

    Add files via upload

--- src/simulation/elements/187.cpp
+++ src/simulation/elements/187.cpp
@@ -67,21 +67,21 @@ int Element_E187::update(UPDATE_FUNC_ARGS)
 			{
 				Element_E187::createPhotons(sim, i, x, y, stmp, parts);
 			}
-		}
-		for (rx=-2; rx<3; rx++)
-			for (ry=-2; ry<3; ry++)
-				if (BOUNDS_CHECK)
-				{
-					r = pmap[y+ry][x+rx];
-					rt = r >> 8;
-					if (parts[rt].type == PT_LAVA && parts[rt].ctype == PT_TUNG && parts[i].temp > 9300 && !(rand()%100))
+			for (rx=-2; rx<3; rx++)
+				for (ry=-2; ry<3; ry++)
+					if (BOUNDS_CHECK)
 					{
-						sim->create_part(rt, x, y, PT_E187);
-						parts[rt].temp = MAX_TEMP;
-						parts[rt].tmp = parts[i].tmp & 0xFFFFFFFE;
-						sim->pv[y/CELL][x/CELL] += 20.0f;
+						r = pmap[y+ry][x+rx];
+						rt = r >> 8;
+						if (parts[rt].type == PT_LAVA && parts[rt].ctype == PT_TUNG && parts[i].temp > 9300 && !(rand()%100))
+						{
+							sim->create_part(rt, x, y, PT_E187);
+							parts[rt].temp = MAX_TEMP;
+							parts[rt].tmp = parts[i].tmp & 0xFFFFFFFE;
+							sim->pv[y/CELL][x/CELL] += 20.0f;
+						}
 					}
-				}
+		}
 		break;
 	case 1:
 		if (parts[i].tmp)
