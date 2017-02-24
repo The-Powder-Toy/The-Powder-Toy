f21f5e64d97201a1dec18bf3cfdaf991925e4da0
My main account <sf2164229838@sina.com>
2017-02-24 22:47:33 +0800

    Add files via upload

--- src/simulation/elements/TRON.cpp
+++ src/simulation/elements/TRON.cpp
@@ -233,7 +233,7 @@ int Element_TRON::trymovetron(Simulation * sim, int x, int y, int dir, int i, in
 		r = sim->pmap[ry][rx];
 		if (canmovetron(sim, r, k-1) && !sim->bmap[(ry)/CELL][(rx)/CELL] && ry > CELL && rx > CELL && ry < YRES-CELL && rx < XRES-CELL)
 		{
-			if (r&0xFF == PT_E189)
+			if ((r&0xFF) == PT_E189)
 				return len+2;
 			count++;
 			for (tx = rx - tron_ry[dir] , ty = ry - tron_rx[dir], j=1; abs(tx-rx) < (len-k) && abs(ty-ry) < (len-k); tx-=tron_ry[dir],ty-=tron_rx[dir],j++)
