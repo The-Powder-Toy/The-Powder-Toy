782af4a598c7166b11e4122be1f5ed7b4bccd242
My main account <sf2164229838@sina.com>
2017-02-27 20:27:44 +0800

    Add files via upload

--- src/simulation/elements/DRAY.cpp
+++ src/simulation/elements/DRAY.cpp
@@ -137,7 +137,7 @@ int Element_DRAY::update(UPDATE_FUNC_ARGS)
 								}
 								else
 								{
-									if (pmap[yCopyTo][xCopyTo] && pmap[yCopyTo][xCopyTo] != PT_DMND)
+									if (pmap[yCopyTo][xCopyTo] && (pmap[yCopyTo][xCopyTo] & 0xFF) != PT_DMND)
 										sim->kill_part(pmap[yCopyTo][xCopyTo]>>8);
 								}
 							}
