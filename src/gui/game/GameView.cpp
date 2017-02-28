23734d267a5406ad0a5bd04f615030c6c6695aa3
My main account <sf2164229838@sina.com>
2017-02-28 10:47:20 +0800

    Add files via upload

--- src/gui/game/GameView.cpp
+++ src/gui/game/GameView.cpp
@@ -2325,7 +2325,7 @@ void GameView::OnDraw()
 				}
 				else
 				{
-					if (type == PT_E189 && partlife >= 0 && partlife <= 10)
+					if (type == PT_E189 && partlife >= 0 && partlife <= 11)
 					{
 						const char* E189Modes[] = {"PRINS", "PRINS", "TRONI", "TRONO", "LASER", "DIRCH", "HEATER", "PHTD", "VIBR2", "VIBR2", "DEBUG", "PHTEM"};
 						sampleInfo << E189Modes[partlife];
@@ -2361,7 +2361,7 @@ void GameView::OnDraw()
 					sampleInfo << c->ElementResolve(type, -1) << " with " << c->ElementResolve(ctype, (int)sample.particle.pavg[1]);
 				else if (type == PT_LIFE)
 					sampleInfo << c->ElementResolve(type, ctype);
-				else if (type == PT_E189 && partlife >= 0 && partlife <= 10)
+				else if (type == PT_E189 && partlife >= 0 && partlife <= 11)
 				{
 					const char* E189Modes[] = {"PRINS", "PRINS", "TRONI", "TRONO", "LASER", "DIRCH", "HEATER", "PHTD", "VIBR2", "VIBR2", "DEBUG", "PHTEM"};
 					sampleInfo << E189Modes[partlife];
