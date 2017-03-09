1192b18323c385bf033c32ff7119d4afd6c951e9
My main account <sf2164229838@sina.com>
2017-03-09 21:17:22 +0800

    Add files via upload

--- src/lua/LuaScriptInterface.cpp
+++ src/lua/LuaScriptInterface.cpp
@@ -2130,6 +2130,7 @@ int LuaScriptInterface::simulation_createDebugComponent (lua_State * l)
 		i = luacon_sim->create_part(-1, __x++, __y, PT_E189, 10);
 		if (i >= 0)
 			luacon_sim->parts[i].ctype = (__dx & 0xFFFF) | (__dy << 16);
+			luacon_sim->parts[i].tmp |= 0x2;
 		while (*__str)
 		{
 			i = luacon_sim->create_part(-1, __x++, __y, PT_E189, 10);
