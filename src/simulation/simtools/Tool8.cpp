c9e1da0f661b3ad745de1c345d351ada7ef4ea5e
My main account <sf2164229838@sina.com>
2017-03-13 17:50:08 +0800

    Add files via upload

--- src/simulation/simtools/Tool8.cpp
+++ src/simulation/simtools/Tool8.cpp
@@ -4,14 +4,21 @@
 Tool_Tool8::Tool_Tool8()
 {
 	Identifier = "DEFAULT_TOOL_TOOL8";
-	Name = "T8";
+	Name = "FRCT";
 	Colour = PIXPACK(0xEE22EE);
-	Description = "Experimental tool.";
+	Description = "Fractalizer!";
 }
 
 int Tool_Tool8::Perform(Simulation * sim, Particle * cpart, int x, int y, float strength)
 {
-	/* freed */
+	if(!cpart)
+	{
+		int i1 = sim->create_part(-1, x, y, PT_E189, 13);
+		if (i1 >= 0)
+		{
+			cpart->ctype = 0xFF000000 | ((x & 0xFF) << 16) | ((y & 0xFF) << 8) | ((x * y) & 0xFF);
+		}
+	}
 }
 
 Tool_Tool8::~Tool_Tool8() {}
