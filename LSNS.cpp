9ce6a2f56fe161342dbbd7c885a487f196b2347b
QuanTech0 <1337h4x0rname@gmail.com>
2017-03-16 21:50:53 -0400

    Celsius is cool
    ya boi

--- LSNS.cpp
+++ LSNS.cpp
@@ -80,7 +80,7 @@ int Element_LSNS::update(UPDATE_FUNC_ARGS)
 					r = sim->photons[y+ry][x+rx];
 				if(!r)
 					continue;
-				if (parts[r>>8].life > parts[i].temp)
+				if (parts[r>>8].life > parts[i].temp+273.15)
 					parts[i].life = 1;
 			}
 	return 0;
