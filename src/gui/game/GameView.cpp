6348cbf364c77f95a4b021b28065538d64855063
My main account <sf2164229838@sina.com>
2017-03-18 19:16:42 +0800

    Add files via upload

--- src/gui/game/GameView.cpp
+++ src/gui/game/GameView.cpp
@@ -2368,7 +2368,7 @@ void GameView::OnDraw()
 		static const char* E189Modes[] = {
 			"PRSINS", "PRSINS", "TRONI", "TRONO", "LASER", "DIRCH", "HEATER", "PHTDUP", "VIBR2", "VIBR2",
 			"DEBUG", "PHTEM", "SPREFL", "DECOR", "DECO2", "PRTINS", "LOGICG", "PHDIOD", "DECO3", "NOTGIN",
-			"PARTEM", "EXPANDER", "EN_REFL", "STKMJ", "MOV_DRAY"
+			"PARTEM", "EXPANDER", "EN_REFL", "STKMJ", "MOV_DRAY", "EXT_DRAY"
 		};
 		static const int E189IntM[] = {0x01055000};
 		//Draw info about simulation under cursor
@@ -2443,7 +2443,7 @@ void GameView::OnDraw()
 				}
 				else
 				{
-					if (type == PT_E189 && partlife >= 0 && partlife <= 24)
+					if (type == PT_E189 && partlife >= 0 && partlife <= 25)
 					{
 						sampleInfo << E189Modes[partlife];
 					}
@@ -2565,7 +2565,7 @@ void GameView::OnDraw()
 					sampleInfo << c->ElementResolve(type, -1) << " with " << c->ElementResolve(ctype, (int)sample.particle.pavg[1]);
 				else if (type == PT_LIFE)
 					sampleInfo << c->ElementResolve(type, ctype);
-				else if (type == PT_E189 && partlife >= 0 && partlife <= 24)
+				else if (type == PT_E189 && partlife >= 0 && partlife <= 25)
 				{
 					sampleInfo << E189Modes[partlife];
 				}
