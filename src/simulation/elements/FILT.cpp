c06bb64f8ea14119da83b22ac34c69cc8dff5ab8
My main account <sf2164229838@sina.com>
2017-03-23 13:15:10 +0800

    Add files via upload

--- src/simulation/elements/FILT.cpp
+++ src/simulation/elements/FILT.cpp
@@ -30,7 +30,7 @@ Element_FILT::Element_FILT()
 	HeatConduct = 251;
 	Description = "Filter for photons, changes the color.";
 
-	Properties = TYPE_SOLID | PROP_NOAMBHEAT | PROP_LIFE_DEC | PROP_TRANSPARENT;
+	Properties = TYPE_SOLID | PROP_NOAMBHEAT | PROP_LIFE_DEC | PROP_TRANSPARENT | PROP_INVISIBLE;
 
 	LowPressure = IPL;
 	LowPressureTransition = NT;
