97e2c8add9a130ede67c649d50f349d74196ad8f
My main account <sf2164229838@sina.com>
2017-03-26 18:58:41 +0800

    Add files via upload

--- src/simulation/elements/PSTN.cpp
+++ src/simulation/elements/PSTN.cpp
@@ -194,7 +194,7 @@ int Element_PSTN::CanMoveStack(Simulation * sim, int stackX, int stackY, int dir
 			break;
 
 		r = sim->pmap[posY][posX];
-		if (sim->IsWallBlocking(posX, posY, 0) || (block && ( (r&0xFF) == block || (r&0xFF) == PT_INDI && (sim->parts[r>>8].tmp & 1) ) ))
+		if (sim->IsWallBlocking(posX, posY, 0) || (block && (r&0xFF) == block) || ( (r&0xFF) == PT_INDI && (sim->parts[r>>8].tmp & 1) ) )
 			return spaces;
 		if (!r)
 		{
@@ -290,7 +290,7 @@ int Element_PSTN::MoveStack(Simulation * sim, int stackX, int stackY, int direct
 				break;
 			}
 			r = sim->pmap[posY][posX];
-			if (!r || (r&0xFF) == block)
+			if (!r || (r&0xFF) == block || ((r&0xFF) == PT_INDI && (parts[r>>8].tmp & 1)))
 				break;
 			if (!sticky && (r&0xFF) != PT_FRME && stickylimit <= 0) {
 				break;
