8c0465c6b058492bca8db875bd80b46ecb12c37e
My main account <sf2164229838@sina.com>
2017-03-29 20:54:57 +0800

    Add files via upload

---
+++ src/simulation/elements/PINS.cpp
@@ -0,0 +1,49 @@
+#include "simulation/Elements.h"
+#include "simulation/Air.h"
+//#TPT-Directive ElementClass Element_PINS PT_PINS 191
+Element_PINS::Element_PINS()
+{
+	Identifier = "DEFAULT_PT_PINS";
+	Name = "PINS";
+	Colour = PIXPACK(0x939699);
+	MenuVisible = 1;
+	MenuSection = SC_SOLIDS;
+	Enabled = 1;
+
+	Advection = 0.0f;
+	AirDrag = 0.00f * CFDS;
+	AirLoss = 0.90f;
+	Loss = 0.00f;
+	Collision = 0.0f;
+	Gravity = 0.0f;
+	Diffusion = 0.00f;
+	HotAir = 0.000f	* CFDS;
+	Falldown = 0;
+
+	Flammable = 0;
+	Explosive = 0;
+	Meltable = 0;
+	Hardness = 0;
+
+	Weight = 100;
+
+	Temperature = R_TEMP+0.0f +273.15f;
+	HeatConduct = 251;
+	Description = "Blocks all air pressure.";
+
+	Properties = TYPE_SOLID;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = IPH;
+	HighPressureTransition = NT;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = ITH;
+	HighTemperatureTransition = NT;
+
+	Update = &Element_TTAN::update;
+}
+
+
+Element_PINS::~Element_PINS() {}
