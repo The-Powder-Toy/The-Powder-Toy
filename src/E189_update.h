45c69ec7db00ae6b0f6f7caa0cc4622931c2b0fe
My main account <sf2164229838@sina.com>
2017-04-11 22:40:25 +0800

    Add files via upload

---
+++ src/E189_update.h
@@ -0,0 +1,23 @@
+#ifndef __E189_UPDATE_H__
+#define __E189_UPDATE_H__
+
+class Simulation;
+struct Particle;
+class E189_Update
+{
+public:
+	E189_Update();
+	virtual ~E189_Update();
+	static int update(UPDATE_FUNC_ARGS);
+	static void InsertText(Simulation *sim, int i, int x, int y, int ix, int iy);
+	static void conductTo (Simulation* sim, int r, int x, int y, Particle *parts) // Inline or macro?
+	{
+		if (!parts[r>>8].life)
+		{
+			parts[r>>8].ctype = r&0xFF;
+			sim->part_change_type(r>>8, x, y, PT_SPRK);
+			parts[r>>8].life = 4;
+		}
+	}
+};
+#endif
