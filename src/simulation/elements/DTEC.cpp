dbeb8e5788315b30d15d00e2d3a27ff19bc36af7
My main account <sf2164229838@sina.com>
2017-04-12 20:29:46 +0800

    Add files via upload

--- src/simulation/elements/DTEC.cpp
+++ src/simulation/elements/DTEC.cpp
@@ -50,7 +50,7 @@ int Element_DTEC::update(UPDATE_FUNC_ARGS)
 	static char DTEC_ntztable[32] = { 0, 1, 2,24, 3,19, 6,25, 22, 4,20,10,16, 7,12,26,  31,23,18, 5,21, 9,15,11, 30,17, 8,14,29,13,28,27};
 	int r, rx, ry, rt, rd = parts[i].tmp2, pavg;
 	if (rd > 25) parts[i].tmp2 = rd = 25;
-	if (parts[i].life)
+	if (!parts[i].life == (parts[i].tmp3 & 1))
 	{
 		parts[i].life = 0;
 		for (rx=-2; rx<3; rx++)
