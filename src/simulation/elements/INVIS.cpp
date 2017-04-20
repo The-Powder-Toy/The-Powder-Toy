7078d7e5289bc6d2b3205ff8e1a4523670a662eb
Nickolay <nickolay02@inbox.ru>
2017-04-21 00:37:09 +0300

    Customizable FRAY and INVS (#390)

--- src/simulation/elements/INVIS.cpp
+++ src/simulation/elements/INVIS.cpp
@@ -48,10 +48,16 @@ Element_INVIS::Element_INVIS()
 //#TPT-Directive ElementHeader Element_INVIS static int update(UPDATE_FUNC_ARGS)
 int Element_INVIS::update(UPDATE_FUNC_ARGS)
 {
-	if (sim->pv[y/CELL][x/CELL]>4.0f || sim->pv[y/CELL][x/CELL]<-4.0f)
-		parts[i].tmp = 1;
+	float pressureResistance = 0.0f;
+	if (parts[i].tmp > 0)
+		pressureResistance = (float) parts[i].tmp;
 	else
-		parts[i].tmp = 0;
+		pressureResistance = 4.0f;
+
+	if (sim->pv[y/CELL][x/CELL] < -pressureResistance || sim->pv[y/CELL][x/CELL] > pressureResistance)
+		parts[i].tmp2 = 1;
+	else
+		parts[i].tmp2 = 0;
 	return 0;
 }
 
@@ -59,7 +65,7 @@ int Element_INVIS::update(UPDATE_FUNC_ARGS)
 int Element_INVIS::graphics(GRAPHICS_FUNC_ARGS)
 {
 	//pv[ny/CELL][nx/CELL]>4.0f || pv[ny/CELL][nx/CELL]<-4.0f
-	if(cpart->tmp)
+	if(cpart->tmp2)
 	{
 		*cola = 100;
 		*colr = 15;
