bf9014ac606164daebfefd9e83dcefcdab7abfb5
jacob1 <jfu614@gmail.com>
2017-07-23 21:17:21 -0400

    fix HEAC conducting to HSWC which is turned off

--- src/simulation/elements/HEAC.cpp
+++ src/simulation/elements/HEAC.cpp
@@ -136,13 +136,13 @@ int Element_HEAC::update(UPDATE_FUNC_ARGS)
 			if (x+rrx >= 0 && x+rrx < XRES && y+rry >= 0 && y+rry < YRES && !Element_HEAC::CheckLine<Element_HEAC::IsInsulator>(sim, x, y, x+rrx, y+rry, isInsulator))
 			{
 				r = pmap[y+rry][x+rrx];
-				if (r && sim->elements[r&0xFF].HeatConduct > 0)
+				if (r && sim->elements[r&0xFF].HeatConduct > 0 && (parts[r>>8].type != PT_HSWC || parts[r>>8].life == 10))
 				{
 					count++;
 					tempAgg += parts[r>>8].temp;
 				}
 				r = sim->photons[y+rry][x+rrx];
-				if (r && sim->elements[r&0xFF].HeatConduct > 0)
+				if (r && sim->elements[r&0xFF].HeatConduct > 0 && (parts[r>>8].type != PT_HSWC || parts[r>>8].life == 10))
 				{
 					count++;
 					tempAgg += parts[r>>8].temp;
@@ -164,12 +164,12 @@ int Element_HEAC::update(UPDATE_FUNC_ARGS)
 				if (x+rrx >= 0 && x+rrx < XRES && y+rry >= 0 && y+rry < YRES && !Element_HEAC::CheckLine<Element_HEAC::IsInsulator>(sim, x, y, x+rrx, y+rry, isInsulator))
 				{
 					r = pmap[y+rry][x+rrx];
-					if (r && sim->elements[r&0xFF].HeatConduct > 0)
+					if (r && sim->elements[r&0xFF].HeatConduct > 0 && (parts[r>>8].type != PT_HSWC || parts[r>>8].life == 10))
 					{
 						parts[r>>8].temp = parts[i].temp;
 					}
 					r = sim->photons[y+rry][x+rrx];
-					if (r && sim->elements[r&0xFF].HeatConduct > 0)
+					if (r && sim->elements[r&0xFF].HeatConduct > 0 && (parts[r>>8].type != PT_HSWC || parts[r>>8].life == 10))
 					{
 						parts[r>>8].temp = parts[i].temp;
 					}
