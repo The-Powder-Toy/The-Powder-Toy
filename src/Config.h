834e966842bec9d4456296d1096fb4335a3e2fa3
jacob1 <jfu614@gmail.com>
2017-09-04 23:08:52 -0400

    Version 92.2.333

--- src/Config.h
+++ src/Config.h
@@ -16,11 +16,11 @@
 #endif
 
 #ifndef MINOR_VERSION
-#define MINOR_VERSION 1
+#define MINOR_VERSION 2
 #endif
 
 #ifndef BUILD_NUM
-#define BUILD_NUM 332
+#define BUILD_NUM 333
 #endif
 
 #ifndef SNAPSHOT_ID
