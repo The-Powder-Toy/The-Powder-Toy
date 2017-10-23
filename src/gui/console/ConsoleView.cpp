ad38c353213a65dbbd2b56c994e71ed80f3ec792
RobertBScott <rscott10@mail.uoguelph.ca>
2017-10-23 15:38:27 -0400

    Update consoleview to redraw box based on cursor position.

--- src/gui/console/ConsoleView.cpp
+++ src/gui/console/ConsoleView.cpp
@@ -62,7 +62,19 @@ void ConsoleView::NotifyPreviousCommandsChanged(ConsoleModel * sender)
 	}
 	commandList.clear();
 	std::deque<ConsoleCommand> commands = sender->GetPreviousCommands();
-	int currentY = Size.Y - 32;
+	
+	/*Edit 1 below*/
+	int numLines = 0;
+	int numSpace = 0;
+	int index = commands.size() - 1;
+	if (index>0)
+	{
+		int totalY = 10 * commands[index].Command.size();
+		numLines = totalY / (Size.X / 2) + 2;
+	}
+	int currentY = Size.Y - 16 * numLines;
+	/*end edit 1*/
+
 	if(commands.size())
 		for(int i = commands.size()-1; i >= 0; i--)
 		{
@@ -78,7 +90,15 @@ void ConsoleView::NotifyPreviousCommandsChanged(ConsoleModel * sender)
 			tempLabel->Appearance.HorizontalAlign = ui::Appearance::AlignLeft;
 			commandList.push_back(tempLabel);
 			AddComponent(tempLabel);
-			currentY-=16;
+
+			/*edit 2 below*/
+			if (i>0)
+			{
+				int totalY = 10 * commands[i - 1].Command.size();
+				numSpace = totalY / (Size.X / 2) + 1;
+			}
+			currentY -= 16 * numSpace;
+			/*end edit 2*/
 		}
 }
 
@@ -94,7 +114,12 @@ void ConsoleView::OnDraw()
 	Graphics * g = GetGraphics();
 	g->fillrect(Position.X, Position.Y, Size.X, Size.Y, 0, 0, 0, 110);
 	g->draw_line(Position.X, Position.Y+Size.Y-16, Position.X+Size.X, Position.Y+Size.Y-16, 255, 255, 255, 160);
-	g->draw_line(Position.X, Position.Y+Size.Y, Position.X+Size.X, Position.Y+Size.Y, 255, 255, 255, 200);
+	//g->draw_line(Position.X, Position.Y+Size.Y, Position.X+Size.X, Position.Y+Size.Y, 255, 255, 255, 200);
+
+	/*new bottom line - edit 3*/
+	double totalY = 4.04 * commandField->GetText().size();
+	int numSpace = 16*(int(totalY) / Size.X);
+	g->draw_line(Position.X, Position.Y + Size.Y + numSpace, Position.X + Size.X, Position.Y + Size.Y + numSpace, 255, 255, 255, 200);
 }
 
 ConsoleView::~ConsoleView() {
