4cbf46f34f83a6812fb2d85217f8550701edb6bd
moonythed <moonythedwarf@protonmail.com>
2018-01-31 13:57:53 -0600

    Fixes some things for jacob1. Restyles the code.

--- src/simulation/elements/RAYT.cpp
+++ src/simulation/elements/RAYT.cpp
@@ -28,7 +28,7 @@ Element_RAYT::Element_RAYT() {
 
 	Temperature = 283.15f;
 	HeatConduct = 0;
-	Description = "RAYT scans in a certain direction until it detects a particle";
+	Description = "RAYT scans in 8 directions for the element in its ctype and sparks the conductor on the opposite side";
 
 	Properties = TYPE_SOLID | PROP_DRAWONCTYPE | PROP_NOCTYPEDRAW;
 
@@ -48,7 +48,6 @@ Element_RAYT::Element_RAYT() {
 #define IGNORE_ENERGY 1
 
 //NOTES:
-// life and tmp is used to store the direction RAYT will scan in, and is initially set based on the nearest conductive particle's position
 // ctype is used to store the target element, if any. (NONE is treated as a wildcard)
 // tmp2 is used for settings (binary flags). The flags are as follows:
 // 10: Inverts the CTYPE filter so that the element in ctype is the only thing that doesn't trigger RAYT, instead of the opposite.
@@ -57,62 +56,72 @@ Element_RAYT::Element_RAYT() {
 //#TPT-Directive ElementHeader Element_RAYT static int update(UPDATE_FUNC_ARGS)
 int Element_RAYT::update(UPDATE_FUNC_ARGS) {
 	int rx, ry, r = 0;
-	for (rx=-2; rx<3; rx++) {
-		for (ry=-2; ry<3; ry++) {
-			if (BOUNDS_CHECK && (rx || ry) && x+rx>=0 && y+ry>=0 && x+rx<XRES && y+ry<YRES) {
+	for (rx = -1; rx <= 1; rx++)
+	{
+		for (ry = -1; ry <= 1; ry++)
+		{
+			if (BOUNDS_CHECK && (rx || ry) && x+rx>=0 && y+ry>=0 && x+rx<XRES && y+ry<YRES)
+			{
 				r = pmap[y+ry][x+rx];
 				if (!r)
 					continue;
 
 				int rt = TYP(r);
-				if ((sim->elements[rt].Properties&PROP_CONDUCTS) && !(rt==PT_WATR||rt==PT_SLTW||rt==PT_NTCT||rt==PT_PTCT||rt==PT_INWR) && parts[ID(r)].life==0) {
+				if ((sim->elements[rt].Properties&PROP_CONDUCTS) && !(rt==PT_WATR||rt==PT_SLTW||rt==PT_NTCT||rt==PT_PTCT||rt==PT_INWR) && parts[ID(r)].life==0)
+				{
 					// Stolen from DRAY
 					bool foundParticle = false;
 					bool isEnergy = false;
 					int p = 0;
-					for (int xStep = rx*-1, yStep = ry*-1, xCurrent = x+xStep, yCurrent = y+yStep; ; xCurrent+=xStep, yCurrent+=yStep) {
+					for (int xStep = rx*-1, yStep = ry*-1, xCurrent = x+xStep, yCurrent = y+yStep; ; xCurrent+=xStep, yCurrent+=yStep)
+					{
 						int rr;
-						if (!(xCurrent>=0 && yCurrent>=0 && xCurrent<XRES && yCurrent<YRES)) {
+						if (!(xCurrent>=0 && yCurrent>=0 && xCurrent<XRES && yCurrent<YRES))
+						{
 							break; // We're out of bounds! Oops!
 						}
-						// haven't found a particle yet, keep looking for one
-						if (!foundParticle) {
-							rr = pmap[yCurrent][xCurrent];
-							if (!rr) {
-								rr = sim->photons[yCurrent][xCurrent];
-									if (rr) {
-										foundParticle = isEnergy = true;
-										goto checks;
-										break;
-									}
-							} else {
-								foundParticle = true;
-								checks:
-								if (parts[i].ctype == PT_NONE && TYP(rr) != PT_NONE) {
-									parts[ID(r)].life = 4;
-									parts[ID(r)].ctype = rt;
-									sim->part_change_type(ID(r),x+rx,y+ry,PT_SPRK);
+						rr = pmap[yCurrent][xCurrent];
+						if (!rr)
+						{
+							rr = sim->photons[yCurrent][xCurrent];
+								if (rr)
+								{
+									isEnergy = true;
 								}
-								if (isEnergy) {
-									if ((parts[i].tmp2 & 1) && ((TYP(rr) == TYP(parts[i].ctype)) xor (parts[i].tmp2 & (1 << 1)))) {
-										if (sim->parts_avg(i,ID(r),PT_INSL) != PT_INSL) {
-											parts[ID(r)].life = 4;
-											parts[ID(r)].ctype = rt;
-											sim->part_change_type(ID(r),x+rx,y+ry,PT_SPRK);
-										}
-									} else {
-										break;
-									}
-								}
-								if ((TYP(rr) == TYP(parts[i].ctype)) xor (parts[i].tmp2 & (1 << 1))) {
-									if (sim->parts_avg(i,ID(r),PT_INSL) != PT_INSL) {
+						}
+						if (rr)
+						{
+							if (parts[i].ctype == PT_NONE && TYP(rr) != PT_NONE)
+							{
+								parts[ID(r)].life = 4;
+								parts[ID(r)].ctype = rt;
+								sim->part_change_type(ID(r),x+rx,y+ry,PT_SPRK);
+							}
+							if (isEnergy)
+							{
+								if ((parts[i].tmp2 & 1) && ((TYP(rr) == TYP(parts[i].ctype)) xor (parts[i].tmp2 & (1 << 1))))
+								{
+									if (sim->parts_avg(i,ID(r),PT_INSL) != PT_INSL)
+									{
 										parts[ID(r)].life = 4;
 										parts[ID(r)].ctype = rt;
 										sim->part_change_type(ID(r),x+rx,y+ry,PT_SPRK);
 									}
+								} else
+								{
+									break;
 								}
-								break;
 							}
+							if ((TYP(rr) == TYP(parts[i].ctype)) xor (parts[i].tmp2 & (1 << 1)))
+							{
+								if (sim->parts_avg(i,ID(r),PT_INSL) != PT_INSL)
+								{
+									parts[ID(r)].life = 4;
+									parts[ID(r)].ctype = rt;
+									sim->part_change_type(ID(r),x+rx,y+ry,PT_SPRK);
+								}
+							}
+							break;
 						}
 					}
 				}
