4c969332e07483f8d065ca56b1838f7f01accf4b
jombo23 <jombo23@gmail.com>
2018-02-03 00:52:55 -0500

    Adjust AND to MOD to apply to all CELL sizes
    Thanks jacob1

--- src/simulation/simtools/Cyclone.cpp
+++ src/simulation/simtools/Cyclone.cpp
@@ -17,25 +17,16 @@ int Tool_Cycl::Perform(Simulation * sim, Particle * cpart, int x, int y, int bru
 		Angle of cell is calculated via cells X/Y relation to the brush center and ArcTangent
 		Angle has 1.57 Radians added to it (90 degrees) in order to make the velocity be at 90 degrees to the centerpoint.
 		Ditto for X, except X uses Cosine
-
-		Strength is changed on CELL != 4 due to the fact that the calculation will be run 16x as much.
 	*/
 	
-	// Cell size of 4 optimization (greatly needed)
-	
-#if CELL == 4
-	if ((x & 3) == 0 && (y & 3) == 0)
+
+	if ((x%CELL) == 0 && (y%CELL) == 0)
 	{
 		sim->air->vy[y / CELL][x / CELL] -= (strength / 16) * (sin(1.57f + (atan2(brushY - y, brushX - x))));
 		sim->air->vx[y / CELL][x / CELL] -= (strength / 16) * (cos(1.57f + (atan2(brushY - y, brushX - x))));
 	}
 
 
-#else
-	sim->air->vy[y / CELL][x / CELL] -= (strength / 256) * (sin(1.57f + (atan2(brushY - y, brushX - x))));
-	sim->air->vx[y / CELL][x / CELL] -= (strength / 256) * (cos(1.57f + (atan2(brushY - y, brushX - x))));
-
-#endif /* CELL == 4*/
 
 	// Clamp velocities
 
