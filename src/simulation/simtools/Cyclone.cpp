f3181059d94ba39845641262b2c47ad67bb3bf77
jacob1 <jfu614@gmail.com>
2018-02-13 00:07:41 -0500

    make the code look nicer

--- src/simulation/simtools/Cyclone.cpp
+++ src/simulation/simtools/Cyclone.cpp
@@ -6,48 +6,38 @@ Tool_Cycl::Tool_Cycl()
 	Identifier = "DEFAULT_TOOL_CYCL";
 	Name = "CYCL";
 	Colour = PIXPACK(0x132f5b);
-	Description = "Cyclone. Produces Swirling Air Currents";
+	Description = "Cyclone. Produces swirling air currents";
 }
 
 int Tool_Cycl::Perform(Simulation * sim, Particle * cpart, int x, int y, int brushX, int brushY, float strength)
 {
 	/* 
 		Air velocity calculation.
-		Air velocity Y = Sine of cell angle
-		Angle of cell is calculated via cells X/Y relation to the brush center and ArcTangent
-		Angle has 1.57 Radians added to it (90 degrees) in order to make the velocity be at 90 degrees to the centerpoint.
-		Ditto for X, except X uses Cosine
+		Air velocity X = cosine of cell angle
+		Angle of cell is calculated via cells X/Y relation to the brush center and arctangent
+		Angle has 1.57 radians added to it (90 degrees) in order to make the velocity be at 90 degrees to the centerpoint.
+		Ditto for X, except X uses sine
 	*/
-	
-
+	// only trigger once per cell (less laggy)
 	if ((x%CELL) == 0 && (y%CELL) == 0)
 	{
-		sim->air->vy[y / CELL][x / CELL] -= (strength / 16) * (sin(1.57f + (atan2(brushY - y, brushX - x))));
-		sim->air->vx[y / CELL][x / CELL] -= (strength / 16) * (cos(1.57f + (atan2(brushY - y, brushX - x))));
+		float *vx = &sim->air->vx[y/CELL][x/CELL];
+		float *vy = &sim->air->vy[y/CELL][x/CELL];
+
+		*vx -= (strength / 16) * (cos(1.57f + (atan2(brushY - y, brushX - x))));
+		*vy -= (strength / 16) * (sin(1.57f + (atan2(brushY - y, brushX - x))));
+
+		// Clamp velocities
+		if (*vx > 256.0f)
+			*vx = 256.0f;
+		else if (*vx < -256.0f)
+			*vx = -256.0f;
+		if (*vy > 256.0f)
+			*vy = 256.0f;
+		else if (*vy < -256.0f)
+			*vy = -256.0f;
+
 	}
-
-
-
-	// Clamp velocities
-
-	if (sim->air->vx[y / CELL][x / CELL] > 256.0f)
-	{
-		sim->air->vx[y / CELL][x / CELL] = 256.0f;
-	}
-	if (sim->air->vy[y / CELL][x / CELL] > 256.0f)
-	{
-		sim->air->vy[y / CELL][x / CELL] = 256.0f;
-	}
-
-	if (sim->air->vx[y / CELL][x / CELL] < -256.0f)
-	{
-		sim->air->vx[y / CELL][x / CELL] = -256.0f;
-	}
-	if (sim->air->vy[y / CELL][x / CELL] < -256.0f)
-	{
-		sim->air->vy[y / CELL][x / CELL] = -256.0f;
-	}
-
 
 	return 1;
 }
