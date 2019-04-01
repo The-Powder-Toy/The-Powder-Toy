1fee3f4bddf175db5c12f1163f12cb089014fc45
cracker1000 <ashishpurohit271@gmail.com>
2019-04-01 12:56:41 +0530

    Modify sprk.cpp for LITH element.

--- src/simulation/elements/SPRK.cpp
+++ src/simulation/elements/SPRK.cpp
@@ -230,6 +230,21 @@ int Element_SPRK::update(UPDATE_FUNC_ARGS)
 						else if (sender==PT_NSCN && parts[ID(r)].tmp == 3) parts[ID(r)].tmp = 1;
 					}
 					continue;
+				case PT_LITH:
+					if (abs(rx) < 2 && abs(ry) < 2 && parts[i].life < 4)
+					{
+						if (sender == PT_PSCN && parts[ID(r)].tmp == 0) parts[ID(r)].tmp = 2;
+						else if (sender == PT_NSCN && parts[ID(r)].tmp == 3) parts[ID(r)].tmp = 1;
+						if (sender == PT_INST && parts[ID(r)].life == 10)
+						{
+							parts[ID(r)].temp += 4;
+						}
+						else if (sender == PT_INST && parts[ID(r)].temp < 474.15f && parts[ID(r)].life == 10)
+						{
+							parts[ID(r)].temp += 40;
+						}
+					}
+					continue;
 				case PT_LED:
 					if (abs(rx) < 2 && abs(ry) < 2 && parts[i].life < 4)
 					{
@@ -237,24 +252,6 @@ int Element_SPRK::update(UPDATE_FUNC_ARGS)
 						else if (sender == PT_NSCN && parts[ID(r)].tmp == 3) parts[ID(r)].tmp = 1;
 					}
 					continue;
-				case PT_RBTR:
-					if (sender == PT_PSCN)
-					{
-						parts[ID(r)].tmp = 10;
-					}
-					 if (sender == PT_INST)
-					{
-						parts[ID(r)].temp += 1;
-					}
-					 if (sender == PT_INST &&  parts[ID(r)].tmp2 == 1)
-					 {
-						 parts[ID(r)].temp += 5;
-					 }
-				 if (sender == PT_NSCN)
-					 {
-						 parts[ID(r)].tmp = 0;
-					 }
-					break;
 								case PT_PPIP:
 					if (parts[i].life == 3 && pavg!=PT_INSL)
 					{
