0a03d00b8f91ce5eecb304116139d4f910db6f7a
cracker1000 <ashishpurohit271@gmail.com>
2019-05-24 22:39:34 +0530

    Change the way it does property reset.

--- src/simulation/simtools/Reset tool.cpp
+++ src/simulation/simtools/Reset tool.cpp
@@ -1,4 +1,5 @@
 #include "simulation/ToolCommon.h"
+#include "simulation/ElementCommon.h"
 
 //#TPT-Directive ToolClass Tool_CRCK TOOL_CRCK 1
 Tool_CRCK::Tool_CRCK()
@@ -10,53 +11,15 @@ Tool_CRCK::Tool_CRCK()
 }
 
 int Tool_CRCK::Perform(Simulation * sim, Particle * cpart, int x, int y, int brushX, int brushY, float strength)
+
 {
-	int r, rx, ry, np;
-	if (!cpart)
+	int i = sim->pmap[y][x];
+	if (!i)
 	{
 		return 0;
 	}
-	cpart->temp = 296.0f;
-	cpart->life = 0;
-	cpart->tmp = 0;
-	cpart->tmp2 = 0;
-	cpart->ctype = PT_NONE;
-	cpart->dcolour = 0x0ACAA;
-
-	 if  (PT_LNTG)
-	{
-		cpart->temp = 70.15f;
-		cpart->dcolour = 0x0ACAA;
-	}
-
-	if (PT_GLOW)
-	{
-		cpart->temp = R_TEMP + 20.0f + 273.15f;
-		cpart->dcolour = 0x0ACAA;
-	}
-
-	if (PT_LO2)
-	{
-		cpart->temp = 80.0f;
-		cpart->dcolour = 0x0ACAA;
-	}
-	if (PT_LAVA)
-	{
-		cpart->temp = R_TEMP + 1500.0f + 273.15f;
-		cpart->ctype = PT_NONE;
-		cpart->dcolour = 0x0ACAA;
-	}
-	if (PT_VIRS)
-	{
-		cpart->dcolour = 0x0ACAA;
-		cpart->temp = 72.0f + 273.15f;
-	}
-	if (PT_PHOT)
-	{
-		R_TEMP + 900.0f + 273.15f;
-	}
-	
-	return 1;
+	sim->create_part (i, x, y, cpart->type);
+	return 0;
 }
 
 Tool_CRCK::~Tool_CRCK() {}
