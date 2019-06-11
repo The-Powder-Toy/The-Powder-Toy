75e6ece1843b211dfe9d563afbfd7738e2a6a390
Tamás Bálint Misius <lbphacker@gmail.com>
2019-06-11 20:43:34 +0200

    Expose transition constants to Lua (fixes #654)

--- src/lua/LuaScriptInterface.cpp
+++ src/lua/LuaScriptInterface.cpp
@@ -789,6 +789,12 @@ void LuaScriptInterface::initSimulationAPI()
 	SETCONST(l, XRES);
 	SETCONST(l, YRES);
 	SETCONST(l, CELL);
+	SETCONST(l, NT);
+	SETCONST(l, ST);
+	SETCONST(l, ITH);
+	SETCONST(l, ITL);
+	SETCONST(l, IPH);
+	SETCONST(l, IPL);
 	SETCONST(l, PT_NUM);
 	lua_pushinteger(l, 0); lua_setfield(l, -2, "NUM_PARTS");
 	SETCONST(l, R_TEMP);
