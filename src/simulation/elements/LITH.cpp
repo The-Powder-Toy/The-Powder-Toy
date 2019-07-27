82708110f220b7f105e13561cf67dcb70d9c0cb2
cracker1000 <ashishpurohit271@gmail.com>
2019-07-27 21:38:41 +0530

    Update Add changes as suggested by @LBPHacker. Colors now change very gradually as LITH charges. .tmp2 = 1 is now powered battery mode. Activated state is now more clearly visible.

--- src/simulation/elements/LITH.cpp
+++ src/simulation/elements/LITH.cpp
@@ -26,9 +26,9 @@ Element_LITH::Element_LITH()
 
 	Weight = 100;
 
-	Temperature = 10.0f + 273.15f;
+	Temperature = R_TEMP + 273.15f;
 	HeatConduct = 0;
-	Description = "Lithium battery.(INST recieve/send charge, .tmp2 sets fast charge, Temp sets capacity.)";
+	Description = "Lithium battery. Charge with INST when deactivated, discharges to INST when activated. (use Heat/Cool)";
 
 	Properties = TYPE_SOLID;
 	LowPressure = IPL;
@@ -49,16 +49,21 @@ Element_LITH::Element_LITH()
 int Element_LITH::update(UPDATE_FUNC_ARGS)
 
 {
-	int r, rx, ry, charge, chargediffuse, capacity = parts[i].temp, In = 10.00f + 273.15f;;
-	//Prevent setting capacity below 10.
+	int r, rx, ry, charge, chargediffuse, In = 1.00f + 273.15f;
+	//Prevent setting capacity below 1.
 	if (parts[i].temp < In)
 	{
 		parts[i].temp = In;
 	}
 	//Explosion code ( Gets ignored in fast charge mode)
-	if (parts[i].tmp > parts[i].temp - 272.0f && parts[i].tmp2 != 1)
+	if (parts[i].tmp > parts[i].temp - 272.0f)
 	{
 		parts[i].type = PT_BOMB;
+	}
+	//Powered battery mode. (.tmp2 = 1 turns it into never discharging powered battery.)
+	if (parts[i].tmp2 == 1)
+	{
+		parts[i].tmp = parts[i].temp - 273.15f;
 	}
 	//Activation and Deactivation.
 	if (parts[i].life != 10)
@@ -150,38 +155,34 @@ int Element_LITH::update(UPDATE_FUNC_ARGS)
 //#TPT-Directive ElementHeader Element_LITH static int graphics(GRAPHICS_FUNC_ARGS)
 int Element_LITH::graphics(GRAPHICS_FUNC_ARGS)
 {
-	int gradv;
-	double tempOver = (((cpart->tmp)));
-	// Different colour states.
-	*pixel_mode |= FIRE_ADD;
+	// Charging/discharging.
+	if (cpart->life == 0)
+	{
+		*colr = 155;
+		*colg = 155;
+		*colb = 155;
+	}
+	if (cpart->tmp2 != 1)
+	{
+		if (cpart->tmp > 0)
+		{
+			int stagec = (int)(((float)cpart->tmp / (cpart->temp - 273.15))*100.0f);
+			*colg += stagec;
+			*colr -= stagec;
+			*colb -= stagec;
 
-	//Almost charged.
-	if (cpart->tmp + 1 >= cpart->temp - 273.15f)
-	{
-		double gradv = sin(tempOver) + 2.0;
-		*fireg = (int)(gradv * 255.0);
-		*colg += *fireg;
+		}
+		if (cpart->tmp <= 0 && cpart->life == 0)
+		{
+			*colr = 255;
+		}
 	}
-	//Discharged.
-	if (cpart->tmp <= 0 && cpart->life == 0)
+
+	//Powered battery mode.
+	if (cpart->tmp2 == 1)
 	{
-		double gradv = sin(tempOver) + 2.0;
-		*firer = (int)(gradv * 255.0);
-		*colr += *firer;
-	}
-	//Activated / Discharging.
-	if (cpart->life == 0 && cpart->tmp != 0 && cpart->tmp != cpart->temp)
-	{
-		double gradv = sin(tempOver) + 2.0;
-		*colr = 255.0;
-		*colg = 255.0;
-		*colb = 255.0;
-	}
-	//Fast charge mode.
-	if (cpart->tmp2 == 1 && cpart->life == 10)
-	{
-		double gradv = sin(tempOver) + 2.0;
-		*colb = 255.0;
+
+		*colb = 255;
 	}
 	return 0;
 }
--- src/simulation/elements/SPRK.cpp
+++ src/simulation/elements/SPRK.cpp
@@ -226,22 +226,18 @@ int Element_SPRK::update(UPDATE_FUNC_ARGS)
 						if (sender==PT_PSCN && parts[ID(r)].tmp == 0) parts[ID(r)].tmp = 2;
 						else if (sender==PT_NSCN && parts[ID(r)].tmp == 3) parts[ID(r)].tmp = 1;
 					}
+					continue;
 				case PT_LITH:
 				{
 					if (sender == PT_INST && parts[ID(r)].life == 10 && parts[ID(r)].tmp < parts[ID(r)].temp - 273.15f)
 					{
 						parts[ID(r)].tmp += 1;
 					}
-
 				}
 				if (parts[i].life < 4)
 				{
 					if (sender == PT_NSCN) parts[ID(r)].life = 10;
 					else if (sender == PT_PSCN && parts[ID(r)].life >= 10) parts[ID(r)].life = 9;
-				}
-				if (sender == PT_INST && parts[ID(r)].life == 10 && parts[ID(r)].tmp2 == 1) // Fast charge mode ignores the capacity limits.
-				{
-					parts[ID(r)].tmp += 10;
 				}
 				continue;
 				case PT_PPIP:
