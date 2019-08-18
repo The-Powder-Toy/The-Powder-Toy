e2787e00e54b8bf2b140c514a41411305df96e85
mniip <mniip@mniip.com>
2019-08-18 12:04:07 +0300

    Fix font editor compilation

--- data/font.h
+++ data/font.h
@@ -5,7 +5,7 @@ extern const unsigned char font_data[];
 extern const unsigned short font_ptrs[];
 extern const unsigned int font_ranges[][2];
 #else
-extern const unsigned char *font_data;
-extern const unsigned short *font_ptrs;
-extern const unsigned int (*font_ranges)[2];
+extern unsigned char *font_data;
+extern unsigned short *font_ptrs;
+extern unsigned int (*font_ranges)[2];
 #endif
