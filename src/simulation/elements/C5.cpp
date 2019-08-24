8dde7156702fd74169d85e0fb2b4bcb430bdedd9
moonheart08 <moonheart08@users.noreply.github.com>
2019-08-24 01:05:47 +0000

    Fix up C5

--- src/simulation/elements/C5.cpp
+++ src/simulation/elements/C5.cpp
@@ -48,12 +48,11 @@ Element_C5::Element_C5()
 //#TPT-Directive ElementHeader Element_C5 static int update(UPDATE_FUNC_ARGS)
 int Element_C5::update(UPDATE_FUNC_ARGS)
 {
-	int r, rx, ry;
-	for (rx=-2; rx<3; rx++)
-		for (ry=-2; ry<3; ry++)
+	for (int rx=-2; rx<3; rx++)
+		for (int ry=-2; ry<3; ry++)
 			if (BOUNDS_CHECK && (rx || ry))
 			{
-				r = pmap[y+ry][x+rx];
+				int r = pmap[y+ry][x+rx];
 				if (!r)
 					continue;
 				if ((TYP(r)!=PT_C5 && parts[ID(r)].temp<100 && sim->elements[TYP(r)].HeatConduct && (TYP(r)!=PT_HSWC||parts[ID(r)].life==10)) || TYP(r)==PT_CFLM)
@@ -73,7 +72,7 @@ int Element_C5::update(UPDATE_FUNC_ARGS)
 		float vy = (parts[i].tmp >> 16) / 255.0f;
 		float dx = ((parts[i].tmp2 << 16) >> 16) / 255.0f;
 		float dy = (parts[i].tmp2 >> 16) / 255.0f;
-		r = sim->create_part(-3, x, y, PT_PHOT);
+		int r = sim->create_part(-3, x, y, PT_PHOT);
 		if (r != -1)
 		{
 			parts[r].ctype = parts[i].ctype;
@@ -97,15 +96,14 @@ int Element_C5::graphics(GRAPHICS_FUNC_ARGS)
 	if(!cpart->ctype)
 		return 0;
 
-	int x = 0;
 	*colr = *colg = *colb = 0;
-	for (x=0; x<12; x++) {
-		*colr += (cpart->ctype >> (x+18)) & 1;
-		*colb += (cpart->ctype >>  x)     & 1;
+	for (int cn=0; cn<12; cn++) {
+		*colr += (cpart->ctype >> (cn+18)) & 1;
+		*colb += (cpart->ctype >>  cn)     & 1;
 	}
-	for (x=0; x<12; x++)
-		*colg += (cpart->ctype >> (x+9))  & 1;
-	x = 624/(*colr+*colg+*colb+1);
+	for (int cn=0; cn<12; cn++)
+		*colg += (cpart->ctype >> (cn+9))  & 1;
+	int x = 624/(*colr+*colg+*colb+1);
 	*colr *= x;
 	*colg *= x;
 	*colb *= x;
