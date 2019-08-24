7e3b2817b5b73147ec4b7e5dd9956b780fbfc159
moonheart08 <moonheart08@users.noreply.github.com>
2019-08-24 01:01:03 +0000

    Fix up BTRY

--- src/simulation/elements/BTRY.cpp
+++ src/simulation/elements/BTRY.cpp
@@ -47,15 +47,14 @@ Element_BTRY::Element_BTRY()
 //#TPT-Directive ElementHeader Element_BTRY static int update(UPDATE_FUNC_ARGS)
 int Element_BTRY::update(UPDATE_FUNC_ARGS)
 {
-	int r, rx, ry, rt;
-	for (rx=-2; rx<3; rx++)
-		for (ry=-2; ry<3; ry++)
+	for (int rx=-2; rx<3; rx++)
+		for (int ry=-2; ry<3; ry++)
 			if (BOUNDS_CHECK && (rx || ry) && abs(rx)+abs(ry)<4)
 			{
-				r = pmap[y+ry][x+rx];
+				int r = pmap[y+ry][x+rx];
 				if (!r)
 					continue;
-				rt = TYP(r);
+				int rt = TYP(r);
 				if (sim->parts_avg(i,ID(r),PT_INSL) != PT_INSL)
 				{
 					if ((sim->elements[rt].Properties&PROP_CONDUCTS) && !(rt==PT_WATR||rt==PT_SLTW||rt==PT_NTCT||rt==PT_PTCT||rt==PT_INWR) && parts[ID(r)].life==0)
