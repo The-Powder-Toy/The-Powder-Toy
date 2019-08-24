4f63bdc18d8a161a3cc61629e301a17bb6bec0b8
moonheart08 <moonheart08@users.noreply.github.com>
2019-08-24 00:55:38 +0000

    Fix up BOMB

--- src/simulation/elements/BOMB.cpp
+++ src/simulation/elements/BOMB.cpp
@@ -48,23 +48,21 @@ Element_BOMB::Element_BOMB()
 //#TPT-Directive ElementHeader Element_BOMB static int update(UPDATE_FUNC_ARGS)
 int Element_BOMB::update(UPDATE_FUNC_ARGS)
 {
-	int r, rx, ry, rt, nb;
-
-	for (rx=-1; rx<2; rx++)
-		for (ry=-1; ry<2; ry++)
+	for (int rx=-1; rx<2; rx++)
+		for (int ry=-1; ry<2; ry++)
 			if (BOUNDS_CHECK && (rx || ry))
 			{
-				r = pmap[y+ry][x+rx];
+				int r = pmap[y+ry][x+rx];
 				if (!r)
 					continue;
-				rt = TYP(r);
+				int rt = TYP(r);
 				if (rt!=PT_BOMB && rt!=PT_EMBR && rt!=PT_DMND && rt!=PT_CLNE && rt!=PT_PCLN && rt!=PT_BCLN && rt!=PT_VIBR)
 				{
 					int rad = 8, nt;
 					int nxi, nxj;
 					pmap[y][x] = 0;
-					for (nxj=-rad; nxj<=rad; nxj++)
-						for (nxi=-rad; nxi<=rad; nxi++)
+					for (int nxj=-rad; nxj<=rad; nxj++)
+						for (int nxi=-rad; nxi<=rad; nxi++)
 							if ((pow((float)nxi,2))/(pow((float)rad,2))+(pow((float)nxj,2))/(pow((float)rad,2))<=1)
 							{
 								int ynxj = y + nxj, xnxi = x + nxi;
@@ -72,13 +70,13 @@ int Element_BOMB::update(UPDATE_FUNC_ARGS)
 								if ((ynxj < 0) || (ynxj >= YRES) || (xnxi <= 0) || (xnxi >= XRES))
 									continue;
 
-								nt = TYP(pmap[ynxj][xnxi]);
+								int nt = TYP(pmap[ynxj][xnxi]);
 								if (nt!=PT_DMND && nt!=PT_CLNE && nt!=PT_PCLN && nt!=PT_BCLN && nt!=PT_VIBR)
 								{
 									if (nt)
 										sim->kill_part(ID(pmap[ynxj][xnxi]));
 									sim->pv[(ynxj)/CELL][(xnxi)/CELL] += 0.1f;
-									nb = sim->create_part(-3, xnxi, ynxj, PT_EMBR);
+									int nb = sim->create_part(-3, xnxi, ynxj, PT_EMBR);
 									if (nb!=-1)
 									{
 										parts[nb].tmp = 2;
@@ -87,11 +85,11 @@ int Element_BOMB::update(UPDATE_FUNC_ARGS)
 									}
 								}
 							}
-					for (nxj=-(rad+1); nxj<=(rad+1); nxj++)
-						for (nxi=-(rad+1); nxi<=(rad+1); nxi++)
+					for (int nxj=-(rad+1); nxj<=(rad+1); nxj++)
+						for (int nxi=-(rad+1); nxi<=(rad+1); nxi++)
 							if ((pow((float)nxi,2))/(pow((float)(rad+1),2))+(pow((float)nxj,2))/(pow((float)(rad+1),2))<=1 && !TYP(pmap[y+nxj][x+nxi]))
 							{
-								nb = sim->create_part(-3, x+nxi, y+nxj, PT_EMBR);
+								int nb = sim->create_part(-3, x+nxi, y+nxj, PT_EMBR);
 								if (nb!=-1)
 								{
 									parts[nb].tmp = 0;
