4648defce5f6069ddb0e00c8936017a1228fdcc0
cracker1000 <ashishpurohit271@gmail.com>
2019-08-26 23:46:32 +0530

    Update Fix col values exceeding 255 in some cases Change menu from powered to ELEC Add more realistic lith reactions LITH now turns into BRMT while slowly reacting with WATR Reacts with ACID, O2. Use variables only where needed.

--- src/simulation/elements/LITH.cpp
+++ src/simulation/elements/LITH.cpp
@@ -6,7 +6,7 @@ Element_LITH::Element_LITH()
 	Name = "LITH";
 	Colour = PIXPACK(0X707070);
 	MenuVisible = 1;
-	MenuSection = SC_POWERED;
+	MenuSection = SC_ELEC;
 	Enabled = 1;
 
 	Advection = 0.0f;
@@ -20,7 +20,7 @@ Element_LITH::Element_LITH()
 	Falldown = 0;
 
 	Flammable = 0;
-	Explosive = 10;
+	Explosive = 0;
 	Meltable = 1;
 	Hardness = 10;
 
@@ -49,7 +49,6 @@ Element_LITH::Element_LITH()
 int Element_LITH::update(UPDATE_FUNC_ARGS)
 
 {
-	int r, rx, ry, charge, chargediffuse;
 	//Prevent setting capacity below 1.
 	if (parts[i].temp <= 1.0f + 273.15f)
 		parts[i].temp = 1.0f + 273.15f;
@@ -57,7 +56,8 @@ int Element_LITH::update(UPDATE_FUNC_ARGS)
 	//Explosion code ( Gets ignored in powered mode)
 	if (parts[i].tmp > parts[i].temp - 272.0f && parts[i].tmp2 != 1)
 	{
-		parts[i].type = PT_BOMB;
+		parts[i].type = PT_FIRE;
+		parts[i].life = 50;
 	}
 	//Powered battery mode. (.tmp2 = 1 turns it into never discharging powered battery.)
 	if (parts[i].tmp2 == 1)
@@ -73,11 +73,11 @@ int Element_LITH::update(UPDATE_FUNC_ARGS)
 
 	else
 	{
-		for (rx = -2; rx < 3; rx++)
-			for (ry = -2; ry < 3; ry++)
+		for (int rx = -2; rx < 3; rx++)
+			for (int ry = -2; ry < 3; ry++)
 				if (BOUNDS_CHECK && (rx || ry))
 				{
-					r = pmap[y + ry][x + rx];
+					int r = pmap[y + ry][x + rx];
 					if (!r || sim->parts_avg(ID(r), i, PT_INSL) == PT_INSL)
 						continue;
 					if (TYP(r) == PT_LITH)
@@ -89,11 +89,11 @@ int Element_LITH::update(UPDATE_FUNC_ARGS)
 					}
 				}
 	}
-	for (rx = -2; rx < 3; rx++)
-		for (ry = -2; ry < 3; ry++)
+	for (int rx = -2; rx < 3; rx++)
+		for (int ry = -2; ry < 3; ry++)
 			if (BOUNDS_CHECK && (rx || ry))
 			{
-				r = pmap[y + ry][x + rx];
+				int r = pmap[y + ry][x + rx];
 				if (!r || sim->parts_avg(ID(r), i, PT_INSL) == PT_INSL)
 					continue;
 				//Battery discharging.
@@ -104,7 +104,7 @@ int Element_LITH::update(UPDATE_FUNC_ARGS)
 					{
 						parts[i].tmp -= 1;
 						sim->FloodINST(x + rx, y + ry, PT_SPRK, PT_INST);
-							
+
 					}
 					break;
 					//Various reactions with different kinds of water elements. Slowly reacts with water and releases H2 gas.
@@ -113,27 +113,51 @@ int Element_LITH::update(UPDATE_FUNC_ARGS)
 				case PT_SLTW:
 				case PT_CBNW:
 				case PT_DSTW:
-					if (RNG::Ref().chance(1, 140))
-					{
-						sim->part_change_type(i, x, y, PT_H2);
+				{  if (RNG::Ref().chance(1, 30))
+				{
+					parts[i].type = PT_H2;
+					parts[ID(r)].type = PT_BRMT;
+					sim->pv[(y / CELL) + ry][(x / CELL) + rx] += 1.0;
+					if (parts[ID(r)].tmp >= 22)
 						parts[i].temp += parts[ID(r)].tmp;
+				}
+				}
+				break;
+				case PT_O2:
+				{
+					parts[i].type = PT_PLSM;
+					parts[i].life = 20;
+					sim->pv[(y / CELL) + ry][(x / CELL) + rx] += 4.0;
+				}
+				break;
+				case PT_ACID:
+				{
+
+					parts[i].type = PT_H2;
+					sim->pv[(y / CELL) + ry][(x / CELL) + rx] += 0.5;
+					if (RNG::Ref().chance(1, 800))
+					{
+
+						parts[i].type = PT_FIRE;
+						parts[i].life = 100;
 					}
-					break;
+				}
+				break;
 				}
 			}
 	//Diffusion of tmp i.e stored charge.
-	for (chargediffuse = 0; chargediffuse < 8; chargediffuse++)
+	for (int chargediffuse = 0; chargediffuse < 8; chargediffuse++)
 	{
-		rx = RNG::Ref().between(-2, 2);
-		ry = RNG::Ref().between(-2, 2);
+		int rx = RNG::Ref().between(-2, 2);
+		int ry = RNG::Ref().between(-2, 2);
 		if (BOUNDS_CHECK && (rx || ry))
 		{
-			r = pmap[y + ry][x + rx];
+			int r = pmap[y + ry][x + rx];
 			if (!r || sim->parts_avg(ID(r), i, PT_INSL) == PT_INSL)
 				continue;
-			if (TYP(r) == PT_LITH && (parts[i].tmp > parts[ID(r)].tmp) && parts[i].tmp >0)//diffusion
+			if (TYP(r) == PT_LITH && (parts[i].tmp > parts[ID(r)].tmp) && parts[i].tmp > 0)//diffusion
 			{
-				charge = parts[i].tmp - parts[ID(r)].tmp;
+				int charge = parts[i].tmp - parts[ID(r)].tmp;
 				if (charge == 1)
 				{
 					parts[ID(r)].tmp++;
@@ -171,18 +195,22 @@ int Element_LITH::graphics(GRAPHICS_FUNC_ARGS)
 			*colb -= chargingstate;
 
 		}
-		if ( cpart->tmp < 1 && cpart->life == 0)
+		if (cpart->tmp < 1 && cpart->life == 0)
 		{
 			*colg = 1;
-			*colr = 255;
+			*colr = 220;
 			*colb = 0;
-		
+
 		}
 	}
 	//Powered battery mode, can also be used to charge the battery.
 	if (cpart->tmp2 == 1)
 	{
 		*colb = 255;
+	}
+	if (*colg > 255)
+	{
+		*colg = 255;
 	}
 	return 0;
 }
