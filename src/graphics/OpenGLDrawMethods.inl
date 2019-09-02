The-Powder-Toy/The-Powder-Toy

commit ff27d694242c2de2d85ccfbed953186bb38a0628

Switch from std::string to String/ByteString in most of the code
Also switch SimulationData from weird arrays to std::vector

src/graphics/OpenGLDrawMethods.inl
@@ -1,7 +1,7 @@
 #include "../data/font.h"
 #include <cmath>
 
-int PIXELMETHODS_CLASS::drawtext_outline(int x, int y, const char *s, int r, int g, int b, int a)
+int PIXELMETHODS_CLASS::drawtext_outline(int x, int y, String s, int r, int g, int b, int a)
 {
 	drawtext(x-1, y-1, s, 0, 0, 0, 120);
 	drawtext(x+1, y+1, s, 0, 0, 0, 120);
@@ -12,7 +12,7 @@ int PIXELMETHODS_CLASS::drawtext_outline(int x, int y, const char *s, int r, int
 	return drawtext(x, y, s, r, g, b, a);
 }
 
-int PIXELMETHODS_CLASS::drawtext(int x, int y, const char *s, int r, int g, int b, int a)
+int PIXELMETHODS_CLASS::drawtext(int x, int y, String str, int r, int g, int b, int a)
 {
 	bool invert = false;
 	if(!strlen(s))
@@ -23,6 +23,7 @@ int PIXELMETHODS_CLASS::drawtext(int x, int y, const char *s, int r, int g, int 
 	VideoBuffer texture(width, height);
 	int characterX = 0, characterY = 0;
 	int startX = characterX;
+	String::value_type *s = str.c_str();
 	for (; *s; s++)
 	{
 		if (*s == '\n')
@@ -98,7 +99,7 @@ int PIXELMETHODS_CLASS::drawtext(int x, int y, const char *s, int r, int g, int 
 		}
 		else
 		{
-			characterX = texture.SetCharacter(characterX, characterY, *(unsigned char *)s, r, g, b, a);
+			characterX = texture.SetCharacter(characterX, characterY, *s, r, g, b, a);
 		}
 	}
 	glEnable(GL_TEXTURE_2D);
@@ -128,12 +129,7 @@ int PIXELMETHODS_CLASS::drawtext(int x, int y, const char *s, int r, int g, int 
 	return x;
 }
 
-int PIXELMETHODS_CLASS::drawtext(int x, int y, std::string s, int r, int g, int b, int a)
-{
-	return drawtext(x, y, s.c_str(), r, g, b, a);
-}
-
-int PIXELMETHODS_CLASS::drawchar(int x, int y, int c, int r, int g, int b, int a)
+int PIXELMETHODS_CLASS::drawchar(int x, int y, String::value_type c, int r, int g, int b, int a)
 {
 	unsigned char *rp = font_data + font_ptrs[c];
 	int w = *(rp++);
@@ -162,7 +158,7 @@ int PIXELMETHODS_CLASS::drawchar(int x, int y, int c, int r, int g, int b, int a
 	return x + w;
 }
 
-int PIXELMETHODS_CLASS::addchar(int x, int y, int c, int r, int g, int b, int a)
+int PIXELMETHODS_CLASS::addchar(int x, int y, String::value_type c, int r, int g, int b, int a)
 {
 	unsigned char *rp = font_data + font_ptrs[c];
 	int w = *(rp++);

commit 1ed17f7f190c3a523c8f2027c1f70624e47aff8c

graphics/gldrawmethods: Fix compilation
Was probably broken by https://github.com/The-Powder-Toy/The-Powder-Toy/commit/ff27d694242c2de2d85ccfbed953186bb38a0628

Signed-off-by: Nick Renieris <velocityra@gmail.com>

src/graphics/OpenGLDrawMethods.inl
@@ -1,5 +1,6 @@
 #include "../data/font.h"
 #include <cmath>
+#include <cstring>
 
 int PIXELMETHODS_CLASS::drawtext_outline(int x, int y, String s, int r, int g, int b, int a)
 {
@@ -15,15 +16,15 @@ int PIXELMETHODS_CLASS::drawtext_outline(int x, int y, String s, int r, int g, i
 int PIXELMETHODS_CLASS::drawtext(int x, int y, String str, int r, int g, int b, int a)
 {
 	bool invert = false;
-	if(!strlen(s))
+	if(str.size() == 0)
 		return 0;
 	int oR = r, oG = g, oB = b;
 	int width, height;
-	Graphics::textsize(s, width, height);
+	Graphics::textsize(str, width, height);
 	VideoBuffer texture(width, height);
 	int characterX = 0, characterY = 0;
 	int startX = characterX;
-	String::value_type *s = str.c_str();
+	const String::value_type *s = str.c_str();
 	for (; *s; s++)
 	{
 		if (*s == '\n')
@@ -131,7 +132,7 @@ int PIXELMETHODS_CLASS::drawtext(int x, int y, String str, int r, int g, int b, 
 
 int PIXELMETHODS_CLASS::drawchar(int x, int y, String::value_type c, int r, int g, int b, int a)
 {
-	unsigned char *rp = font_data + font_ptrs[c];
+	const unsigned char *rp = font_data + font_ptrs[c];
 	int w = *(rp++);
 	VideoBuffer texture(w, FONT_H);
 	texture.SetCharacter(0, 0, c, r, g, b, a);
@@ -160,7 +161,7 @@ int PIXELMETHODS_CLASS::drawchar(int x, int y, String::value_type c, int r, int 
 
 int PIXELMETHODS_CLASS::addchar(int x, int y, String::value_type c, int r, int g, int b, int a)
 {
-	unsigned char *rp = font_data + font_ptrs[c];
+	const unsigned char *rp = font_data + font_ptrs[c];
 	int w = *(rp++);
 	VideoBuffer texture(w, FONT_H);
 	texture.AddCharacter(0, 0, c, r, g, b, a);