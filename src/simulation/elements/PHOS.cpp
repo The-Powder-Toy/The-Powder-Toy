e319fd4cf63167dbd754f4da4b5f3fc8143d6e78
cracker1000 <ashishpurohit271@gmail.com>
2020-04-17 21:05:18 +0530

    Add new element PHOS.

---
+++ src/simulation/elements/PHOS.cpp
@@ -0,0 +1,97 @@
+#include "simulation/ElementCommon.h"
+static int update(UPDATE_FUNC_ARGS);
+static int graphics(GRAPHICS_FUNC_ARGS);
+
+void Element::Element_PHOS()
+{
+	Identifier = "DEFAULT_PT_PHOS";
+	Name = "PHOS";
+	Colour = PIXPACK(0xFFFFFF);
+	MenuVisible = 1;
+	MenuSection = SC_POWDERS;
+	Enabled = 1;
+
+	Advection = 0.4f;
+	AirDrag = 0.04f * CFDS;
+	AirLoss = 0.94f;
+	Loss = 0.95f;
+	Collision = -0.1f;
+	Gravity = 0.3f;
+	Diffusion = 0.00f;
+	HotAir = 0.000f	* CFDS;
+	Falldown = 1;
+
+	Flammable = 0;
+	Explosive = 1;
+	Meltable = 5;
+	Hardness = 1;
+
+	Weight = 75;
+
+	HeatConduct = 110;
+	Description = "Phosphorus, slowly turns red, melts at 45C and reacts with O2.";
+
+	Properties = TYPE_PART;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = IPH;
+	HighPressureTransition = NT;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = 317.0f;
+	HighTemperatureTransition = PT_LAVA;
+
+	Update = &update;
+	Graphics = &graphics;
+}
+
+static int update(UPDATE_FUNC_ARGS)
+{
+	if (parts[i].tmp < 555 && (RNG::Ref().chance(1, 5)))
+	{
+		parts[i].tmp++;
+	}
+
+	int r, rx, ry, np, rndstore;
+	for (rx = -1; rx < 2; rx++)
+		for (ry = -1; ry < 2; ry++)
+			if (BOUNDS_CHECK && (rx || ry))
+			{
+				r = pmap[y + ry][x + rx];
+				switch (TYP(r))
+				{
+				case PT_O2:
+				{
+					parts[i].life = 200;
+					parts[i].type = PT_PLSM;
+				}
+				case PT_PLSM:
+				{
+					parts[i].life = 200;
+					parts[i].type = PT_PLSM;
+				}
+				break;
+
+				}
+			}
+	return 0;
+}
+
+static int graphics(GRAPHICS_FUNC_ARGS)
+{
+	*colr += cpart->tmp;
+	*colg -= cpart->tmp;
+	*colb -= cpart->tmp;
+
+	if (*colr > 155)
+	{
+		*colr = 155;
+	}
+
+	if (cpart->tmp <= 100)
+	{
+		*pixel_mode |= PMODE_FLARE;
+	}
+	return 0;
+}
