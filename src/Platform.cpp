The-Powder-Toy/The-Powder-Toy

commit 3056b86780fd793ec101d20e6f30b84402543d14

Fix hang on exit when using platform.restart

src/Platform.cpp
@@ -19,6 +19,7 @@
 #endif
 
 #include "Misc.h"
+#include "client/Client.h"
 
 namespace Platform
 {
@@ -80,10 +81,25 @@ void DoRestart()
 	if (exename.length())
 	{
 #ifdef WIN
-		ShellExecute(NULL, "open", exename.c_str(), NULL, NULL, SW_SHOWNORMAL);
+		int ret = (int)ShellExecute(NULL, NULL, exename.c_str(), NULL, NULL, SW_SHOWNORMAL);
+		if (ret <= 32)
+		{
+			fprintf(stderr, "cannot restart: ShellExecute(...) failed: code %i\n", ret);
+		}
+		else
+		{
+			Client::Ref().Shutdown(); // very ugly hack; will fix soon(tm)
+			exit(0);
+		}
 #elif defined(LIN) || defined(MACOSX)
 		execl(exename.c_str(), "powder", NULL);
+		int ret = errno;
+		fprintf(stderr, "cannot restart: execl(...) failed: code %i\n", ret);
 #endif
+	}
+	else
+	{
+		fprintf(stderr, "cannot restart: no executable name???\n");
 	}
 	exit(-1);
 }

meson.build
@@ -135,6 +135,9 @@ if copt_msvc
 		args_msvc_opt = [ '/Oy-', '/fp:fast' ]
 		project_c_args += args_msvc_opt
 		project_cpp_args += args_msvc_opt
+		project_link_args += [
+			'/NODEFAULTLIB:LIBCMT',
+		]
 	endif
 else
 	if copt_platform == 'mac'

commit 413c18950db1c3cf3ebf4904dabcb5cd119703c5

Fix font and render targets broken by 3056b86

src/Platform.cpp
@@ -88,7 +88,9 @@ void DoRestart()
 		}
 		else
 		{
+#if !defined(RENDERER) && !defined(FONTEDITOR)
 			Client::Ref().Shutdown(); // very ugly hack; will fix soon(tm)
+#endif
 			exit(0);
 		}
 #elif defined(LIN) || defined(MACOSX)