5ae19d006ce8ce886ce7ce7a210437eb4da6b646
RCAProduction <RCAProductionMaster@gmail.com>
2021-03-13 20:08:47 -0800

    Added new variable pressure transition (required update func). Added Sulfide variants of ROCK with tmp 1 and tmp 2. Added graphics for sulfides.

--- src/simulation/elements/ROCK.cpp
+++ src/simulation/elements/ROCK.cpp
@@ -1,6 +1,7 @@
 #include "simulation/ElementCommon.h"
 
 static int graphics(GRAPHICS_FUNC_ARGS);
+static int update(UPDATE_FUNC_ARGS);
 static void create(ELEMENT_CREATE_FUNC_ARGS);
 
 void Element::Element_ROCK()
@@ -36,17 +37,61 @@ void Element::Element_ROCK()
 
 	LowPressure = IPL;
 	LowPressureTransition = NT;
-	HighPressure = 120;
-	HighPressureTransition = PT_STNE;
+	HighPressure = IPH;
+	HighPressureTransition = NT;
 	LowTemperature = ITL;
 	LowTemperatureTransition = NT;
-	HighTemperature = 1943.15f;
-	HighTemperatureTransition = PT_LAVA;
+	HighTemperature = ITH;
+	HighTemperatureTransition = ST;
 
+	Update = &update;
 	Graphics = &graphics;
 	Create = &create;
 }
 
+static int update(UPDATE_FUNC_ARGS)
+{
+	int r, rx, ry, rt, t = parts[i].type;
+	parts[i].pavg[0] = parts[i].pavg[1];
+	parts[i].pavg[1] = sim->pv[y / CELL][x / CELL];
+	float diff = parts[i].pavg[1] - parts[i].pavg[0];
+	if (parts[i].pavg[1] >= 50.00f && (diff > 10.00f || diff < -10.00f))
+	{
+		if (parts[i].tmp==1 && RNG::Ref().chance(1, 500)) //1 in 500 sulfides will produce GOLD
+			sim->part_change_type(i, x, y, PT_GOLD);
+		else if (parts[i].tmp == 2 && RNG::Ref().chance(1, 10)) //1 in 10 roasted sulfides will produce GOLD
+			sim->part_change_type(i, x, y, PT_GOLD);
+		else
+			sim->part_change_type(i, x, y, PT_STNE);
+	}
+
+	if (parts[i].tmp == 1 && parts[i].temp >= 873 && RNG::Ref().chance(1, 5000)) //Allow sulfides to burn off at temperature, changing chemistry and releasing CAUS
+	{
+		if (RNG::Ref().chance(1, 15))
+		{
+			rx = RNG::Ref().chance(1, 2) - 1;
+			ry = RNG::Ref().chance(1, 2) - 1;
+			sim->create_part(-1, x + rx, y + ry, PT_CAUS);
+		}
+		rx = RNG::Ref().chance(1, 2) - 1;
+		ry = RNG::Ref().chance(1, 2) - 1;
+		sim->create_part(-1, x + rx, y + ry, PT_SMKE);
+		parts[i].tmp = 2;
+	}
+
+	if (parts[i].temp >= 1943.15 && parts[i].tmp == 0)
+	{
+		parts[i].type = PT_LAVA;
+		parts[i].ctype = PT_ROCK;
+	}
+	else if (parts[i].temp >= 1153.15 && (parts[i].tmp == 1 || parts[i].tmp == 2))
+	{
+		parts[i].type = PT_LAVA;
+		parts[i].ctype = PT_ROCK;
+		parts[i].tmp = 1;
+	}
+	return 0;
+}
 
 static int graphics(GRAPHICS_FUNC_ARGS)
 {
@@ -63,6 +108,18 @@ static int graphics(GRAPHICS_FUNC_ARGS)
 		*firer = *colr;
 		*fireg = *colg;
 		*fireb = *colb;
+	}
+	if (cpart->tmp == 1) // Sulfide Color
+	{
+		*colr += 50;
+		*colg += 30;
+		*colb += -30;
+	}
+	if (cpart->tmp == 2) // Roasted Sulfide Color
+	{
+		*colr += 25;
+		*colg += 15;
+		*colb += -15;
 	}
 	return 0;
 }
