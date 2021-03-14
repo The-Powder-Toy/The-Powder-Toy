904edc97cf738d1521f4b31a871b34f144b80f82
RCAProduction <RCAProductionMaster@gmail.com>
2021-03-14 16:37:54 -0700

    Add graphics func to STNE for sulfides. Add update func to STNE (uses ROCK update func for melting).

--- src/simulation/elements/STNE.cpp
+++ src/simulation/elements/STNE.cpp
@@ -1,5 +1,9 @@
 #include "simulation/ElementCommon.h"
 
+static int graphics(GRAPHICS_FUNC_ARGS);
+int Element_ROCK_update(UPDATE_FUNC_ARGS);
+static void create(ELEMENT_CREATE_FUNC_ARGS);
+
 void Element::Element_STNE()
 {
 	Identifier = "DEFAULT_PT_STNE";
@@ -37,6 +41,34 @@ void Element::Element_STNE()
 	HighPressureTransition = NT;
 	LowTemperature = ITL;
 	LowTemperatureTransition = NT;
-	HighTemperature = 983.0f;
-	HighTemperatureTransition = PT_LAVA;
+	HighTemperature = ITH;
+	HighTemperatureTransition = ST;
+
+	Graphics = &graphics;
+	Update = &Element_ROCK_update;
+	Create = &create;
+}
+
+static int graphics(GRAPHICS_FUNC_ARGS)
+{
+	int z = (cpart->tmp2 - 7) * 6; // Randomized color noise based on tmp2 for sulfides only
+
+	if (cpart->tmp == 1) // Sulfide Color
+	{
+		*colr = 255;
+		*colg = 208 + z + 30;
+		*colb = 144 - z - 30;
+	}
+	if (cpart->tmp == 2) // Roasted Sulfide Color
+	{
+		*colr = 255 - z - 25;
+		*colg = 208 - z - 15;
+		*colb = 144 - z - 30;
+	}
+	return 0;
+}
+
+static void create(ELEMENT_CREATE_FUNC_ARGS)
+{
+	sim->parts[i].tmp2 = RNG::Ref().between(0, 10);
 }
