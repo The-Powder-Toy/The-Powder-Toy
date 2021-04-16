b94e26d6f0abb0cea1ebc043fe1b70cf7a6552a3
Tamás Bálint Misius <lbphacker@gmail.com>
2021-04-16 17:54:25 +0200

    Disconnect version numbers from the content of v* tags

--- .github/get-type.py
+++ .github/get-type.py
@@ -9,7 +9,7 @@ match_snapshot = re.match(r'refs/tags/snapshot-([0-9]+)', ref)
 if match_stable:
 	print('::set-output name=TYPE::stable')
 	print('::set-output name=NAME::v%s.%s.%s' % (match_stable.group(1), match_stable.group(2), match_stable.group(3)))
-	print('::set-output name=RELTYPECFG::%s' % base64.b64encode(('-Dignore_updates=false\t-Dversion_major=%s\t-Dversion_minor=%s\t-Dversion_build=%s' % (match_stable.group(1), match_stable.group(2), match_stable.group(3))).encode('utf-8')).decode('utf-8'))
+	print('::set-output name=RELTYPECFG::%s' % base64.b64encode(('-Dignore_updates=false').encode('utf-8')).decode('utf-8'))
 elif match_snapshot:
 	print('::set-output name=TYPE::snapshot')
 	print('::set-output name=NAME::snapshot-%s' % match_snapshot.group(1))
