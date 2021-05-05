f33f82c2c183ca5d6651bef617d35502d8632e51
Bitl <bitlblox@gmail.com>
2021-05-04 18:44:21 -0700

    fix various bugs

--- src/simulation/elements/FIGH.cpp
+++ src/simulation/elements/FIGH.cpp
@@ -96,11 +96,11 @@ static int update(UPDATE_FUNC_ARGS)
 		tary = (int)sim->player.legs[3];
 		parts[i].tmp2 = 1;
 	}
-	else
+	else if (sim->fighcount > 1)
 	{
 		playerst fighRand[MAX_FIGHTERS];
 		std::copy(std::begin(sim->fighters), std::end(sim->fighters), std::begin(fighRand));
-		std::random_shuffle(&fighRand[0], &fighRand[sim->fighcount - 1]);
+		std::random_shuffle(&fighRand[0], &fighRand[sim->fighcount]);
 		if (&fighRand[0] != figh && ((pow((float)figh->legs[2] - x, 2) + pow((float)figh->legs[3] - y, 2)) <=
 			pow((float)fighRand[0].legs[2] - x, 2) + pow((float)fighRand[0].legs[3] - y, 2)))
 		{
