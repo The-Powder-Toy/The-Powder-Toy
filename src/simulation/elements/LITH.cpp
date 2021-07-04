98257ba92e484cb16ae75a985d896c1675dd0aa6
moonheart08 <moonheart08@users.noreply.github.com>
2021-07-04 03:42:39 -0500

    color adjust

--- src/simulation/elements/LITH.cpp
+++ src/simulation/elements/LITH.cpp
@@ -7,7 +7,7 @@ void Element::Element_LITH()
 {
 	Identifier = "DEFAULT_PT_LITH";
 	Name = "LITH";
-	Colour = PIXPACK(0xC5C5BB);
+	Colour = PIXPACK(0xb6aabf);
 	MenuVisible = 1;
 	MenuSection = SC_EXPLOSIVE;
 	Enabled = 1;
