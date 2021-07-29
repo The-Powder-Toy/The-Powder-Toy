2dc95e461e48625d330678558b2701c1abcd89d7
Tamás Bálint Misius <lbphacker@gmail.com>
2021-07-29 17:43:26 +0200

    Pre-merge cleanup

--- src/simulation/simtools/AMBP.cpp
+++ src/simulation/simtools/AMBP.cpp
@@ -2,22 +2,24 @@
 
 static int perform(Simulation * sim, Particle * cpart, int x, int y, int brushX, int brushY, float strength);
 
-void SimTool::Tool_AMBP() {
-    Identifier = "DEFAULT_TOOL_AMBP";
-    Name = "AMB+";
-    Colour = PIXPACK(0xFFDD00);
-    Description = "Air heater. (Ambient heat required).";
-    Perform = &perform;
+void SimTool::Tool_AMBP()
+{
+	Identifier = "DEFAULT_TOOL_AMBP";
+	Name = "AMBP";
+	Colour = PIXPACK(0xFFDD00);
+	Description = "Increases ambient air temperature.";
+	Perform = &perform;
 }
 
-static int perform(Simulation *sim, Particle *cpart, int x, int y, int brushX, int brushY, float strength) {
-    if (!sim->aheat_enable)
-        return 0;
-    
-    sim->hv[y / CELL][x / CELL] += strength * 2.0f;
-    if (sim->hv[y / CELL][x / CELL] > MAX_TEMP)
-        sim->hv[y / CELL][x / CELL] = MAX_TEMP;
-    else if (sim->hv[y / CELL][x / CELL] < 0)
-        sim->hv[y / CELL][x / CELL] = 0;
-    return 1;
+static int perform(Simulation *sim, Particle *cpart, int x, int y, int brushX, int brushY, float strength)
+{
+	if (!sim->aheat_enable)
+	{
+		return 0;
+	}
+
+	sim->hv[y / CELL][x / CELL] += strength * 2.0f;
+	if (sim->hv[y / CELL][x / CELL] > MAX_TEMP) sim->hv[y / CELL][x / CELL] = MAX_TEMP;
+	if (sim->hv[y / CELL][x / CELL] < MIN_TEMP) sim->hv[y / CELL][x / CELL] = MIN_TEMP;
+	return 1;
 }
