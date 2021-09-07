55f3684f33e84ebb6d795c6701573556a7bbd65d
4D4850 <84929757+4D4850@users.noreply.github.com>
2021-09-07 18:31:28 -0500

    Update GameView.cpp

--- src/gui/game/GameView.cpp
+++ src/gui/game/GameView.cpp
@@ -2150,7 +2150,7 @@ void GameView::OnDraw()
 				else if (c->IsBio(type))
 				{
 					sampleInfo << c->ElementResolve(type, 0) << ", O2: " << sample.particle.bio.o2 << ", CO2: " << sample.particle.bio.co2 << ", Health: " << sample.particle.bio.health;
-					sampleInfo << c->ElementResolve(type, 0)  "Max Health: " << sample.particle.bio.maxHealth << ", Radiation Damage: " << sample.particle.bio.radDamage;
+					sampleInfo << c->ElementResolve(type, 0) << "Max Health: " << sample.particle.bio.maxHealth << ", Radiation Damage: " << sample.particle.bio.radDamage;
 				}
 				else if ((type == PT_PIPE || type == PT_PPIP) && c->IsValidElement(ctype))
 				{
