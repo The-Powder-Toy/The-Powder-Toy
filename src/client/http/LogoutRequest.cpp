99566c800304db3a7e74672f0f3fedf4b4691e22
jacob1 <jfu614@gmail.com>
2024-01-06 23:31:18 -0500

    LogoutRequest - set checkStatus to true

--- src/client/http/LogoutRequest.cpp
+++ src/client/http/LogoutRequest.cpp
@@ -5,7 +5,7 @@
 namespace http
 {
 	LogoutRequest::LogoutRequest() :
-		APIRequest(ByteString::Build(SCHEME, SERVER, "/Logout.json?Key=" + Client::Ref().GetAuthUser().SessionKey), authRequire, false)
+		APIRequest(ByteString::Build(SCHEME, SERVER, "/Logout.json?Key=" + Client::Ref().GetAuthUser().SessionKey), authRequire, true)
 	{
 	}
 
