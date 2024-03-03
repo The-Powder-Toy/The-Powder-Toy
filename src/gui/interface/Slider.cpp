The-Powder-Toy/The-Powder-Toy

commit 69e0a8b0aaa1f78d6ce3e37e7f54f5a3b4c697fe

Clean up OnMouseClick/Unclick madness
This is a prerequisite for making ScrollPanel work nicely on touch screens.

Engine used the terms MouseClick and MouseUnclick to refer to events that are traditionally called MouseDown and MouseUp, this was fixed with simple renaming.

Component and friends similarly used the terms MouseClick and MouseUnclick to refer to events that are traditionally called MouseDown and MouseUp and, succumbing to their own confusing terminology, also implemented behaviours associated with both the actual events MouseDown and MouseClick in code that was responsible for handling only the actual event MouseDown (i.e. what they called MouseClick).

This had been overlooked for a long time because nobody cares that a checkbox changes state when the mouse button is pressed on it rather than when it is released.

The fix is to migrate many pieces of code that run in response to MouseDown events, such as checkbox state change code, to MouseClick events, and to redefine a MouseClick to mean a sequence of MouseDown and MouseUp inside the component, rather than just a MouseDown. This is complicated by the fact that MouseClick events report mouse coordinates relative to the top left corner of the component, while MouseDown events report them relative to the top left corner of the container of the component.

Other pieces of code that make sense to be run in response to MouseDown events, such as label selection code, were left alone.

src/gui/interface/Slider.cpp
@@ -48,10 +48,10 @@ void Slider::OnMouseMoved(int x, int y, int dx, int dy)
 	}
 }
 
-void Slider::OnMouseClick(int x, int y, unsigned button)
+void Slider::OnMouseDown(int x, int y, unsigned button)
 {
 	isMouseDown = true;
-	updatePosition(x);
+	updatePosition(x - Position.X);
 }
 
 void Slider::OnMouseUp(int x, int y, unsigned button)

src/PowderToySDL.cpp
@@ -349,7 +349,7 @@ static void EventProcess(const SDL_Event &event)
 			mousey = event.button.y;
 		}
 		mouseButton = event.button.button;
-		engine.onMouseClick(mousex, mousey, mouseButton);
+		engine.onMouseDown(mousex, mousey, mouseButton);
 
 		mouseDown = true;
 		if constexpr (!DEBUG)
@@ -365,7 +365,7 @@ static void EventProcess(const SDL_Event &event)
 			mousey = event.button.y;
 		}
 		mouseButton = event.button.button;
-		engine.onMouseUnclick(mousex, mousey, mouseButton);
+		engine.onMouseUp(mousex, mousey, mouseButton);
 
 		mouseDown = false;
 		if constexpr (!DEBUG)

src/gui/game/GameView.cpp
@@ -87,7 +87,7 @@ public:
 			}
 		}
 	}
-	void OnMouseUnclick(int x, int y, unsigned int button) override
+	void OnMouseClick(int x, int y, unsigned int button) override
 	{
 		if(isButtonDown)
 		{
@@ -96,7 +96,7 @@ public:
 			else if(rightDown)
 				DoRightAction();
 		}
-		ui::Button::OnMouseUnclick(x, y, button);
+		ui::Button::OnMouseClick(x, y, button);
 
 	}
 	void OnMouseHover(int x, int y) override
@@ -120,15 +120,18 @@ public:
 		toolTip = newToolTip1;
 		toolTip2 = newToolTip2;
 	}
-	void OnMouseClick(int x, int y, unsigned int button) override
+	void OnMouseDown(int x, int y, unsigned int button) override
 	{
-		ui::Button::OnMouseClick(x, y, button);
-		rightDown = false;
-		leftDown = false;
-		if(x >= splitPosition)
-			rightDown = true;
-		else if(x < splitPosition)
-			leftDown = true;
+		ui::Button::OnMouseDown(x, y, button);
+		if (MouseDownInside)
+		{
+			rightDown = false;
+			leftDown = false;
+			if(x - Position.X >= splitPosition)
+				rightDown = true;
+			else if(x - Position.X < splitPosition)
+				leftDown = true;
+		}
 	}
 	void DoRightAction()
 	{

src/gui/game/ToolButton.cpp
@@ -16,12 +16,15 @@ ToolButton::ToolButton(ui::Point position, ui::Point size, String text, ByteStri
 	Component::TextPosition(buttonDisplayText);
 }
 
-void ToolButton::OnMouseClick(int x, int y, unsigned int button)
+void ToolButton::OnMouseDown(int x, int y, unsigned int button)
 {
-	isButtonDown = true;
+	if (MouseDownInside)
+	{
+		isButtonDown = true;
+	}
 }
 
-void ToolButton::OnMouseUnclick(int x, int y, unsigned int button)
+void ToolButton::OnMouseClick(int x, int y, unsigned int button)
 {
 	if(isButtonDown)
 	{

src/gui/game/ToolButton.h
@@ -9,7 +9,7 @@ class ToolButton: public ui::Button
 	ByteString toolIdentifier;
 public:
 	ToolButton(ui::Point position, ui::Point size, String text, ByteString toolIdentifier, String toolTip = String());
-	void OnMouseUnclick(int x, int y, unsigned int button) override;
+	void OnMouseDown(int x, int y, unsigned int button) override;
 	void OnMouseUp(int x, int y, unsigned int button) override;
 	void OnMouseClick(int x, int y, unsigned int button) override;
 	void Draw(const ui::Point& screenPos) override;

src/gui/interface/AvatarButton.cpp
@@ -51,7 +51,7 @@ void AvatarButton::Draw(const Point& screenPos)
 	}
 }
 
-void AvatarButton::OnMouseUnclick(int x, int y, unsigned int button)
+void AvatarButton::OnMouseClick(int x, int y, unsigned int button)
 {
 	if(button != 1)
 	{
@@ -70,16 +70,19 @@ void AvatarButton::OnContextMenuAction(int item)
 	//Do nothing
 }
 
-void AvatarButton::OnMouseClick(int x, int y, unsigned int button)
+void AvatarButton::OnMouseDown(int x, int y, unsigned int button)
 {
-	if(button == SDL_BUTTON_RIGHT)
+	if (MouseDownInside)
 	{
-		if(menu)
-			menu->Show(GetScreenPos() + ui::Point(x, y));
-	}
-	else
-	{
-		isButtonDown = true;
+		if(button == SDL_BUTTON_RIGHT)
+		{
+			if(menu)
+				menu->Show(GetContainerPos() + ui::Point(x, y));
+		}
+		else
+		{
+			isButtonDown = true;
+		}
 	}
 }
 

src/gui/interface/AvatarButton.h
@@ -30,7 +30,7 @@ public:
 	virtual ~AvatarButton() = default;
 
 	void OnMouseClick(int x, int y, unsigned int button) override;
-	void OnMouseUnclick(int x, int y, unsigned int button) override;
+	void OnMouseDown(int x, int y, unsigned int button) override;
 
 	void OnMouseEnter(int x, int y) override;
 	void OnMouseLeave(int x, int y) override;

src/gui/interface/Button.cpp
@@ -86,7 +86,7 @@ void Button::Draw(const Point& screenPos)
 
 	if (Enabled)
 	{
-		if (isButtonDown || (isTogglable && toggle))
+		if ((isButtonDown && MouseDownInside) || (isTogglable && toggle))
 		{
 			textColour = Appearance.TextActive;
 			borderColour = Appearance.BorderActive;
@@ -140,7 +140,7 @@ void Button::Draw(const Point& screenPos)
 	}
 }
 
-void Button::OnMouseUnclick(int x, int y, unsigned int button)
+void Button::OnMouseClick(int x, int y, unsigned int button)
 {
 	if(button == 1)
 	{
@@ -171,17 +171,20 @@ void Button::OnMouseUp(int x, int y, unsigned int button)
 	isAltButtonDown = false;
 }
 
-void Button::OnMouseClick(int x, int y, unsigned int button)
+void Button::OnMouseDown(int x, int y, unsigned int button)
 {
-	if(!Enabled)
-		return;
-	if(button == 1)
+	if (MouseDownInside)
 	{
-		isButtonDown = true;
-	}
-	else if(button == 3)
-	{
-		isAltButtonDown = true;
+		if(!Enabled)
+			return;
+		if(button == 1)
+		{
+			isButtonDown = true;
+		}
+		else if(button == 3)
+		{
+			isAltButtonDown = true;
+		}
 	}
 }
 

commit ab28f93753e36b84d94f1583543d4ec772928f91

Fix sliders accepting clicks from anywhere
Broken by 69e0a8b0aaa1 where I added an extra MouseDownInside check to the OnMouseDown (used to be OnMouseClick) of every component except that of sliders.

src/gui/interface/Slider.cpp
@@ -50,8 +50,11 @@ void Slider::OnMouseMoved(int x, int y)
 
 void Slider::OnMouseDown(int x, int y, unsigned button)
 {
-	isMouseDown = true;
-	updatePosition(x - Position.X);
+	if (MouseDownInside)
+	{
+		isMouseDown = true;
+		updatePosition(x - Position.X);
+	}
 }
 
 void Slider::OnMouseUp(int x, int y, unsigned button)