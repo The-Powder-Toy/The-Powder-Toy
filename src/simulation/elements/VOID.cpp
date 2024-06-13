d1cf8373eba4d3ab8cd43cae9cca15d5651268af
Onestay42 <stonebeckman@gmail.com>
2024-06-12 21:47:40 -0400

    Update VOID.cpp

--- src/simulation/elements/VOID.cpp
+++ src/simulation/elements/VOID.cpp
@@ -39,5 +39,5 @@ void Element::Element_VOID()
 	LowTemperatureTransition = NT;
 	HighTemperature = ITH;
 	HighTemperatureTransition = NT;
-	CtypeDraw = &Element::ctypeDrawVInTmp;
+	CtypeDraw = &Element::basicCtypeDraw;
 }
