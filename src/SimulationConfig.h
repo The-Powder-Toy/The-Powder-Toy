935a6724722b474b62d368f562dd7539e9323d63
A2dev3 <158003499+A2dev3@users.noreply.github.com>
2024-06-14 14:11:44 -0400

    Update SimulationConfig.h

--- src/SimulationConfig.h
+++ src/SimulationConfig.h
@@ -8,7 +8,7 @@ constexpr int BARSIZE  = 17;
 constexpr float M_GRAV = 6.67300e-1f;
 
 //CELL, the size of the pressure, gravity, and wall maps. Larger than 1 to prevent extreme lag
-constexpr int CELL = 4;
+constexpr int CELL = 1;
 constexpr Vec2<int> CELLS = Vec2(153, 96);
 constexpr Vec2<int> RES = CELLS * CELL;
 
