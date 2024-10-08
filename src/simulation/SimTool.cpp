The-Powder-Toy/The-Powder-Toy

commit a26544ba955f3a8888393a583c1b58166f825858

Move tool logic out of Simulation
Also uncurse WindTool in the process; it is now an ordinary SimTool. This required extending SimTool with a Drag method that is called every tick while the user prepares for drawing a line and is shown the preview.

Since the Simulation doesn't have to understand SimTools anymore, they don't have to be a part of SimulationData and so NoToolClasses.cpp can finally be removed from the renderer executable.

src/simulation/SimTool.cpp
@@ -1,16 +1,153 @@
-#include "common/tpt-rand.h"
-#include "graphics/Renderer.h"
-#include "simulation/ElementGraphics.h"
-#include "simulation/gravity/Gravity.h"
-#include "simulation/Simulation.h"
-#include "Misc.h"
-#include "ToolClasses.h"
-#include <cmath>
+#include "SimTool.h"
+#include "Simulation.h"
+#include "gui/game/Brush.h"
 
-SimTool::SimTool():
-Identifier("DEFAULT_TOOL_INVALID"),
-Name(""),
-Colour(0xFFFFFF_rgb),
-Description("NULL Tool, does NOTHING")
+void SimTool::CallPerform(Simulation *sim, ui::Point position, ui::Point brushOffset)
 {
+	Particle * cpart = NULL;
+	int r;
+	if ((r = sim->pmap[position.Y][position.X]))
+		cpart = &(sim->parts[ID(r)]);
+	else if ((r = sim->photons[position.Y][position.X]))
+		cpart = &(sim->parts[ID(r)]);
+	if (Perform)
+	{
+		// TODO: maybe do something with the result
+		Perform(this, sim, cpart, position.X, position.Y, brushOffset.X, brushOffset.Y, Strength);
+	}
+}
+
+static void defaultPerformDraw(SimTool *tool, Simulation *sim, const Brush &brush, ui::Point position)
+{
+	for (ui::Point off : brush)
+	{
+		ui::Point coords = position + off;
+		if (coords.X >= 0 && coords.Y >= 0 && coords.X < XRES && coords.Y < YRES)
+		{
+			tool->CallPerform(sim, coords, position);
+		}
+	}
+}
+
+static void defaultPerformDrawLine(SimTool *tool, Simulation *sim, const Brush &brush, ui::Point position1, ui::Point position2, bool dragging)
+{
+	auto x1 = position1.X;
+	auto y1 = position1.Y;
+	auto x2 = position2.X;
+	auto y2 = position2.Y;
+	bool reverseXY = abs(y2-y1) > abs(x2-x1);
+	int x, y, dx, dy, sy, rx = brush.GetRadius().X, ry = brush.GetRadius().Y;
+	float e = 0.0f, de;
+	if (reverseXY)
+	{
+		y = x1;
+		x1 = y1;
+		y1 = y;
+		y = x2;
+		x2 = y2;
+		y2 = y;
+	}
+	if (x1 > x2)
+	{
+		y = x1;
+		x1 = x2;
+		x2 = y;
+		y = y1;
+		y1 = y2;
+		y2 = y;
+	}
+	dx = x2 - x1;
+	dy = abs(y2 - y1);
+	de = dx ? dy/(float)dx : 0.0f;
+	y = y1;
+	sy = (y1<y2) ? 1 : -1;
+	for (x=x1; x<=x2; x++)
+	{
+		if (reverseXY)
+			defaultPerformDraw(tool, sim, brush, { y, x });
+		else
+			defaultPerformDraw(tool, sim, brush, { x, y });
+		e += de;
+		if (e >= 0.5f)
+		{
+			y += sy;
+			if (!(rx+ry) && ((y1<y2) ? (y<=y2) : (y>=y2)))
+			{
+				if (reverseXY)
+					defaultPerformDraw(tool, sim, brush, { y, x });
+				else
+					defaultPerformDraw(tool, sim, brush, { x, y });
+			}
+			e -= 1.0f;
+		}
+	}
+}
+
+static void defaultPerformDrawRect(SimTool *tool, Simulation *sim, const Brush &brush, ui::Point position1, ui::Point position2)
+{
+	int brushX = ((position1.X + position2.X) / 2);
+	int brushY = ((position1.Y + position2.Y) / 2);
+	for (auto p : RectBetween(position1, position2))
+	{
+		tool->CallPerform(sim, p, { brushX, brushY });
+	}
+}
+
+SimTool::SimTool()
+{
+	Perform = nullptr;
+	PerformClick = nullptr;
+	PerformDrag = nullptr;
+	PerformDraw = defaultPerformDraw;
+	PerformDrawLine = defaultPerformDrawLine;
+	PerformDrawRect = defaultPerformDrawRect;
+	PerformDrawFill = nullptr;
+}
+
+void SimTool::Click(Simulation * sim, const Brush &brush, ui::Point position)
+{
+	if (PerformClick)
+	{
+		PerformClick(this, sim, brush, position);
+	}
+}
+
+void SimTool::Drag(Simulation *sim, const Brush &brush, ui::Point position1, ui::Point position2)
+{
+	if (PerformDrag)
+	{
+		PerformDrag(this, sim, brush, position1, position2);
+	}
+}
+
+void SimTool::Draw(Simulation *sim, const Brush &brush, ui::Point position)
+{
+	if (PerformDraw)
+	{
+		PerformDraw(this, sim, brush, position);
+	}
+}
+
+void SimTool::DrawLine(Simulation *sim, const Brush &brush, ui::Point position1, ui::Point position2, bool dragging)
+{
+	if (PerformDrawLine)
+	{
+		PerformDrawLine(this, sim, brush, position1, position2, dragging);
+	}
+}
+
+void SimTool::DrawRect(Simulation *sim, const Brush &brush, ui::Point position1, ui::Point position2)
+{
+	if (PerformDrawRect)
+	{
+		PerformDrawRect(this, sim, brush, position1, position2);
+	}
+}
+
+void SimTool::DrawFill(Simulation * sim, const Brush &brush, ui::Point position)
+{
+	if (PerformDrawFill)
+	{
+		PerformDrawFill(this, sim, brush, position);
+	}
 }

src/gui/game/GameController.cpp
@@ -339,11 +339,6 @@ ui::Point GameController::PointTranslate(ui::Point point)
 	return gameModel->AdjustZoomCoords(point);
 }
 
-ui::Point GameController::PointTranslateNoClamp(ui::Point point)
-{
-	return gameModel->AdjustZoomCoords(point);
-}
-
 ui::Point GameController::NormaliseBlockCoord(ui::Point point)
 {
 	return (point/CELL)*CELL;
@@ -370,7 +365,7 @@ void GameController::DrawLine(int toolSelection, ui::Point point1, ui::Point poi
 	if (!activeTool)
 		return;
 	activeTool->Strength = 1.0f;
-	activeTool->DrawLine(sim, cBrush, point1, point2);
+	activeTool->DrawLine(sim, cBrush, point1, point2, false);
 }
 
 void GameController::DrawFill(int toolSelection, ui::Point point)
@@ -434,6 +429,16 @@ void GameController::ToolClick(int toolSelection, ui::Point point)
 	if (!activeTool)
 		return;
 	activeTool->Click(sim, cBrush, point);
+}
+
+void GameController::ToolDrag(int toolSelection, ui::Point point1, ui::Point point2)
+{
+	Simulation * sim = gameModel->GetSimulation();
+	Tool * activeTool = gameModel->GetActiveTool(toolSelection);
+	Brush &cBrush = gameModel->GetBrush();
+	if (!activeTool)
+		return;
+	activeTool->Drag(sim, cBrush, point1, point2);
 }
 
 static Rect<int> SaneSaveRect(Vec2<int> point1, Vec2<int> point2)

src/gui/game/GameController.h
@@ -104,6 +104,7 @@ public:
 	void SetBrushSize(ui::Point newSize);
 	void AdjustZoomSize(int direction, bool logarithmic = false);
 	void ToolClick(int toolSelection, ui::Point point);
+	void ToolDrag(int toolSelection, ui::Point point1, ui::Point point2);
 	void DrawPoints(int toolSelection, ui::Point oldPos, ui::Point newPos, bool held);
 	void DrawRect(int toolSelection, ui::Point point1, ui::Point point2);
 	void DrawLine(int toolSelection, ui::Point point1, ui::Point point2);
@@ -172,7 +173,6 @@ public:
 	void TransformPlaceSave(Mat2<int> transform, Vec2<int> nudge);
 	bool MouseInZoom(ui::Point position);
 	ui::Point PointTranslate(ui::Point point);
-	ui::Point PointTranslateNoClamp(ui::Point point);
 	ui::Point NormaliseBlockCoord(ui::Point point);
 	String ElementResolve(int type, int ctype);
 	String BasicParticleInfo(Particle const &sample_part);

src/gui/game/GameModel.cpp
@@ -192,10 +192,6 @@ GameModel::~GameModel()
 			menuList[i]->ClearTools();
 		delete menuList[i];
 	}
-	for (std::vector<Tool*>::iterator iter = extraElementTools.begin(), end = extraElementTools.end(); iter != end; ++iter)
-	{
-		delete *iter;
-	}
 	delete sim;
 	delete ren;
 	//if(activeTools)
@@ -259,12 +255,9 @@ void GameModel::BuildMenus()
 	menuList.clear();
 	toolList.clear();
 
-	for(std::vector<Tool*>::iterator iter = extraElementTools.begin(), end = extraElementTools.end(); iter != end; ++iter)
-	{
-		delete *iter;
-	}
 	extraElementTools.clear();
 	elementTools.clear();
+	tools.clear();
 
 	//Create menus
 	for (auto &section : sd.msections)
@@ -280,19 +273,19 @@ void GameModel::BuildMenus()
 			Tool * tempTool;
 			if(i == PT_LIGH)
 			{
-				tempTool = new Element_LIGH_Tool(i, elements[i].Name, elements[i].Description, elements[i].Colour, elements[i].Identifier, elements[i].IconGenerator);
+				tempTool = AddTool<Element_LIGH_Tool>(i, elements[i].Name, elements[i].Description, elements[i].Colour, elements[i].Identifier, elements[i].IconGenerator);
 			}
 			else if(i == PT_TESC)
 			{
-				tempTool = new Element_TESC_Tool(i, elements[i].Name, elements[i].Description, elements[i].Colour, elements[i].Identifier, elements[i].IconGenerator);
+				tempTool = AddTool<Element_TESC_Tool>(i, elements[i].Name, elements[i].Description, elements[i].Colour, elements[i].Identifier, elements[i].IconGenerator);
 			}
 			else if(i == PT_STKM || i == PT_FIGH || i == PT_STKM2)
 			{
-				tempTool = new PlopTool(i, elements[i].Name, elements[i].Description, elements[i].Colour, elements[i].Identifier, elements[i].IconGenerator);
+				tempTool = AddTool<PlopTool>(i, elements[i].Name, elements[i].Description, elements[i].Colour, elements[i].Identifier, elements[i].IconGenerator);
 			}
 			else
 			{
-				tempTool = new ElementTool(i, elements[i].Name, elements[i].Description, elements[i].Colour, elements[i].Identifier, elements[i].IconGenerator);
+				tempTool = AddTool<ElementTool>(i, elements[i].Name, elements[i].Description, elements[i].Colour, elements[i].Identifier, elements[i].IconGenerator);
 			}
 
 			if (elements[i].MenuSection >= 0 && elements[i].MenuSection < int(sd.msections.size()) && elements[i].MenuVisible)
@@ -310,7 +303,7 @@ void GameModel::BuildMenus()
 	//Build menu for GOL types
 	for(int i = 0; i < NGOL; i++)
 	{
-		Tool * tempTool = new ElementTool(PT_LIFE|PMAPID(i), builtinGol[i].name, builtinGol[i].description, builtinGol[i].colour, "DEFAULT_PT_LIFE_"+builtinGol[i].name.ToAscii());
+		Tool * tempTool = AddTool<ElementTool>(PT_LIFE|PMAPID(i), builtinGol[i].name, builtinGol[i].description, builtinGol[i].colour, "DEFAULT_PT_LIFE_"+builtinGol[i].name.ToAscii());
 		menuList[SC_LIFE]->AddTool(tempTool);
 	}
 	{
@@ -363,7 +356,7 @@ void GameModel::BuildMenus()
 		}
 		for (auto &gd : newCustomGol)
 		{
-			Tool * tempTool = new ElementTool(PT_LIFE|PMAPID(gd.rule), gd.nameString, "Custom GOL type: " + gd.ruleString, RGB<uint8_t>::Unpack(gd.colour1), "DEFAULT_PT_LIFECUST_"+gd.nameString.ToAscii(), NULL);
+			Tool * tempTool = AddTool<ElementTool>(PT_LIFE|PMAPID(gd.rule), gd.nameString, "Custom GOL type: " + gd.ruleString, RGB<uint8_t>::Unpack(gd.colour1), "DEFAULT_PT_LIFECUST_"+gd.nameString.ToAscii(), nullptr);
 			menuList[SC_LIFE]->AddTool(tempTool);
 		}
 		sd.SetCustomGOL(newCustomGol);
@@ -372,37 +365,29 @@ void GameModel::BuildMenus()
 	//Build other menus from wall data
 	for(int i = 0; i < UI_WALLCOUNT; i++)
 	{
-		Tool * tempTool = new WallTool(i, sd.wtypes[i].descs, sd.wtypes[i].colour, sd.wtypes[i].identifier, sd.wtypes[i].textureGen);
+		Tool * tempTool = AddTool<WallTool>(i, sd.wtypes[i].descs, sd.wtypes[i].colour, sd.wtypes[i].identifier, sd.wtypes[i].textureGen);
 		menuList[SC_WALL]->AddTool(tempTool);
 	}
 
 	//Build menu for tools
-	for (size_t i = 0; i < sd.tools.size(); i++)
+	for (auto &tool : ::GetTools())
 	{
-		Tool *tempTool = new Tool(
-			i,
-			sd.tools[i].Name,
-			sd.tools[i].Description,
-			sd.tools[i].Colour,
-			sd.tools[i].Identifier
-		);
-		menuList[SC_TOOL]->AddTool(tempTool);
+		menuList[SC_TOOL]->AddTool(AddTool<SimTool>(tool));
 	}
 	//Add special sign and prop tools
-	menuList[SC_TOOL]->AddTool(new WindTool());
-	menuList[SC_TOOL]->AddTool(new PropertyTool(*this));
-	menuList[SC_TOOL]->AddTool(new SignTool(*this));
-	menuList[SC_TOOL]->AddTool(new SampleTool(*this));
-	menuList[SC_LIFE]->AddTool(new GOLTool(*this));
+	menuList[SC_TOOL]->AddTool(AddTool<PropertyTool>(*this));
+	menuList[SC_TOOL]->AddTool(AddTool<SignTool>(*this));
+	menuList[SC_TOOL]->AddTool(AddTool<SampleTool>(*this));
+	menuList[SC_LIFE]->AddTool(AddTool<GOLTool>(*this));
 
 	//Add decoration tools to menu
-	menuList[SC_DECO]->AddTool(new DecorationTool(view, DECO_ADD, "ADD", "Colour blending: Add.", 0x000000_rgb, "DEFAULT_DECOR_ADD"));
-	menuList[SC_DECO]->AddTool(new DecorationTool(view, DECO_SUBTRACT, "SUB", "Colour blending: Subtract.", 0x000000_rgb, "DEFAULT_DECOR_SUB"));
-	menuList[SC_DECO]->AddTool(new DecorationTool(view, DECO_MULTIPLY, "MUL", "Colour blending: Multiply.", 0x000000_rgb, "DEFAULT_DECOR_MUL"));
-	menuList[SC_DECO]->AddTool(new DecorationTool(view, DECO_DIVIDE, "DIV", "Colour blending: Divide." , 0x000000_rgb, "DEFAULT_DECOR_DIV"));
-	menuList[SC_DECO]->AddTool(new DecorationTool(view, DECO_SMUDGE, "SMDG", "Smudge tool, blends surrounding deco together.", 0x000000_rgb, "DEFAULT_DECOR_SMDG"));
-	menuList[SC_DECO]->AddTool(new DecorationTool(view, DECO_CLEAR, "CLR", "Erase any set decoration.", 0x000000_rgb, "DEFAULT_DECOR_CLR"));
-	menuList[SC_DECO]->AddTool(new DecorationTool(view, DECO_DRAW, "SET", "Draw decoration (No blending).", 0x000000_rgb, "DEFAULT_DECOR_SET"));
+	menuList[SC_DECO]->AddTool(AddTool<DecorationTool>(view, DECO_ADD, "ADD", "Colour blending: Add.", 0x000000_rgb, "DEFAULT_DECOR_ADD"));
+	menuList[SC_DECO]->AddTool(AddTool<DecorationTool>(view, DECO_SUBTRACT, "SUB", "Colour blending: Subtract.", 0x000000_rgb, "DEFAULT_DECOR_SUB"));
+	menuList[SC_DECO]->AddTool(AddTool<DecorationTool>(view, DECO_MULTIPLY, "MUL", "Colour blending: Multiply.", 0x000000_rgb, "DEFAULT_DECOR_MUL"));
+	menuList[SC_DECO]->AddTool(AddTool<DecorationTool>(view, DECO_DIVIDE, "DIV", "Colour blending: Divide." , 0x000000_rgb, "DEFAULT_DECOR_DIV"));
+	menuList[SC_DECO]->AddTool(AddTool<DecorationTool>(view, DECO_SMUDGE, "SMDG", "Smudge tool, blends surrounding deco together.", 0x000000_rgb, "DEFAULT_DECOR_SMDG"));
+	menuList[SC_DECO]->AddTool(AddTool<DecorationTool>(view, DECO_CLEAR, "CLR", "Erase any set decoration.", 0x000000_rgb, "DEFAULT_DECOR_CLR"));
+	menuList[SC_DECO]->AddTool(AddTool<DecorationTool>(view, DECO_DRAW, "SET", "Draw decoration (No blending).", 0x000000_rgb, "DEFAULT_DECOR_SET"));
 	SetColourSelectorColour(colour); // update tool colors
 	decoToolset[0] = GetToolFromIdentifier("DEFAULT_DECOR_SET");
 	decoToolset[1] = GetToolFromIdentifier("DEFAULT_DECOR_CLR");
@@ -1730,4 +1715,13 @@ void GameModel::AfterSim()
 {
 	sim->AfterSim();
 	CommandInterface::Ref().HandleEvent(AfterSimEvent{});
+}
+
+Tool *GameModel::GetToolByIndex(int index)
+{
+	if (index < 0 || index >= int(tools.size()))
+	{
+		return nullptr;
+	}
+	return tools[index].get();
 }

src/gui/game/GameModel.h
@@ -60,6 +60,17 @@ private:
 	std::vector<GameView*> observers;
 	std::vector<Tool*> toolList;
 
+	std::vector<std::unique_ptr<Tool>> tools;
+
+	template<class ToolType, class... Args>
+	ToolType *AddTool(Args &&...args)
+	{
+		auto ptr = std::make_unique<ToolType>(std::forward<Args>(args)...);
+		auto raw = ptr.get();
+		tools.push_back(std::move(ptr));
+		return raw;
+	}
+
 	//All tools that are associated with elements
 	std::vector<Tool*> elementTools;
 	//Tools that are present in elementTools, but don't have an associated menu and need to be freed manually
@@ -138,6 +149,8 @@ public:
 
 	void Tick();
 
+	Tool *GetToolByIndex(int index);
+
 	void SetEdgeMode(int edgeMode);
 	int GetEdgeMode();
 	void SetTemperatureScale(int temperatureScale);
@@ -199,6 +212,11 @@ public:
 	Tool * GetElementTool(int elementID);
 	std::vector<Tool*> GetToolList();
 	std::vector<Tool*> GetUnlistedTools();
+
+	const std::vector<std::unique_ptr<Tool>> &GetTools()
+	{
+		return tools;
+	}
 
 	Brush &GetBrush();
 	Brush *GetBrushByID(int i);

src/gui/game/GameView.cpp
@@ -178,7 +178,6 @@ GameView::GameView():
 	wallBrush(false),
 	toolBrush(false),
 	decoBrush(false),
-	windTool(false),
 	toolIndex(0),
 	currentSaveType(0),
 	lastMenu(-1),
@@ -1152,7 +1151,6 @@ void GameView::OnMouseDown(int x, int y, unsigned button)
 				return;
 			Tool *lastTool = c->GetActiveTool(toolIndex);
 			c->SetLastTool(lastTool);
-			windTool = lastTool->Identifier == "DEFAULT_UI_WIND";
 			decoBrush = lastTool->Identifier.BeginsWith("DEFAULT_DECOR_");
 
 			UpdateDrawMode();
@@ -1724,12 +1722,12 @@ void GameView::OnTick(float dt)
 		{
 			c->DrawFill(toolIndex, c->PointTranslate(currentMouse));
 		}
-		else if (windTool && drawMode == DrawLine)
+		else if (drawMode == DrawLine)
 		{
 			ui::Point drawPoint2 = currentMouse;
 			if (altBehaviour)
 				drawPoint2 = lineSnapCoords(c->PointTranslate(drawPoint1), currentMouse);
-			c->DrawLine(toolIndex, c->PointTranslate(drawPoint1), c->PointTranslateNoClamp(drawPoint2));
+			c->ToolDrag(toolIndex, c->PointTranslate(drawPoint1), c->PointTranslate(drawPoint2));
 		}
 	}
 
@@ -2177,7 +2175,7 @@ void GameView::OnDraw()
 
 	if (showBrush && selectMode == SelectNone && (!zoomEnabled || zoomCursorFixed) && activeBrush && (isMouseDown || (currentMouse.X >= 0 && currentMouse.X < XRES && currentMouse.Y >= 0 && currentMouse.Y < YRES)))
 	{
-		ui::Point finalCurrentMouse = windTool ? c->PointTranslateNoClamp(currentMouse) : c->PointTranslate(currentMouse);
+		ui::Point finalCurrentMouse = c->PointTranslate(currentMouse);
 		ui::Point initialDrawPoint = drawPoint1;
 
 		if (wallBrush)

src/gui/game/GameView.h
@@ -61,7 +61,6 @@ private:
 	bool wallBrush;
 	bool toolBrush;
 	bool decoBrush;
-	bool windTool;
 	int toolIndex;
 	int currentSaveType;
 	int lastMenu;

src/gui/game/Menu.cpp
@@ -13,10 +13,6 @@ Menu::Menu(String::value_type icon_, String description_, int visible_):
 
 Menu::~Menu()
 {
-	for(unsigned int i = 0; i < tools.size(); i++)
-	{
-		delete tools[i];
-	}
 	tools.clear();
 }
 

commit 2cc5d9bbbb246110caf900fa65e4ea6e40e73bc7

Fix tools drawing rectangles with tools not doing anything in some cases
Namely, when those rectangles are not drawn in a top left to bottom right corner order. Broken by a26544ba955f, which neglected to port the rectangle normalization logic from Simulation::ToolBox.

src/simulation/SimTool.cpp
@@ -87,7 +87,9 @@ static void defaultPerformDrawRect(SimTool *tool, Simulation *sim, const Brush &
 {
 	int brushX = ((position1.X + position2.X) / 2);
 	int brushY = ((position1.Y + position2.Y) / 2);
-	for (auto p : RectBetween(position1, position2))
+	auto tl = ui::Point{ std::min(position1.X, position2.X), std::min(position1.Y, position2.Y) };
+	auto br = ui::Point{ std::max(position1.X, position2.X), std::max(position1.Y, position2.Y) };
+	for (auto p : RectBetween(tl, br))
 	{
 		tool->CallPerform(sim, p, { brushX, brushY });
 	}