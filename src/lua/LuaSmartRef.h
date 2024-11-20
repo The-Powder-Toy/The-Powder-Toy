The-Powder-Toy/The-Powder-Toy

commit 06e28367266e3cceb14fa1ac4fd6dd97c011b15c

Fix Lua reference leaks (fixes #638)

src/lua/LuaSmartRef.h
@@ -1,0 +1,20 @@
+#pragma once
+
+#include "LuaCompat.h"
+
+class LuaSmartRef
+{
+	int ref;
+
+protected:
+	lua_State *l;
+
+public:
+	LuaSmartRef(lua_State *l);
+	~LuaSmartRef();
+	void Assign(int index); // Copies the value before getting reference, stack unchanged.
+	void Clear();
+	operator int() const;
+	operator bool() const;
+};
+

src/graphics/Renderer.cpp
@@ -18,6 +18,7 @@
 #ifdef LUACONSOLE
 #include "lua/LuaScriptInterface.h"
 #include "lua/LuaScriptHelper.h"
+#include "lua/LuaSmartRef.h"
 #endif
 #include "hmap.h"
 #ifdef OGLR

src/gui/interface/Window.cpp
@@ -80,6 +80,7 @@ void Window::RemoveComponent(Component* c)
 			Components.erase(Components.begin() + i);
 
 			// we're done
+			c->SetParentWindow(NULL);
 			return;
 		}
 	}

src/lua/LegacyLuaAPI.cpp
@@ -8,6 +8,7 @@
 #include "Format.h"
 #include "LuaScriptInterface.h"
 #include "LuaScriptHelper.h"
+#include "LuaSmartRef.h"
 #include "Platform.h"
 #include "PowderToy.h"
 
@@ -136,7 +137,7 @@ int luacon_partsread(lua_State* l)
 	if (i < 0 || i >= NPART)
 		return luaL_error(l, "array index out of bounds");
 
-	lua_rawgeti(l, LUA_REGISTRYINDEX, tptPart);
+	lua_rawgeti(l, LUA_REGISTRYINDEX, *tptPart);
 	cIndex = i;
 	return 1;
 }
@@ -317,12 +318,9 @@ int luatpt_element_func(lua_State *l)
 	{
 		int element = luaL_optint(l, 2, 0);
 		int replace = luaL_optint(l, 3, 0);
-		int function;
-		lua_pushvalue(l, 1);
-		function = luaL_ref(l, LUA_REGISTRYINDEX);
 		if(element > 0 && element < PT_NUM)
 		{
-			lua_el_func[element] = function;
+			lua_el_func[element].Assign(1);
 			if (replace == 2)
 				lua_el_mode[element] = 3; //update before
 			else if (replace)
@@ -341,7 +339,7 @@ int luatpt_element_func(lua_State *l)
 		int element = luaL_optint(l, 2, 0);
 		if(element > 0 && element < PT_NUM)
 		{
-			lua_el_func[element] = 0;
+			lua_el_func[element].Clear();
 			lua_el_mode[element] = 0;
 		}
 		else
@@ -390,12 +388,9 @@ int luatpt_graphics_func(lua_State *l)
 	if(lua_isfunction(l, 1))
 	{
 		int element = luaL_optint(l, 2, 0);
-		int function;
-		lua_pushvalue(l, 1);
-		function = luaL_ref(l, LUA_REGISTRYINDEX);
 		if (element > 0 && element < PT_NUM)
 		{
-			lua_gr_func[element] = function;
+			lua_gr_func[element].Assign(1);
 			luacon_ren->graphicscache[element].isready = 0;
 			return 0;
 		}
@@ -409,7 +404,7 @@ int luatpt_graphics_func(lua_State *l)
 		int element = luaL_optint(l, 2, 0);
 		if (element > 0 && element < PT_NUM)
 		{
-			lua_gr_func[element] = 0;
+			lua_gr_func[element].Clear();
 			luacon_ren->graphicscache[element].isready = 0;
 			return 0;
 		}

src/lua/LuaButton.cpp
@@ -20,7 +20,7 @@ Luna<LuaButton>::RegType LuaButton::methods[] = {
 
 LuaButton::LuaButton(lua_State * l) :
 	LuaComponent(l),
-	actionFunction(0)
+	actionFunction(l)
 {
 	int posX = luaL_optinteger(l, 1, 0);
 	int posY = luaL_optinteger(l, 2, 0);
@@ -62,17 +62,7 @@ int LuaButton::enabled(lua_State * l)
 
 int LuaButton::action(lua_State * l)
 {
-	if(lua_type(l, 1) != LUA_TNIL)
-	{
-		luaL_checktype(l, 1, LUA_TFUNCTION);
-		lua_pushvalue(l, 1);
-		actionFunction = luaL_ref(l, LUA_REGISTRYINDEX);
-	}
-	else
-	{
-		actionFunction = 0;
-	}
-	return 0;
+	return actionFunction.CheckAndAssignArg1();
 }
 
 int LuaButton::text(lua_State * l)
@@ -96,7 +86,7 @@ void LuaButton::triggerAction()
 	if(actionFunction)
 	{
 		lua_rawgeti(l, LUA_REGISTRYINDEX, actionFunction);
-		lua_rawgeti(l, LUA_REGISTRYINDEX, UserData);
+		lua_rawgeti(l, LUA_REGISTRYINDEX, owner_ref);
 		if (lua_pcall(l, 1, 0, 0))
 		{
 			ci->Log(CommandInterface::LogError, ByteString(lua_tostring(l, -1)).FromUtf8());

src/lua/LuaButton.h
@@ -13,7 +13,7 @@ class LuaScriptInterface;
 class LuaButton: public LuaComponent
 {
 	ui::Button * button;
-	int actionFunction;
+	LuaComponentCallback actionFunction;
 	void triggerAction();
 	int action(lua_State * l);
 	int text(lua_State * l);

src/lua/LuaCheckbox.cpp
@@ -20,7 +20,7 @@ Luna<LuaCheckbox>::RegType LuaCheckbox::methods[] = {
 
 LuaCheckbox::LuaCheckbox(lua_State * l) :
 	LuaComponent(l),
-	actionFunction(0)
+	actionFunction(l)
 {
 	int posX = luaL_optinteger(l, 1, 0);
 	int posY = luaL_optinteger(l, 2, 0);
@@ -60,17 +60,7 @@ int LuaCheckbox::checked(lua_State * l)
 
 int LuaCheckbox::action(lua_State * l)
 {
-	if(lua_type(l, 1) != LUA_TNIL)
-	{
-		luaL_checktype(l, 1, LUA_TFUNCTION);
-		lua_pushvalue(l, 1);
-		actionFunction = luaL_ref(l, LUA_REGISTRYINDEX);
-	}
-	else
-	{
-		actionFunction = 0;
-	}
-	return 0;
+	return actionFunction.CheckAndAssignArg1();
 }
 
 int LuaCheckbox::text(lua_State * l)
@@ -93,7 +83,7 @@ void LuaCheckbox::triggerAction()
 	if(actionFunction)
 	{
 		lua_rawgeti(l, LUA_REGISTRYINDEX, actionFunction);
-		lua_rawgeti(l, LUA_REGISTRYINDEX, UserData);
+		lua_rawgeti(l, LUA_REGISTRYINDEX, owner_ref);
 		lua_pushboolean(l, checkbox->GetChecked());
 		if (lua_pcall(l, 2, 0, 0))
 		{

src/lua/LuaCheckbox.h
@@ -13,7 +13,7 @@ class LuaScriptInterface;
 class LuaCheckbox: public LuaComponent
 {
 	ui::Checkbox * checkbox;
-	int actionFunction;
+	LuaComponentCallback actionFunction;
 	void triggerAction();
 	int action(lua_State * l);
 	int checked(lua_State * l);

commit 0c74c8080dcc7a453339aea66e3ed9ba313d5dbd

Make LuaSmartRef properly non-copyable
Broken since 06e28367266e, which added LuaSmartRef. That it had absolutely not followed the rule of five at all didn't use to be a problem because it had only ever been in vectors that got resized only once throughout the lifetime of the program >_>

src/lua/LuaSmartRef.h
@@ -1,12 +1,27 @@
 #pragma once
 #include "LuaCompat.h"
+#include <utility>
 
 class LuaSmartRef
 {
 	int ref = LUA_REFNIL;
 
 public:
+	LuaSmartRef() = default;
+	LuaSmartRef(const LuaSmartRef &other) = delete;
 	~LuaSmartRef();
+
+	LuaSmartRef(LuaSmartRef &&other) noexcept : LuaSmartRef()
+	{
+		swap(*this, other);
+	}
+
+	LuaSmartRef &operator =(LuaSmartRef other)
+	{
+		swap(*this, other);
+		return *this;
+	}
+
 	void Assign(lua_State *L, int index); // Copies the value before getting reference, stack unchanged.
 	void Clear();
 	int Push(lua_State *L); // Always pushes exactly one value, possibly nil.
@@ -19,5 +34,11 @@ public:
 	inline operator bool() const
 	{
 		return ref != LUA_REFNIL;
+	}
+
+	friend void swap(LuaSmartRef &one, LuaSmartRef &other)
+	{
+		using std::swap;
+		swap(one.ref, other.ref);
 	}
 };