The-Powder-Toy/The-Powder-Toy

commit 02b679aec37f59e3059e67a2fab3e6f121ed6318

Fix views leaking when closing themselves
Views would invoke c->Exit() upon closing themselves, which would close them but not delete them. The controller would then be destroyed, fail to close the view because it wouldn't be the window on top, and skip destroying it in fear of something worse. This behaviour was introduced by c2f8a7df25f3, fixing something indeed worse: views would be destroyed while they were still in use by ui::Engine, sitting somewhere on the window stack but not at the top. In other words, the problem was (and is) that the terms of ownership of views between controllers and ui::Engine are unclear.

This commit effectively undoes that earlier commit and tries a different approach: when something is closed, close everything above it. This seems a correct thing to do and also allows controllers to unconditionally take care of their views. Prepare your popcorn though.

This fixes a bug where the stamp browser would stop rendering stamps if it was previously closed "too quickly", which turned out to mean that it was closed before all stamps had finished rendering, and thus ThumbnailRendererTasks belonging to LocalBrowserViews indirectly through SaveButtons would get stuck in a queued state, preventing other SaveButtons from starting their own ThumbnailRendererTasks.

src/gui/game/GameController.cpp
@@ -150,10 +150,8 @@ GameController::~GameController()
 	}
 	commandInterface.reset();
 	delete gameModel;
-	if (gameView->CloseActiveWindow())
-	{
-		delete gameView;
-	}
+	gameView->CloseActiveWindow();
+	delete gameView;
 }
 
 bool GameController::HistoryRestore()

src/gui/console/ConsoleController.cpp
@@ -72,9 +72,7 @@ ConsoleView * ConsoleController::GetView()
 ConsoleController::~ConsoleController()
 {
 	delete consoleModel;
-	if (consoleView->CloseActiveWindow())
-	{
-		delete consoleView;
-	}
+	consoleView->CloseActiveWindow();
+	delete consoleView;
 }
 

src/gui/interface/Engine.cpp
@@ -32,8 +32,8 @@ Engine::~Engine()
 	//Dispose of any Windows.
 	while (!windows.empty())
 	{
-		delete windows.top();
-		windows.pop();
+		delete windows.back();
+		windows.pop_back();
 	}
 }
 
@@ -79,6 +79,7 @@ void Engine::ConfirmExit()
 
 void Engine::ShowWindow(Window * window)
 {
+	CloseWindowAndEverythingAbove(window);
 	if (state_)
 		ignoreEvents = true;
 	if(window->Position.X==-1)
@@ -101,7 +102,7 @@ void Engine::ShowWindow(Window * window)
 		frozenGraphics.emplace(FrozenGraphics{0, std::make_unique<pixel []>(g->Size().X * g->Size().Y)});
 		std::copy_n(g->Data(), g->Size().X * g->Size().Y, frozenGraphics.top().screen.get());
 
-		windows.push(state_);
+		windows.push_back(state_);
 		mousePositions.push(ui::Point(mousex_, mousey_));
 	}
 	if(state_)
@@ -111,13 +112,31 @@ void Engine::ShowWindow(Window * window)
 
 }
 
+void Engine::CloseWindowAndEverythingAbove(Window *window)
+{
+	if (window == state_)
+	{
+		CloseWindow();
+		return;
+	}
+	auto it = std::find(windows.begin(), windows.end(), window);
+	if (it != windows.end())
+	{
+		auto toPop = int(windows.end() - it) + 1; // including state_
+		for (int i = 0; i < toPop; ++i)
+		{
+			CloseWindow();
+		}
+	}
+}
+
 int Engine::CloseWindow()
 {
 	if(!windows.empty())
 	{
 		frozenGraphics.pop();
-		state_ = windows.top();
-		windows.pop();
+		state_ = windows.back();
+		windows.pop_back();
 
 		if(state_)
 			state_->DoFocus();

src/gui/interface/Engine.h
@@ -27,6 +27,7 @@ namespace ui
 
 		void ShowWindow(Window * window);
 		int CloseWindow();
+		void CloseWindowAndEverythingAbove(Window *window);
 
 		void initialMouse(int x, int y);
 		void onMouseMove(int x, int y);
@@ -90,7 +91,7 @@ namespace ui
 
 		float dt;
 		float fps;
-		std::stack<Window*> windows;
+		std::deque<Window*> windows;
 		std::stack<Point> mousePositions;
 		//Window* statequeued_;
 		Window* state_;

src/gui/interface/Window.cpp
@@ -141,14 +141,9 @@ void Window::MakeActiveWindow()
 		Engine::Ref().ShowWindow(this);
 }
 
-bool Window::CloseActiveWindow()
+void Window::CloseActiveWindow()
 {
-	if (Engine::Ref().GetWindow() == this)
-	{
-		Engine::Ref().CloseWindow();
-		return true;
-	}
-	return false;
+	Engine::Ref().CloseWindowAndEverythingAbove(this);
 }
 
 Graphics * Window::GetGraphics()

src/gui/interface/Window.h
@@ -82,7 +82,7 @@ namespace ui
 		enum ExitMethod { MouseOutside, Escape, ExitButton };
 
 		void MakeActiveWindow();
-		bool CloseActiveWindow();
+		void CloseActiveWindow();
 		Graphics * GetGraphics();
 
 	protected:

src/gui/localbrowser/LocalBrowserController.cpp
@@ -161,9 +161,7 @@ void LocalBrowserController::Exit()
 LocalBrowserController::~LocalBrowserController()
 {
 	delete browserModel;
-	if (browserView->CloseActiveWindow())
-	{
-		delete browserView;
-	}
+	browserView->CloseActiveWindow();
+	delete browserView;
 }
 

src/gui/login/LoginController.cpp
@@ -44,9 +44,7 @@ void LoginController::Exit()
 LoginController::~LoginController()
 {
 	delete loginModel;
-	if (loginView->CloseActiveWindow())
-	{
-		delete loginView;
-	}
+	loginView->CloseActiveWindow();
+	delete loginView;
 }
 

commit 74386631e0b6f25f24abc01190b0c813e0d576eb

Fix crash on exit while there are Lua windows visible
Broken since 02b679aec37f, where GameView started popping everything above itself before popping itself. The problem was that LSI would get destroyed before GameView did this, but popping Lua windows needed LSI to be alive still.

src/gui/game/GameController.cpp
@@ -147,10 +147,10 @@ GameController::~GameController()
 	{
 		delete *iter;
 	}
-	commandInterface.reset();
-	delete gameModel;
 	gameView->CloseActiveWindow();
 	delete gameView;
+	commandInterface.reset();
+	delete gameModel;
 }
 
 bool GameController::HistoryRestore()