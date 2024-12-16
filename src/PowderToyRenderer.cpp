The-Powder-Toy/The-Powder-Toy

commit f52e0470373147eef2990a5262417fcc277ebd39

Move sampling further away from Renderer

src/PowderToyRenderer.cpp
@@ -1,4 +1,5 @@
 #include "graphics/Graphics.h"
+#include "graphics/VideoBuffer.h"
 #include "graphics/Renderer.h"
 #include "common/String.h"
 #include "common/tpt-rand.h"
@@ -73,6 +74,6 @@ int main(int argc, char *argv[])
 
 	ren->RenderSimulation();
 
-	if (auto data = ren->DumpFrame().ToPNG())
+	if (auto data = VideoBuffer(ren->GetVideo()).ToPNG())
 		Platform::WriteFile(*data, outputFilename);
 }

src/graphics/Graphics.h
@@ -1,5 +1,10 @@
 #pragma once
-#include "VideoBuffer.h"
+#include "Icons.h"
+#include "gui/interface/Point.h"
+#include "common/Plane.h"
+#include "SimulationConfig.h"
+#include "RasterDrawMethods.h"
+#include <array>
 
 class Graphics: public RasterDrawMethods<Graphics>
 {

src/graphics/Renderer.cpp
@@ -1,5 +1,6 @@
 #include "Renderer.h"
 #include "Misc.h"
+#include "VideoBuffer.h"
 #include "common/tpt-rand.h"
 #include "common/tpt-compat.h"
 #include "simulation/Simulation.h"
@@ -36,7 +37,7 @@ void Renderer::RenderSimulation()
 	}
 }
 
-void Renderer::render_gravlensing(const Video &source)
+void Renderer::render_gravlensing(const RendererFrame &source)
 {
 	for (auto p : RES.OriginRect())
 	{

src/graphics/Renderer.h
@@ -1,13 +1,13 @@
 #pragma once
-#include "VideoBuffer.h"
+#include "Icons.h"
+#include "RasterDrawMethods.h"
 #include "gui/game/RenderPreset.h"
 #include "gui/interface/Point.h"
 #include "common/tpt-rand.h"
-#include "SimulationConfig.h"
+#include "RendererFrame.h"
 #include "FindingElement.h"
 #include <cstdint>
 #include <optional>
-#include <array>
 #include <memory>
 #include <mutex>
 #include <vector>
@@ -30,10 +30,9 @@ int HeatToColour(float temp);
 
 class Renderer: public RasterDrawMethods<Renderer>
 {
-	using Video = PlaneAdapter<std::array<pixel, WINDOW.X * RES.Y>, WINDOW.X, RES.Y>;
-	Video video;
+	RendererFrame video;
 	std::array<pixel, WINDOW.X * RES.Y> persistentVideo;
-	Video warpVideo;
+	RendererFrame warpVideo;
 
 	Rect<int> GetClipRect() const
 	{
@@ -45,18 +44,13 @@ class Renderer: public RasterDrawMethods<Renderer>
 	float fireIntensity = 1;
 
 public:
-	Vec2<int> Size() const
-	{
-		return video.Size();
-	}
-
-	pixel const *Data() const
-	{
-		return video.data();
-	}
-
 	RNG rng;
 	const RenderableSimulation *sim = nullptr;
+
+	const RendererFrame &GetVideo() const
+	{
+		return video;
+	}
 
 	uint32_t renderMode = 0;
 	uint32_t colorMode = 0;
@@ -73,7 +67,6 @@ public:
 	int decorations_enable;
 	bool blackDecorations;
 	bool debugLines;
-	pixel sampleColor;
 	std::optional<FindingElement> findingElement;
 	int foundElements;
 
@@ -86,7 +79,7 @@ public:
 	void DrawBlob(Vec2<int> pos, RGB<uint8_t> colour);
 	void DrawWalls();
 	void DrawSigns();
-	void render_gravlensing(const Video &source);
+	void render_gravlensing(const RendererFrame &source);
 	void render_fire();
 	float GetFireIntensity() const
 	{
@@ -101,13 +94,9 @@ public:
 
 	void ClearAccumulation();
 	void clearScreen();
-	void SetSample(Vec2<int> pos);
 
 	void draw_icon(int x, int y, Icon icon);
 
-	VideoBuffer DumpFrame();
-
-	pixel GetPixel(Vec2<int> pos) const;
 	//...
 	//Display mode modifiers
 	void SetRenderMode(uint32_t newRenderMode);

src/graphics/RendererBasic.cpp
@@ -5,12 +5,8 @@
 #include "simulation/ElementClasses.h"
 #include "simulation/ElementGraphics.h"
 
-void Renderer::SetSample(Vec2<int> pos)
+void Renderer::clearScreen()
 {
-	sampleColor = GetPixel(pos);
-}
-
-void Renderer::clearScreen() {
 	if(displayMode & DISPLAY_PERS)
 	{
 		std::copy(persistentVideo.begin(), persistentVideo.end(), video.RowIterator({ 0, 0 }));
@@ -54,13 +50,6 @@ void Renderer::prepare_alpha(int size, float intensity)
 		for (y=0; y<CELL*3; y++)
 			fire_alpha[y][x] = (int)(multiplier*temp[y][x]/(CELL*CELL));
 
-}
-
-pixel Renderer::GetPixel(Vec2<int> pos) const
-{
-	if (pos.X<0 || pos.Y<0 || pos.X>=WINDOWW || pos.Y>=WINDOWH)
-		return 0;
-	return video[pos];
 }
 
 std::vector<RGB<uint8_t>> Renderer::flameTable;
@@ -128,7 +117,6 @@ Renderer::Renderer():
 	decorations_enable(1),
 	blackDecorations(false),
 	debugLines(false),
-	sampleColor(0xFFFFFFFF),
     foundElements(0),
 	mousePos(0, 0),
 	gridSize(0)
@@ -266,13 +254,6 @@ void Renderer::ResetModes()
 	SetRenderMode(RENDER_BASC | RENDER_FIRE | RENDER_SPRK | RENDER_EFFE);
 	SetDisplayMode(0);
 	SetColorMode(COLOUR_DEFAULT);
-}
-
-VideoBuffer Renderer::DumpFrame()
-{
-	VideoBuffer newBuffer(RES);
-	newBuffer.BlendImage(video.data(), 0xFF, Size().OriginRect());
-	return newBuffer;
 }
 
 template struct RasterDrawMethods<Renderer>;

src/graphics/RendererFrame.h
@@ -1,0 +1,8 @@
+#pragma once
+#include "SimulationConfig.h"
+#include "Pixel.h"
+#include "common/Plane.h"
+#include <array>
+
+constexpr auto RendererFrameSize = Vec2<int>{ WINDOW.X, RES.Y };
+using RendererFrame = PlaneAdapter<std::array<pixel, WINDOW.X * RES.Y>, RendererFrameSize.X, RendererFrameSize.Y>;

src/graphics/VideoBuffer.h
@@ -26,6 +26,11 @@ public:
 	VideoBuffer(pixel const *data, Vec2<int> size, size_t rowStride);
 	VideoBuffer(Vec2<int> size);
 
+	template<class Plane>
+	VideoBuffer(const Plane &plane) : VideoBuffer(plane.data(), plane.Size())
+	{
+	}
+
 	Vec2<int> Size() const
 	{
 		return video.Size();

src/gui/game/DecorationTool.cpp
@@ -54,11 +54,15 @@ void DecorationTool::DrawRect(Simulation * sim, Brush const &brush, ui::Point po
 
 void DecorationTool::DrawFill(Simulation * sim, Brush const &brush, ui::Point position)
 {
-	auto loc = RGB<uint8_t>::Unpack(ren.GetPixel(position));
+	if (!rendererFrame.Size().OriginRect().Contains(position))
+	{
+		return;
+	}
+	auto loc = RGB<uint8_t>::Unpack(rendererFrame[position]);
 	if (ToolID == DECO_CLEAR)
 		// TODO: this is actually const-correct
-		sim->ApplyDecorationFill(const_cast<Renderer *>(&ren), position.X, position.Y, 0, 0, 0, 0, loc.Red, loc.Green, loc.Blue);
+		sim->ApplyDecorationFill(rendererFrame, position.X, position.Y, 0, 0, 0, 0, loc.Red, loc.Green, loc.Blue);
 	else
-		sim->ApplyDecorationFill(const_cast<Renderer *>(&ren), position.X, position.Y, Colour.Red, Colour.Green, Colour.Blue, Colour.Alpha, loc.Red, loc.Green, loc.Blue);
+		sim->ApplyDecorationFill(rendererFrame, position.X, position.Y, Colour.Red, Colour.Green, Colour.Blue, Colour.Alpha, loc.Red, loc.Green, loc.Blue);
 }
 

commit 5739170f20d73cae3f4400daa134f0863f308f54

Fix oversize thumbnails
Broken since f52e04703731, where Renderer::DumpFrame was removed in favour of constructing VideoBuffers directly from pixel data, but Renderer::video's exact width wasn't taken into account. This was chosen to be WINDOW.X, rather than the intuitive RES.X, in order to enable blitting the sim framebuffer onto the window framebuffer with a single memcpy.

src/PowderToyRenderer.cpp
@@ -68,6 +68,7 @@ int main(int argc, char *argv[])
 		ren->BlendText({ x+8, y+8 }, "Save file invalid", 0xC0C0F0_rgb .WithAlpha(255));
 	}
 
-	if (auto data = VideoBuffer(ren->GetVideo()).ToPNG())
+	auto &video = ren->GetVideo();
+	if (auto data = VideoBuffer(video.data(), RES, video.Size().X).ToPNG())
 		Platform::WriteFile(*data, outputFilename);
 }