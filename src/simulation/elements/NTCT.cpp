d157046f31b1f169a4f92bb59d17e7381a81c500
Your Name <rebmiami22@gmail.com>
2024-12-18 08:36:27 -0500

    Removed degree symbol from NTCT/PTCT description; renders incorrectly

--- src/simulation/elements/NTCT.cpp
+++ src/simulation/elements/NTCT.cpp
@@ -28,7 +28,7 @@ void Element::Element_NTCT()
 	Weight = 100;
 
 	HeatConduct = 251;
-	Description = "NTC Thermistor. Conducts electricity with PSCN and NSCN, but only when heated above 100°C.";
+	Description = "NTC Thermistor. Conducts electricity with PSCN and NSCN, but only when heated above 100C.";
 
 	Properties = TYPE_SOLID|PROP_CONDUCTS|PROP_LIFE_DEC;
 
