The-Powder-Toy/The-Powder-Toy

commit 6b5bbb177dd5cb9c133c717fff4a0b60c98cf993

Stop copying the gravity process's input every frame
In the vast majority of cases (i.e. when Gravity::Exchange is indirectly called from BeforeSim), the input is cleared to zeros anyway, so it's less work in Exchange to actually just exchange the old input with the new one than to copy the new one into the old one.

Also make gravity functions private in Simulation.

src/simulation/gravity/Fft.cpp
@@ -204,7 +204,7 @@ void GravityImpl::Init()
 	});
 }
 
-void Gravity::Exchange(GravityOutput &gravOut, const GravityInput &gravIn)
+void Gravity::Exchange(GravityOutput &gravOut, GravityInput &gravIn)
 {
 	auto *fftGravity = static_cast<GravityImpl *>(this);
 
@@ -231,7 +231,7 @@ void Gravity::Exchange(GravityOutput &gravOut, const GravityInput &gravIn)
 	    std::memcmp(&fftGravity->gravIn.mask[{ 0, 0 }], &gravIn.mask[{ 0, 0 }], NCELL * sizeof(float)))
 	{
 		fftGravity->copyGravOut = true;
-		fftGravity->gravIn = gravIn;
+		std::swap(gravIn, fftGravity->gravIn);
 	}
 
 	fftGravity->Dispatch();

src/simulation/Simulation.cpp
@@ -3740,6 +3740,7 @@ void Simulation::BeforeSim()
 			air->update_airh();
 
 		DispatchNewtonianGravity();
+		// gravIn is now potentially garbage, which is ok, we were going to clear it for the frame anyway
 		for (auto p : gravIn.mass.Size().OriginRect())
 		{
 			gravIn.mass[p] = 0.f;
@@ -3964,6 +3965,8 @@ void Simulation::ResetNewtonianGravity(GravityInput newGravIn, GravityOutput new
 {
 	gravIn = newGravIn;
 	DispatchNewtonianGravity();
+	// gravIn is now potentially garbage, set it again
+	gravIn = newGravIn;
 	if (grav)
 	{
 		gravOut = newGravOut;
@@ -3981,7 +3984,10 @@ void Simulation::EnableNewtonianGravity(bool enable)
 	if (!grav && enable)
 	{
 		grav = Gravity::Create();
+		auto oldGravIn = gravIn;
 		DispatchNewtonianGravity();
+		// gravIn is now potentially garbage, set it again
+		gravIn = std::move(oldGravIn);
 	}
 }
 

src/simulation/Simulation.h
@@ -220,10 +220,11 @@ public:
 	~Simulation();
 
 	void EnableNewtonianGravity(bool enable);
-	void ResetNewtonianGravity(GravityInput newGravIn, GravityOutput newGravOut);
-	void DispatchNewtonianGravity();
-	void UpdateGravityMask();
 
 private:
 	CoordStack& getCoordStackSingleton();
+
+	void ResetNewtonianGravity(GravityInput newGravIn, GravityOutput newGravOut);
+	void DispatchNewtonianGravity();
+	void UpdateGravityMask();
 };

src/simulation/gravity/Gravity.h
@@ -8,7 +8,8 @@ protected:
 	Gravity() = default;
 
 public:
-	void Exchange(GravityOutput &gravOut, const GravityInput &gravIn);
+	// potentially clobbers gravIn
+	void Exchange(GravityOutput &gravOut, GravityInput &gravIn);
 
 	static GravityPtr Create();
 };

src/simulation/gravity/Null.cpp
@@ -1,6 +1,6 @@
 #include "Gravity.h"
 
-void Gravity::Exchange(GravityOutput &gravOut, const GravityInput &gravIn)
+void Gravity::Exchange(GravityOutput &gravOut, GravityInput &gravIn)
 {
 }
 

commit f580a0b9d4a03822743644c9fb07fa46b5d7c1a7

Fix gravity wall being intermittently broken when drawn while paused
In fact, even when it was drawn while unpaused, there would be two different gravity masks being swapped between, and only one of them was what the user wanted. However, there wasn't as much difference between them, because one was just one frame more out of date than the other, so this was hard to notice.

Broken since 6b5bbb177dd5, where I forgot that the gravity mask exists at all. The gravity mass input is thrown away every frame in Simulation, and so that commit applies to it well, but the gravity mask isn't.

src/simulation/gravity/Fft.cpp
@@ -232,7 +232,8 @@ void Gravity::Exchange(GravityOutput &gravOut, GravityInput &gravIn, bool forceR
 	    std::memcmp(&fftGravity->gravIn.mask[{ 0, 0 }], &gravIn.mask[{ 0, 0 }], NCELL * sizeof(float)))
 	{
 		fftGravity->copyGravOut = true;
-		std::swap(gravIn, fftGravity->gravIn);
+		std::swap(gravIn.mass, fftGravity->gravIn.mass);
+		fftGravity->gravIn.mask = gravIn.mask;
 		fftGravity->Dispatch();
 	}
 }