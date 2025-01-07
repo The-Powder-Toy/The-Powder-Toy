The-Powder-Toy/The-Powder-Toy

commit 462460b6b3f0907b9b47549d958bfe47f8070f9c

Manage particle lifetime with create/kill_part only
This should fix the long-standing issue of Create/ChangeType callbacks not being called in all cases when particles were created or changed type.

I'm eagerly awaiting reports of the horrendous crashes this will inevitably cause.

src/simulation/Simulation.cpp
@@ -127,15 +127,16 @@ void Simulation::Load(const GameSave *save, bool includePressure, Vec2<int> bloc
 		removeExistingParticles({ x, y });
 
 		// Allocate particle (this location is guaranteed to be empty due to "full scan" logic above)
-		if (pfree == -1)
+		auto i = create_part(-3, x, y, tempPart.type);
+		if (i == -1)
+		{
 			break;
-		auto i = pfree;
-		pfree = parts[i].life;
-		NUM_PARTS += 1;
+		}
 		if (i > parts.lastActiveIndex)
+		{
 			parts.lastActiveIndex = i;
+		}
 		parts[i] = tempPart;
-		elementCount[tempPart.type]++;
 
 
 		switch (parts[i].type)
@@ -977,6 +978,13 @@ int Simulation::parts_avg(int ci, int ni,int t)
 
 void Simulation::clear_sim(void)
 {
+	for (auto i = 0; i <= parts.lastActiveIndex; i++)
+	{
+		if (parts[i].type)
+		{
+			kill_part(i);
+		}
+	}
 	ensureDeterminism = false;
 	frameCount = 0;
 	debug_nextToUpdate = 0;

commit 59f34a15261a5f973ea62823d4ceaffed46af0b4

Fix pasting bailing early when any particle fails to spawn
Broken since 462460b6b3f0, where the break in the old code was thoughtlessly moved to the new code, the issue being that create_part failing doesn't necessarily mean that we're out of particle IDs. I don't think pasting is a hot enough operation for such optimizations to be necessary, so continue should be fine.

src/simulation/Simulation.cpp
@@ -130,7 +130,7 @@ void Simulation::Load(const GameSave *save, bool includePressure, Vec2<int> bloc
 		auto i = create_part(-3, x, y, tempPart.type);
 		if (i == -1)
 		{
-			break;
+			continue;
 		}
 		if (i > parts.lastActiveIndex)
 		{