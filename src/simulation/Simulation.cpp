The-Powder-Toy/The-Powder-Toy

commit 462460b6b3f0907b9b47549d958bfe47f8070f9c

Manage particle lifetime with create/kill_part only
This should fix the long-standing issue of Create/ChangeType callbacks not being called in all cases when particles were created or changed type.

I'm eagerly awaiting reports of the horrendous crashes this will inevitably cause.

src/simulation/Simulation.cpp
@@ -127,15 +127,16 @@ void Simulation::Load(const GameSave *save, bool includePressure, Vec2<int> bloc
 		removeExistingParticles({ x, y });
 
 		// Allocate particle (this location is guaranteed to be empty due to "full scan" logic above)
-		if (pfree == -1)
+		auto i = create_part(-3, x, y, tempPart.type);
+		if (i == -1)
+		{
 			break;
-		auto i = pfree;
-		pfree = parts[i].life;
-		NUM_PARTS += 1;
+		}
 		if (i > parts.lastActiveIndex)
+		{
 			parts.lastActiveIndex = i;
+		}
 		parts[i] = tempPart;
-		elementCount[tempPart.type]++;
 
 
 		switch (parts[i].type)
@@ -977,6 +978,13 @@ int Simulation::parts_avg(int ci, int ni,int t)
 
 void Simulation::clear_sim(void)
 {
+	for (auto i = 0; i <= parts.lastActiveIndex; i++)
+	{
+		if (parts[i].type)
+		{
+			kill_part(i);
+		}
+	}
 	ensureDeterminism = false;
 	frameCount = 0;
 	debug_nextToUpdate = 0;

commit 30942137c753cd23707ba94769025c26ac06fdf3

Fix SPRK sometimes being impossible to paste
Broken since 462460b6b3f0, where Load started using create_part with p == -3, running all sorts of checks against SPRK and sometimes failing.

src/simulation/Simulation.cpp
@@ -1812,7 +1812,7 @@ int Simulation::create_part(int p, int x, int y, int t, int v)
 	if (x<0 || y<0 || x>=XRES || y>=YRES || t<=0 || t>=PT_NUM || !elements[t].Enabled)
 		return -1;
 
-	if (t == PT_SPRK && !(p == -2 && elements[TYP(pmap[y][x])].CtypeDraw))
+	if (t == PT_SPRK && p != -3 && !(p == -2 && elements[TYP(pmap[y][x])].CtypeDraw))
 	{
 		int type = TYP(pmap[y][x]);
 		int index = ID(pmap[y][x]);