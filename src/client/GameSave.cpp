The-Powder-Toy/The-Powder-Toy

commit a38e1c48bbf029257700af957e14787a7d34d03d

Default to identity mapping elements not in the palette for pre-98.0 saves
Newer saves include an element palette which maps save-space element numbers to element identifier strings, see 29189693b381. This is useful because the numbers of custom elements can change, while their identifiers are expected to not change. Not all elements make it into the palette, only the ones that are in use in the save.

98.0 is staged to extend this feature in two ways. First, it'll warn the user of missing custom elements when loading a save that uses such elements, see 36800a76cd97. Second, it'll do a better job of deciding what to put in the element palette, see a13c29875f64.

In order for detection of missing elements to work, a save's palette has to account for every element number used in the save, including built-in elements. To dispel a misunderstanding regarding that last part: yes, including built-in elements is not crucial if the set of built-in elements only ever grows, but this is not guaranteed.

98.0 creates such palettes, but older code didn't, for various reasons. One reason is that the palette at some point wasn't meant to include built-in elements, see e0d982367b71, although this was rectified later in 67b87b1dab9f. Another reason is that not all cases of element numbers encoded in particle properties were considered, see for example f45d0d1683ed and 1f1062408c98.

Palettes in existing saves being thus incomplete didn't use to be a problem because older code would just assume that whatever element number wasn't listed in the palette referred to a built-in element and would just map such save-space element numbers to the same simulation-space element number, hence identity mapping.

However, this approach doesn't cover custom elements whose numbers can change, nor does it cover extra built-in elements added by a mod. Worse, a different mod with different extra built-in elements may map save-space element numbers not listed in the palette to its own extra elements.

src/client/GameSave.cpp
@@ -59,9 +59,12 @@ void GameSave::MapPalette()
 	auto &elements = sd.elements;
 	if(palette.size())
 	{
-		for(int i = 0; i < PT_NUM; i++)
+		if (version >= Version(98, 0))
 		{
-			partMap[i] = 0;
+			for(int i = 0; i < PT_NUM; i++)
+			{
+				partMap[i] = 0;
+			}
 		}
 		for(auto &pi : palette)
 		{

src/common/Version.h
@@ -27,6 +27,21 @@ struct Version
 		return *this < other || *this == other;
 	}
 
+	constexpr bool operator >=(const Version &other) const
+	{
+		return !(*this < other);
+	}
+
+	constexpr bool operator >(const Version &other) const
+	{
+		return !(*this <= other);
+	}
+
+	constexpr bool operator !=(const Version &other) const
+	{
+		return !(*this == other);
+	}
+
 	constexpr size_t operator [](size_t index) const
 	{
 		return components[index];

commit efd3a80611d580393f6755c667a6e1267ab7ab90

Fix missing elements in some older saves not showing up as such
Broken since a38e1c48bbf0, where older saves were allowed to work off the legacy identity mapping of save numbers to element numbers. The problem was that this could leave even those element numbers mapped that were associated by the palette with unknown element identifiers. These elements would then fail to spawn but would not show up as missing in the save preview.

Still not perfect, see the TODO in the diff.

src/client/GameSave.cpp
@@ -52,6 +52,24 @@ GameSave::GameSave(const std::vector<char> &data, bool newWantAuthors)
 
 void GameSave::MapPalette()
 {
+	// - the palette is always right
+	//   - there are palettes with missing entries but there are no palettes with incorrect entries
+	//   - for every (identifier, number) pair in the palette
+	//     - if the identifier is recognized, map the number to the appropriate element
+	//     - if not, map it to 0
+	//       - complain about the identifier if the corresponding number is actually used
+	// - to handle every number not covered by the palette
+	//   - in the case of any 98.0+ save, the palette is comprehensive
+	//     - except for the few cases already handled below
+	//       - e.g RSSS et al not having CarriesTypeIn set properly until 98.2
+	//       - can handle mistakes like this with similar extra code later
+	//     - map any number seen used to 0 and complain about the number
+	//   - in the case of any pre-98.0 save, the palette may not be comprehensive
+	//     - do the legacy identity mapping
+	//       - this is not perfect because it lets numbers slip that are clearly invalid
+	//       - TODO: maybe identity-map only ranges of numbers that are known to have existed
+	//         at the point in time in vanilla when the save was made, based on this->version
+
 	int partMap[PT_NUM];
 	bool ignoreMissingErrors[PT_NUM];
 	for(int i = 0; i < PT_NUM; i++)
@@ -91,11 +109,8 @@ void GameSave::MapPalette()
 						myId = i;
 					}
 				}
-				if (myId)
-				{
-					partMap[pi.second] = myId;
-				}
-				else
+				partMap[pi.second] = myId;
+				if (!myId)
 				{
 					missingElementIdentifiers.insert(pi);
 				}