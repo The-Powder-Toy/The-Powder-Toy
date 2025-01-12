a6af07b028b87691e232de655319e67a0724cf82
takeraparterer <96804457+takeraparterer@users.noreply.github.com>
2025-01-12 11:50:52 +1000

    Whitespace fixes

--- src/simulation/elements/MOTH.cpp
+++ src/simulation/elements/MOTH.cpp
@@ -49,7 +49,7 @@ int moth_search_radius = 10;
 int SDmultiplier = 3; // only check for target element every this many. expand search radius by times this without searching more cells at the cost of accuracy
 int Element_MOTH_graphics(GRAPHICS_FUNC_ARGS)
 {
-	int z = (cpart -> tmp2 - 5) * 16;//speckles!
+	int z = (cpart->tmp2 - 5) * 16;//speckles!
 	*colr += z;
 	*colg += z;
 	*colb += z;
@@ -57,31 +57,31 @@ int Element_MOTH_graphics(GRAPHICS_FUNC_ARGS)
 }
 static void create(ELEMENT_CREATE_FUNC_ARGS)
 {
-	sim -> parts[i].tmp  = 0;
-	sim -> parts[i].tmp2 = sim -> rng.between(0, 10);
-	sim -> parts[i].tmp3 = 0;
-	sim -> parts[i].tmp4 = 0;
-	sim -> parts[i].life = 800 + (sim -> rng.between(-60, 0));
+	sim->parts[i].tmp  = 0;
+	sim->parts[i].tmp2 = sim->rng.between(0, 10);
+	sim->parts[i].tmp3 = 0;
+	sim->parts[i].tmp4 = 0;
+	sim->parts[i].life = 800 + (sim->rng.between(-60, 0));
 }
 int Element_MOTH_update(UPDATE_FUNC_ARGS)
 {
 	auto &sd = SimulationData::CRef();
 	auto &elements = sd.elements;	
-	int r = sim -> pmap[y - 1][x];
+	int r = sim->pmap[y - 1][x];
 	if (r && (elements[TYP(r)].Properties & (TYPE_PART | TYPE_LIQUID)))
 	{
-		sim -> parts[i].tmp4 += 1;
+		sim->parts[i].tmp4 += 1;
 	} else {
-		sim -> parts[i].tmp4 -= 3;
+		sim->parts[i].tmp4 -= 3;
 		if (parts[i].tmp4 < 0)
 		{
-			sim -> parts[i].tmp4 = 0;
+			sim->parts[i].tmp4 = 0;
 		}
 	}
-	sim -> parts[i].life -= 1;
+	sim->parts[i].life -= 1;
 	if (parts[i].life <= 0 || parts[i].tmp4 > 20)
 	{
-		sim -> parts[i].type = PT_DUST;
+		sim->parts[i].type = PT_DUST;
 
 		return 0;
 	}
@@ -95,14 +95,14 @@ int Element_MOTH_update(UPDATE_FUNC_ARGS)
 
             if (nx < 0 || ny < 0 || nx >= XRES || ny >= YRES) continue;
 
-            int r = sim -> pmap[ny][nx];
+            int r = sim->pmap[ny][nx];
 			if (parts[i].tmp4 >= 4)
 			{
 				if (ID(r) == 0)
 				{
-					sim -> parts[i].vx = ((dx < 0) ? parts[i].vx - 1.0f : parts[i].vx + 1.0f);
+					sim->parts[i].vx = ((dx < 0) ? parts[i].vx - 1.0f : parts[i].vx + 1.0f);
 
-					sim -> parts[i].vy = ((dy < 0) ? parts[i].vy - 1.0f : parts[i].vy + 1.0f);
+					sim->parts[i].vy = ((dy < 0) ? parts[i].vy - 1.0f : parts[i].vy + 1.0f);
 
         	        found = true;
 					break;
@@ -113,16 +113,16 @@ int Element_MOTH_update(UPDATE_FUNC_ARGS)
         	    if (r && (TYP(r) == PT_FIRE || TYP(r) == PT_PHOT || TYP(r) == PT_PLNT || (TYP(r) == PT_GEL && parts[ID(r)].tmp >= 5) || TYP(r) == PT_DYST || TYP(r) == PT_YEST || TYP(r) == PT_DUST || TYP(r) == PT_WOOD || TYP(r) == PT_GOO) && parts[i].life <= 1570)
         	    {
 
-					sim -> parts[i].vx = ((dx < 0) ? parts[i].vx - 0.5f : parts[i].vx + 0.5f);
+					sim->parts[i].vx = ((dx < 0) ? parts[i].vx - 0.5f : parts[i].vx + 0.5f);
 
-					sim -> parts[i].vy = ((dy < 0) ? parts[i].vy - 0.5f : parts[i].vy + 0.5f);
+					sim->parts[i].vy = ((dy < 0) ? parts[i].vy - 0.5f : parts[i].vy + 0.5f);
 
         	        found = true;
 					break;
         	    } else if (r && TYP(r) == PT_GAS)
 				{
-					sim -> parts[i].vx = ((dx < 0) ? parts[i].vx + 0.5f : parts[i].vx - 0.5f);
-					sim -> parts[i].vy = ((dy < 0) ? parts[i].vy + 0.5f : parts[i].vy - 0.5f);
+					sim->parts[i].vx = ((dx < 0) ? parts[i].vx + 0.5f : parts[i].vx - 0.5f);
+					sim->parts[i].vy = ((dy < 0) ? parts[i].vy + 0.5f : parts[i].vy - 0.5f);
 					found = true;
 					break;
 				}
@@ -133,73 +133,73 @@ int Element_MOTH_update(UPDATE_FUNC_ARGS)
 	{
 		for (auto ry = -1; ry <= 1; ry++)
 		{
-			int r = sim -> pmap[y+ry][x+rx];
+			int r = sim->pmap[y+ry][x+rx];
 			if (r && (TYP(r) == PT_FIRE || TYP(r) == PT_PHOT || TYP(r) == PT_PLNT || (TYP(r) == PT_GEL && parts[ID(r)].tmp >= 5) || TYP(r) == PT_DYST || TYP(r) == PT_YEST || TYP(r) == PT_DUST || TYP(r) == PT_WOOD || TYP(r) == PT_GOO))
 			{
 				if (parts[i].tmp == 0)
 				{
-					sim -> parts[i].tmp = 5;
+					sim->parts[i].tmp = 5;
 					switch(TYP(r))
 					{
 						case PT_GEL:
 							if (parts[ID(r)].tmp >= 5)
 							{
-							sim -> parts[ID(r)].tmp -= 5;
-							sim -> parts[i].life += 10;
+							sim->parts[ID(r)].tmp -= 5;
+							sim->parts[i].life += 10;
 							}  
 							break;
 						case PT_PLNT:
-							sim -> parts[i].life += 30;
-							sim -> kill_part(ID(r));
+							sim->parts[i].life += 30;
+							sim->kill_part(ID(r));
 							break;
 						case PT_DYST:
-							sim -> parts[i].life += 20;
-							sim -> kill_part(ID(r));
+							sim->parts[i].life += 20;
+							sim->kill_part(ID(r));
 							break;
 						case PT_YEST:
-							sim -> parts[i].life += 30;
-							sim -> kill_part(ID(r));
+							sim->parts[i].life += 30;
+							sim->kill_part(ID(r));
 							break;
 						case PT_DUST:
-							sim -> parts[i].life += 10;
-							sim -> kill_part(ID(r));
+							sim->parts[i].life += 10;
+							sim->kill_part(ID(r));
 							break;
 						case PT_WOOD:
-							sim -> parts[i].life += 10;
-							sim -> kill_part(ID(r));
+							sim->parts[i].life += 10;
+							sim->kill_part(ID(r));
 							break;
 						case PT_GOO:
-							sim -> parts[i].life += 15;
-							sim -> kill_part(ID(r));
+							sim->parts[i].life += 15;
+							sim->kill_part(ID(r));
 							break;
 					}
-					//sim -> kill_part(ID(r));
-					//sim -> parts[i].life = (TYP(r) == PT_GEL)? parts[i].life+30 : parts[i].life+20;
+					//sim->kill_part(ID(r));
+					//sim->parts[i].life = (TYP(r) == PT_GEL)? parts[i].life+30 : parts[i].life+20;
 					if (parts[i].life > 1600)
 					{
-						sim -> parts[i].life = 1600;
+						sim->parts[i].life = 1600;
 					}
 				}
 				
 			}
 		}
 	}
-	sim -> parts[i].tmp -= 1;
+	sim->parts[i].tmp -= 1;
 	if (parts[i].tmp < 0)
 	{
-		sim -> parts[i].tmp = 0;
+		sim->parts[i].tmp = 0;
 	}
 
-	if (parts[i].life > 800 && parts[i].tmp3 == 0 && ID(sim -> pmap[y][x+1]) == 0)
+	if (parts[i].life > 800 && parts[i].tmp3 == 0 && ID(sim->pmap[y][x+1]) == 0)
 	{
-		sim -> create_part(-1, x, y + 1, PT_MEGG);
-		sim -> parts[i].life -= 400;
-		sim -> parts[i].tmp3 = 400;
+		sim->create_part(-1, x, y + 1, PT_MEGG);
+		sim->parts[i].life -= 400;
+		sim->parts[i].tmp3 = 400;
 	}
-	sim -> parts[i].tmp3 = parts[i].tmp3 - 1;
+	sim->parts[i].tmp3 = parts[i].tmp3 - 1;
 	if (parts[i].tmp3 < 0)
 	{
-		sim -> parts[i].tmp3 = 0;
+		sim->parts[i].tmp3 = 0;
 	}
 
 	return 0;
