The-Powder-Toy/The-Powder-Toy

commit 0f1218df0ca3c3dda049823d4c2bc3f169d6403a

Sort out constness of Simulation and Renderer in graphics functions
Mostly. They are now const but only in graphics functions called from secondary Renderers. The primary (main thread) Renderer still allows graphics functions to mutate Simulation; this is required for correct in-PIPE rendering of custom elements.

src/simulation/elements/PIPE.cpp
@@ -4,6 +4,7 @@ extern const std::array<Vec2<int>, 8> Element_PIPE_offsets;
 void Element_PIPE_transformPatchOffsets(Particle &part, const std::array<int, 8> &offsetMap);
 int Element_PIPE_update(UPDATE_FUNC_ARGS);
 int Element_PIPE_graphics(GRAPHICS_FUNC_ARGS);
+static void props_pipe_to_part(const Particle *pipe, Particle *part, bool STOR);
 void Element_PIPE_transfer_pipe_to_part(Simulation * sim, Particle *pipe, Particle *part, bool STOR);
 static void transfer_part_to_pipe(Particle *part, Particle *pipe);
 static void transfer_pipe_to_pipe(Particle *src, Particle *dest, bool STOR);
@@ -368,32 +369,38 @@ int Element_PIPE_graphics(GRAPHICS_FUNC_ARGS)
 		}
 		else
 		{
-			// Temp particle used for graphics.
-			Particle tpart = *cpart;
-
-			// Emulate the graphics of stored particle.
-			memset(cpart, 0, sizeof(Particle));
-			cpart->type = t;
-			cpart->temp = tpart.temp;
-			cpart->life = tpart.tmp2;
-			cpart->tmp = tpart.tmp3;
-			cpart->ctype = tpart.tmp4;
-
+			// We emulate the graphics of the stored particle. We need a const Particle *cpart to pass to the graphics function,
+			// but we don't have a Particle that is populated the way the graphics function expects, so we construct a temporary
+			// one and present that to it.
+			//
+			// Native graphics functions are well-behaved and use the cpart we give them, no questions asked, so we can just have
+			// the Particle on stack. Swapping the pointers in cpart with tpart takes care of passing the particle on stack to the
+			// native graphics function. Lua graphics functions are more complicated to appease: they access particle data through the
+			// particle ID, so not only do we have to give them a correctly populated Particle, it also has to be somewhere in Simulation.
+			// luaGraphicsWrapper takes care of this.
 			RGB<uint8_t> colour = elements[t].Colour;
 			*colr = colour.Red;
 			*colg = colour.Green;
 			*colb = colour.Blue;
-			if (elements[t].Graphics)
+			auto *graphics = elements[t].Graphics;
+			if (graphics)
 			{
-				(*(elements[t].Graphics))(ren, cpart, nx, ny, pixel_mode, cola, colr, colg, colb, firea, firer, fireg, fireb);
+				Particle tpart;
+				props_pipe_to_part(cpart, &tpart, false);
+				auto *prevPipeSubcallCpart = gfctx.pipeSubcallCpart;
+				auto *prevPipeSubcallTpart = gfctx.pipeSubcallTpart;
+				gfctx.pipeSubcallCpart = cpart;
+				gfctx.pipeSubcallTpart = &tpart;
+				cpart = gfctx.pipeSubcallTpart;
+				graphics(GRAPHICS_FUNC_SUBCALL_ARGS);
+				cpart = gfctx.pipeSubcallCpart;
+				gfctx.pipeSubcallCpart = prevPipeSubcallCpart;
+				gfctx.pipeSubcallTpart = prevPipeSubcallTpart;
 			}
 			else
 			{
-				Element::defaultGraphics(ren, cpart, nx, ny, pixel_mode, cola, colr, colg, colb, firea, firer, fireg, fireb);
+				Element::defaultGraphics(GRAPHICS_FUNC_SUBCALL_ARGS);
 			}
-
-			// Restore original particle data.
-			*cpart = tpart;
 		}
 	}
 	else
@@ -422,7 +429,7 @@ int Element_PIPE_graphics(GRAPHICS_FUNC_ARGS)
 	return 0;
 }
 
-void Element_PIPE_transfer_pipe_to_part(Simulation * sim, Particle *pipe, Particle *part, bool STOR)
+static void props_pipe_to_part(const Particle *pipe, Particle *part, bool STOR)
 {
 	auto &sd = SimulationData::CRef();
 	auto &elements = sd.elements;
@@ -431,12 +438,10 @@ void Element_PIPE_transfer_pipe_to_part(Simulation * sim, Particle *pipe, Partic
 	if (STOR)
 	{
 		part->type = TYP(pipe->tmp);
-		pipe->tmp = 0;
 	}
 	else
 	{
 		part->type = TYP(pipe->ctype);
-		pipe->ctype = 0;
 	}
 	part->temp = pipe->temp;
 	part->life = pipe->tmp2;
@@ -451,6 +456,19 @@ void Element_PIPE_transfer_pipe_to_part(Simulation * sim, Particle *pipe, Partic
 	part->tmp2 = 0;
 	part->flags = 0;
 	part->dcolour = 0;
+}
+
+void Element_PIPE_transfer_pipe_to_part(Simulation * sim, Particle *pipe, Particle *part, bool STOR)
+{
+	props_pipe_to_part(pipe, part, STOR);
+	if (STOR)
+	{
+		pipe->tmp = 0;
+	}
+	else
+	{
+		pipe->ctype = 0;
+	}
 }
 
 static void transfer_part_to_pipe(Particle *part, Particle *pipe)

src/graphics/Renderer.cpp
@@ -7,6 +7,7 @@
 #include "simulation/ElementClasses.h"
 #include "simulation/Air.h"
 #include "simulation/gravity/Gravity.h"
+#include "simulation/orbitalparts.h"
 #include <cmath>
 
 std::unique_ptr<VideoBuffer> Renderer::WallIcon(int wallID, Vec2<int> size)
@@ -174,12 +175,17 @@ void Renderer::render_parts()
 	auto &sd = SimulationData::CRef();
 	auto &elements = sd.elements;
 	auto &graphicscache = sd.graphicscache;
+	GraphicsFuncContext gfctx;
+	gfctx.ren = this;
+	gfctx.sim = sim;
+	gfctx.rng.seed(rng());
+	gfctx.pipeSubcallCpart = nullptr;
+	gfctx.pipeSubcallTpart = nullptr;
 	int deca, decr, decg, decb, cola, colr, colg, colb, firea, firer, fireg, fireb, pixel_mode, q, i, t, nx, ny, x, y;
 	int orbd[4] = {0, 0, 0, 0}, orbl[4] = {0, 0, 0, 0};
-	Particle * parts;
 	if(!sim)
 		return;
-	parts = sim->parts;
+	auto *parts = sim->parts;
 	if (gridSize)//draws the grid
 	{
 		for (ny=0; ny<YRES; ny++)
@@ -244,11 +250,11 @@ void Renderer::render_parts()
 				}
 				else if(!(colour_mode & COLOUR_BASC))
 				{
-					auto *graphics = useGraphicsFunction ? elements[t].Graphics : nullptr;
-					auto makeReady = !graphics || graphics(this, &(sim->parts[i]), nx, ny, &pixel_mode, &cola, &colr, &colg, &colb, &firea, &firer, &fireg, &fireb); //That's a lot of args, a struct might be better
-					if (makeReady && useGraphicsFunction)
+					auto *graphics = elements[t].Graphics;
+					auto makeReady = !graphics || graphics(gfctx, &(sim->parts[i]), nx, ny, &pixel_mode, &cola, &colr, &colg, &colb, &firea, &firer, &fireg, &fireb); //That's a lot of args, a struct might be better
+					if (makeReady && sim->useLuaCallbacks)
 					{
-						// I sure hope we locked sd.elementGraphicsMx exclusively
+						// useLuaCallbacks is true so we locked sd.elementGraphicsMx exclusively
 						auto &wgraphicscache = SimulationData::Ref().graphicscache;
 						wgraphicscache[t].isready = 1;
 						wgraphicscache[t].pixel_mode = pixel_mode;
@@ -437,7 +443,7 @@ void Renderer::render_parts()
 				if(pixel_mode & PSPEC_STICKMAN)
 				{
 					int legr, legg, legb;
-					playerst *cplayer;
+					const playerst *cplayer;
 					if(t==PT_STKM)
 						cplayer = &sim->player;
 					else if(t==PT_STKM2)
@@ -625,7 +631,7 @@ void Renderer::render_parts()
 				}
 				if(pixel_mode & PMODE_SPARK)
 				{
-					auto flicker = float(rng()%20);
+					auto flicker = float(gfctx.rng()%20);
 					auto gradv = 4*sim->parts[i].life + flicker;
 					for (x = 0; gradv>0.5; x++) {
 						auto col = RGBA<uint8_t>(
@@ -642,7 +648,7 @@ void Renderer::render_parts()
 				}
 				if(pixel_mode & PMODE_FLARE)
 				{
-					auto flicker = float(rng()%20);
+					auto flicker = float(gfctx.rng()%20);
 					auto gradv = flicker + fabs(parts[i].vx)*17 + fabs(sim->parts[i].vy)*17;
 					BlendPixel({ nx, ny }, RGBA<uint8_t>(colr, colg, colb, int((gradv*4)>255?255:(gradv*4)) ));
 					BlendPixel({ nx+1, ny }, RGBA<uint8_t>(colr, colg, colb,int( (gradv*2)>255?255:(gradv*2)) ));
@@ -664,7 +670,7 @@ void Renderer::render_parts()
 				}
 				if(pixel_mode & PMODE_LFLARE)
 				{
-					auto flicker = float(rng()%20);
+					auto flicker = float(gfctx.rng()%20);
 					auto gradv = flicker + fabs(parts[i].vx)*17 + fabs(parts[i].vy)*17;
 					BlendPixel({ nx, ny }, RGBA<uint8_t>(colr, colg, colb, int((gradv*4)>255?255:(gradv*4)) ));
 					BlendPixel({ nx+1, ny }, RGBA<uint8_t>(colr, colg, colb, int((gradv*2)>255?255:(gradv*2)) ));
@@ -691,7 +697,7 @@ void Renderer::render_parts()
 					int r;
 					float drad = 0.0f;
 					float ddist = 0.0f;
-					sim->orbitalparts_get(parts[i].life, parts[i].ctype, orbd, orbl);
+					orbitalparts_get(parts[i].life, parts[i].ctype, orbd, orbl);
 					for (r = 0; r < 4; r++) {
 						ddist = ((float)orbd[r])/16.0f;
 						drad = (TPT_PI_FLT * ((float)orbl[r]) / 180.0f)*1.41f;
@@ -708,7 +714,7 @@ void Renderer::render_parts()
 					int r;
 					float drad = 0.0f;
 					float ddist = 0.0f;
-					sim->orbitalparts_get(parts[i].life, parts[i].ctype, orbd, orbl);
+					orbitalparts_get(parts[i].life, parts[i].ctype, orbd, orbl);
 					for (r = 0; r < 4; r++) {
 						ddist = ((float)orbd[r])/16.0f;
 						drad = (TPT_PI_FLT * ((float)orbl[r]) / 180.0f)*1.41f;

src/graphics/Renderer.h
@@ -13,6 +13,17 @@
 
 class RenderPreset;
 class Simulation;
+class Renderer;
+struct Particle;
+
+struct GraphicsFuncContext
+{
+	const Renderer *ren;
+	const Simulation *sim;
+	RNG rng;
+	const Particle *pipeSubcallCpart;
+	Particle *pipeSubcallTpart;
+};
 
 int HeatToColour(float temp);
 
@@ -43,7 +54,7 @@ public:
 
 	RNG rng;
 
-	Simulation * sim;
+	const Simulation *sim;
 
 	std::vector<unsigned int> render_modes;
 	unsigned int render_mode;
@@ -125,7 +136,7 @@ public:
 
 	static std::unique_ptr<VideoBuffer> WallIcon(int wallID, Vec2<int> size);
 
-	Renderer(Simulation * sim);
+	Renderer(Simulation *newSim);
 
 #define RENDERER_TABLE(name) \
 	static std::vector<RGB<uint8_t>> name; \
@@ -143,8 +154,6 @@ public:
 	RENDERER_TABLE(firwTable)
 #undef RENDERER_TABLE
 	static void PopulateTables();
-
-	bool useGraphicsFunction = false;
 
 private:
 	int gridSize;

src/graphics/RendererBasic.cpp
@@ -224,8 +224,8 @@ void Renderer::PopulateTables()
 	}
 }
 
-Renderer::Renderer(Simulation * sim):
-	sim(NULL),
+Renderer::Renderer(Simulation *newSim):
+	sim(newSim),
 	render_mode(0),
 	colour_mode(0),
 	display_mode(0),
@@ -245,8 +245,6 @@ Renderer::Renderer(Simulation * sim):
 	gridSize(0)
 {
 	PopulateTables();
-
-	this->sim = sim;
 
 	memset(fire_r, 0, sizeof(fire_r));
 	memset(fire_g, 0, sizeof(fire_g));

src/gui/game/GOLTool.cpp
@@ -23,14 +23,13 @@ class GOLWindow: public ui::Window
 	ui::Button *highColourButton, *lowColourButton;
 	ui::Textbox *nameField, *ruleField;
 	GameModel &gameModel;
-	Simulation *sim;
 	int toolSelection;
 
 	void updateGradient();
 	void validate();
 
 public:
-	GOLWindow(GameModel &gameModel, Simulation *sim, int toolSelection, int rule, RGB<uint8_t> colour1, RGB<uint8_t> colour2);
+	GOLWindow(GameModel &gameModel, int toolSelection, int rule, RGB<uint8_t> colour1, RGB<uint8_t> colour2);
 
 	virtual ~GOLWindow()
 	{}
@@ -39,12 +38,11 @@ public:
 	void OnTryExit(ExitMethod method) override;
 };
 
-GOLWindow::GOLWindow(GameModel &gameModel_, Simulation *sim_, int toolSelection, int rule, RGB<uint8_t> colour1, RGB<uint8_t> colour2):
+GOLWindow::GOLWindow(GameModel &gameModel_, int toolSelection, int rule, RGB<uint8_t> colour1, RGB<uint8_t> colour2):
 	ui::Window(ui::Point(-1, -1), ui::Point(200, 108)),
 	highColour(colour1.WithAlpha(0xFF)),
 	lowColour(colour2.WithAlpha(0xFF)),
 	gameModel(gameModel_),
-	sim(sim_),
 	toolSelection(toolSelection)
 {
 	highColour.Alpha = 255;
@@ -205,5 +203,5 @@ void GOLWindow::OnDraw()
 
 void GOLTool::OpenWindow(Simulation *sim, int toolSelection, int rule, RGB<uint8_t> colour1, RGB<uint8_t> colour2)
 {
-	new GOLWindow(gameModel, sim, toolSelection, rule, colour1, colour2);
+	new GOLWindow(gameModel, toolSelection, rule, colour1, colour2);
 }

src/gui/game/GameModel.cpp
@@ -56,8 +56,8 @@ GameModel::GameModel():
 	decoSpace(0)
 {
 	sim = new Simulation();
+	sim->useLuaCallbacks = true;
 	ren = new Renderer(sim);
-	ren->useGraphicsFunction = true;
 
 	activeTools = regularToolset;
 

src/lua/LuaScriptInterface.cpp
@@ -3491,11 +3491,21 @@ static int luaUpdateWrapper(UPDATE_FUNC_ARGS)
 
 static int luaGraphicsWrapper(GRAPHICS_FUNC_ARGS)
 {
+	if (!gfctx.sim->useLuaCallbacks)
+	{
+		return Element::defaultGraphics(GRAPHICS_FUNC_SUBCALL_ARGS);
+	}
 	auto *luacon_ci = static_cast<LuaScriptInterface *>(commandInterface);
 	if (lua_gr_func[cpart->type])
 	{
+		auto *pipeSubcallWcpart = gfctx.pipeSubcallCpart ? luacon_sim->parts + (gfctx.pipeSubcallCpart - gfctx.sim->parts) : nullptr;
+		if (pipeSubcallWcpart)
+		{
+			std::swap(*pipeSubcallWcpart, *gfctx.pipeSubcallTpart);
+			cpart = pipeSubcallWcpart;
+		}
 		int cache = 0, callret;
-		int i = cpart - ren->sim->parts; // pointer arithmetic be like
+		int i = cpart - gfctx.sim->parts; // pointer arithmetic be like
 		lua_rawgeti(luacon_ci->l, LUA_REGISTRYINDEX, lua_gr_func[cpart->type]);
 		lua_pushinteger(luacon_ci->l, i);
 		lua_pushinteger(luacon_ci->l, *colr);
@@ -3530,6 +3540,10 @@ static int luaGraphicsWrapper(GRAPHICS_FUNC_ARGS)
 				*fireb = luaL_optint(luacon_ci->l, -1, *fireb);
 			}
 			lua_pop(luacon_ci->l, 10);
+		}
+		if (pipeSubcallWcpart)
+		{
+			std::swap(*pipeSubcallWcpart, *gfctx.pipeSubcallTpart);
 		}
 		return cache;
 	}

src/simulation/Element.h
@@ -8,6 +8,7 @@
 
 class Simulation;
 class Renderer;
+struct GraphicsFuncContext;
 class VideoBuffer;
 struct Particle;
 class Element

commit 3ee6741fcbf0e158392e54da970efc2013f091f6

Fix PIPE rendering garbage when carrying PIPE
Broken since 0f1218df0ca3 where I neglected to port a memset over.

src/simulation/elements/PIPE.cpp
@@ -381,7 +381,7 @@ int Element_PIPE_graphics(GRAPHICS_FUNC_ARGS)
 			auto *graphics = elements[t].Graphics;
 			if (graphics)
 			{
-				Particle tpart;
+				Particle tpart{};
 				props_pipe_to_part(cpart, &tpart, false);
 				auto *prevPipeSubcallCpart = gfctx.pipeSubcallCpart;
 				auto *prevPipeSubcallTpart = gfctx.pipeSubcallTpart;