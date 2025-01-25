53230ee102e111409f496e3525dc0402564988cd
Tamás Bálint Misius <lbphacker@gmail.com>
2025-01-25 19:38:32 +0100

    Disable fake newer version check for stable builds
    It's really annoying when a stable workflow fails because nextVersion didn't get updated, whereas we actively seek out this variable when planning a new version between stables.

--- src/client/GameSave.cpp
+++ src/client/GameSave.cpp
@@ -18,7 +18,7 @@
 
 constexpr auto currentVersion = UPSTREAM_VERSION.displayVersion;
 constexpr auto nextVersion = Version(99, 1);
-static_assert(nextVersion >= currentVersion);
+static_assert(!ALLOW_FAKE_NEWER_VERSION || nextVersion >= currentVersion);
 
 constexpr auto effectiveVersion = ALLOW_FAKE_NEWER_VERSION ? nextVersion : currentVersion;
 
