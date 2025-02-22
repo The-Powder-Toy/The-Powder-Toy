53a1f8d8bc14716c17736671db106cb535b2c6a5
jacob1 <jfu614@gmail.com>
2025-02-21 20:53:37 -0500

    Version 99.3 (Build 3384)

--- src/client/GameSave.cpp
+++ src/client/GameSave.cpp
@@ -17,7 +17,7 @@
 #include <algorithm>
 
 constexpr auto currentVersion = UPSTREAM_VERSION.displayVersion;
-constexpr auto nextVersion = Version(99, 2);
+constexpr auto nextVersion = Version(99, 3);
 static_assert(!ALLOW_FAKE_NEWER_VERSION || nextVersion >= currentVersion);
 
 constexpr auto effectiveVersion = ALLOW_FAKE_NEWER_VERSION ? nextVersion : currentVersion;
