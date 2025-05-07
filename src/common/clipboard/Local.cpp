6784d8a10f5cfd6592ca320169734273932c6819
Tamás Bálint Misius <lbphacker@gmail.com>
2025-05-07 18:32:41 +0200

    Share object files between powder/render/font
    I'm not sure why we weren't using static_libraries to begin with; most likely because the object files (as opposed to the sources) were not possible to share due to different sets of macros being defined in cpp_args, see 3a591b853945 to 4f0c365e05a2.

---
+++ src/common/clipboard/Local.cpp
@@ -0,0 +1,36 @@
+#include "Clipboard.h"
+#include "client/GameSave.h"
+
+namespace Clipboard
+{
+	void SetClipboardData(std::unique_ptr<GameSave> data)
+	{
+	}
+
+	const GameSave *GetClipboardData()
+	{
+		return nullptr;
+	}
+
+	void Init()
+	{
+	}
+
+	bool GetEnabled()
+	{
+		return false;
+	}
+
+	void SetEnabled(bool)
+	{
+	}
+
+	void RecreateWindow()
+	{
+	}
+
+	std::optional<String> Explanation()
+	{
+		return std::nullopt;
+	}
+}
--- src/common/clipboard/Null.cpp
+++ src/common/clipboard/Null.cpp
@@ -1,36 +1,12 @@
 #include "Clipboard.h"
-#include "client/GameSave.h"
 
 namespace Clipboard
 {
-	void SetClipboardData(std::unique_ptr<GameSave> data)
-	{
-	}
-
-	const GameSave *GetClipboardData()
-	{
-		return nullptr;
-	}
-
 	void Init()
-	{
-	}
-
-	bool GetEnabled()
-	{
-		return false;
-	}
-
-	void SetEnabled(bool)
 	{
 	}
 
 	void RecreateWindow()
 	{
-	}
-
-	std::optional<String> Explanation()
-	{
-		return std::nullopt;
 	}
 }
