1f04a7dfb7d65575c3b252d86e7784907bc7603c
Tamás Bálint Misius <lbphacker@gmail.com>
2025-06-30 20:53:01 +0200

    Fix the "fullscreen" parameter of tpt.screenshot being inverted
    Broken since c8c0f9087152, where this got accidentally inverted.

--- src/gui/game/GameView.cpp
+++ src/gui/game/GameView.cpp
@@ -940,11 +940,11 @@ ByteString GameView::TakeScreenshot(int captureUI, int fileType)
 	std::unique_ptr<VideoBuffer> screenshot;
 	if (captureUI)
 	{
-		screenshot = std::make_unique<VideoBuffer>(*rendererFrame);
+		screenshot = std::make_unique<VideoBuffer>(ui::Engine::Ref().g->DumpFrame());
 	}
 	else
 	{
-		screenshot = std::make_unique<VideoBuffer>(ui::Engine::Ref().g->DumpFrame());
+		screenshot = std::make_unique<VideoBuffer>(*rendererFrame);
 	}
 
 	ByteString filename;
