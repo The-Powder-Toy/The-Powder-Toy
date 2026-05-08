The-Powder-Toy/The-Powder-Toy

commit 6127dd278c960dd7bc1aeddb39d7bfc5de554816

Factor transitions out of Simulation::UpdateParticles

src/simulation/Simulation.h
@@ -285,4 +285,5 @@ private:
 	};
 	void MovementPhase(int i, Neighbourhood neighbourhood);
 	Neighbourhood GetNeighbourhood(int i) const;
+	bool TransitionPhase(int i, const Neighbourhood &neighbourhood);
 };

commit 1774110c63807f865a4473f5a7d5b98a9825a972

Possibly fix a performance issue
Introduced by 6127dd278c96 and d22bdaac7c4a, which factored out some code from UpdateParticles, to make it slightly more readable. Sadly, this also exposed them to the rest of the codebase by making them globally visible, which prevents the compiler from keeping them inlined. Only the linker has enough information to know that the new functions are not used anywhere else, and it's too dumb to inline them back in.

Therefore, hide these functions in Simulation.cpp. Sadly, this necessitates removing them from the header, but that then prevents them from accessing Simulation internals. Therefore, create an object of SimulationImpl instead at runtime and hide it behind a Simulation pointer from the rest of the codebase. There are other solutions to this problem that don't involve what is essentially pimpl, but they're too ugly for my taste.

On my system, this indeed fixes the ~10% performance degradation caused by the aforementioned two commits.

src/PowderToyRenderer.cpp
@@ -43,9 +43,9 @@ int main(int argc, char *argv[])
 			throw e;
 	}
 
-	Simulation * sim = new Simulation();
+	auto sim = Simulation::Factory();
 	Renderer * ren = new Renderer();
-	ren->sim = sim;
+	ren->sim = sim.get();
 
 	if (gameSave)
 	{

src/gui/game/GameModel.cpp
@@ -68,7 +68,7 @@ GameModel::GameModel(GameView *newView):
 	decoSpace(DECOSPACE_SRGB),
 	view(newView)
 {
-	sim = new Simulation();
+	sim = Simulation::Factory();
 	sim->useLuaCallbacks = true;
 	ren = new Renderer();
 
@@ -235,7 +235,7 @@ GameModel::~GameModel()
 		prefs.Set("Decoration.Alpha", (int)colour.Alpha);
 	}
 
-	delete sim;
+	sim.reset();
 	delete ren;
 	//if(activeTools)
 	//	delete[] activeTools;
@@ -929,7 +929,7 @@ void GameModel::SetSaveFile(std::unique_ptr<SaveFile> newSave, bool invertInclud
 
 Simulation * GameModel::GetSimulation()
 {
-	return sim;
+	return sim.get();
 }
 
 Renderer * GameModel::GetRenderer()

src/gui/game/GameModel.h
@@ -68,7 +68,7 @@ private:
 	void DeselectTool(ByteString identifier);
 	void InitTools();
 
-	Simulation * sim;
+	std::unique_ptr<Simulation> sim;
 	bool paused = false;
 	int queuedFrames = 0;
 	Renderer * ren;

src/simulation/SaveRenderer.cpp
@@ -10,7 +10,7 @@
 
 SaveRenderer::SaveRenderer()
 {
-	sim = std::make_unique<Simulation>();
+	sim = Simulation::Factory();
 	ren = std::make_unique<Renderer>();
 	ren->sim = sim.get();
 }

src/simulation/Simulation.cpp
@@ -22,6 +22,26 @@
 #include <set>
 #include <stack>
 
+namespace
+{
+	struct SimulationImpl : public Simulation
+	{
+		struct Neighbourhood
+		{
+			std::array<int, 8> surround;
+			int surround_space = 0;
+			int nt = 0; //if nt is greater than 1 after this, then there is a particle around the current particle, that is NOT the current particle's type, for water movement.
+			float pGravX = 0;
+			float pGravY = 0;
+		};
+		void MovementPhase(int i, Neighbourhood neighbourhood);
+		Neighbourhood GetNeighbourhood(int i) const;
+		bool TransitionPhase(int i, const Neighbourhood &neighbourhood);
+
+		void UpdateParticles(int start, int end) final override;
+	};
+}
+
 static float remainder_p(float x, float y)
 {
 	return std::fmod(x, y) + (x>=0 ? 0 : y);
@@ -1696,22 +1716,6 @@ Simulation::GetNormalResult Simulation::get_normal(int pt, int x, int y, float d
 	return { true, nx, ny, lx, ly, rx, ry };
 }
 
-
-
-template<bool PhotoelectricEffect, class Sim>
-void PhotoelectricEffectHelper(Sim &sim, int x, int y);
-
-template<>
-void PhotoelectricEffectHelper<false, const Simulation>(const Simulation &sim, int x, int y)
-{
-}
-
-template<>
-void PhotoelectricEffectHelper<true, Simulation>(Simulation &sim, int x, int y)
-{
-	sim.photoelectric_effect(x, y);
-}
-
 template<bool PhotoelectricEffect, class Sim>
 Simulation::GetNormalResult Simulation::get_normal_interp(Sim &sim, int pt, float x0, float y0, float dx, float dy)
 {
@@ -1735,8 +1739,11 @@ Simulation::GetNormalResult Simulation::get_normal_interp(Sim &sim, int pt, floa
 	if (i >= NORMAL_INTERP)
 		return { false };
 
-	if (pt == PT_PHOT)
-		PhotoelectricEffectHelper<PhotoelectricEffect, Sim>(sim, x, y);
+	if constexpr (PhotoelectricEffect)
+	{
+		if (pt == PT_PHOT)
+			sim.photoelectric_effect(x, y);
+	}
 
 	return sim.get_normal(pt, x, y, dx, dy);
 }
@@ -2246,7 +2253,12 @@ Simulation::PlanMoveResult Simulation::PlanMove(Sim &sim, int i, int x, int y)
 template
 Simulation::PlanMoveResult Simulation::PlanMove<false, const Simulation>(const Simulation &sim, int i, int x, int y);
 
-Simulation::Neighbourhood Simulation::GetNeighbourhood(int i) const
+std::unique_ptr<Simulation> Simulation::Factory()
+{
+	return std::make_unique<SimulationImpl>();
+}
+
+SimulationImpl::Neighbourhood SimulationImpl::GetNeighbourhood(int i) const
 {
 	auto t = parts[i].type;
 	auto x = int(parts[i].x + 0.5f);
@@ -2276,7 +2288,7 @@ Simulation::Neighbourhood Simulation::GetNeighbourhood(int i) const
 	return n;
 }
 
-void Simulation::UpdateParticles(int start, int end)
+void SimulationImpl::UpdateParticles(int start, int end)
 {
 	//the main particle loop function, goes over all particles.
 	auto &sd = SimulationData::CRef();
@@ -2412,7 +2424,7 @@ void Simulation::UpdateParticles(int start, int end)
 	}
 }
 
-bool Simulation::TransitionPhase(int i, const Neighbourhood &neighbourhood)
+bool SimulationImpl::TransitionPhase(int i, const Neighbourhood &neighbourhood)
 {
 	auto &sd = SimulationData::CRef();
 	auto &elements = sd.elements;
@@ -2860,7 +2872,7 @@ bool Simulation::TransitionPhase(int i, const Neighbourhood &neighbourhood)
 	return transitionOccurred;
 }
 
-void Simulation::MovementPhase(int i, Neighbourhood neighbourhood)
+void SimulationImpl::MovementPhase(int i, Neighbourhood neighbourhood)
 {
 	auto &sd = SimulationData::CRef();
 	auto &elements = sd.elements;

src/simulation/Simulation.h
@@ -218,7 +218,7 @@ public:
 	int is_wire_off(int x, int y);
 	void set_emap(int x, int y);
 	int parts_avg(int ci, int ni, int t);
-	void UpdateParticles(int start, int end); // Dispatches an update to the range [start, end).
+	virtual void UpdateParticles(int start, int end) = 0; // Dispatches an update to the range [start, end).
 	void SimulateGoL();
 	void RecalcFreeParticles(bool do_life_dec);
 	void CheckStacking();
@@ -266,11 +266,13 @@ public:
 	static GetNormalResult get_normal_interp(Sim &sim, int pt, float x0, float y0, float dx, float dy);
 	void clear_sim();
 	Simulation();
-	~Simulation();
+	virtual ~Simulation();
 
 	void EnableNewtonianGravity(bool enable);
 
 	FrameTime *frameTime = nullptr;
+
+	static std::unique_ptr<Simulation> Factory();
 
 private:
 	CoordStack& getCoordStackSingleton();
@@ -278,16 +280,4 @@ private:
 	void ResetNewtonianGravity(GravityInput newGravIn, GravityOutput newGravOut);
 	void DispatchNewtonianGravity();
 	void UpdateGravityMask();
-
-	struct Neighbourhood
-	{
-		std::array<int, 8> surround;
-		int surround_space = 0;
-		int nt = 0; //if nt is greater than 1 after this, then there is a particle around the current particle, that is NOT the current particle's type, for water movement.
-		float pGravX = 0;
-		float pGravY = 0;
-	};
-	void MovementPhase(int i, Neighbourhood neighbourhood);
-	Neighbourhood GetNeighbourhood(int i) const;
-	bool TransitionPhase(int i, const Neighbourhood &neighbourhood);
 };