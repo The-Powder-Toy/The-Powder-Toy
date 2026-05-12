The-Powder-Toy/The-Powder-Toy

commit 3028192c74e9f0254fca12122fe7bc47d84ba6b9

Fix odd zero block values at the edges of areas being pasted over
Pastes are grown by one cell when they're nudged (their particles are moved by some amount that isn't a multiple of CELL), but the new blocks they acquire this way aren't filled with any meaningful data, only zeros. These zeros are then propagated to the simulation, overwriting existing good values.

src/client/GameSave.cpp
@@ -289,6 +289,7 @@ void GameSave::Transform(Mat2<int> transform, Vec2<int> nudge)
 
 	// Grow as needed.
 	assert((Vec2{ CELL, CELL }.OriginRect().Contains(nudge)));
+	blockContent = newBlockS.OriginRect();
 	if (nudge.X) newBlockS.X += 1;
 	if (nudge.Y) newBlockS.Y += 1;
 

src/client/GameSave.h
@@ -69,6 +69,7 @@ class GameSave
 
 public:
 	Vec2<int> blockSize = { 0, 0 };
+	Rect<int> blockContent = { { 0, 0 }, { 0, 0 } };
 	bool fromNewerVersion = false;
 	bool fromUnstableVersion = false;
 	Version<2> version{};

src/simulation/Simulation.cpp
@@ -290,7 +290,7 @@ void Simulation::Load(const GameSave *save, bool includePressure, Vec2<int> bloc
 		}
 	}
 	auto useGravityMaps = save->hasGravityMaps && grav;
-	auto targetBlocks = RectSized(blockP, save->blockSize) & CELLS.OriginRect();
+	auto targetBlocks = RectSized(blockP + save->blockContent.TopLeft(), save->blockContent.size) & CELLS.OriginRect();
 	for (auto bpos : targetBlocks)
 	{
 		auto spos = bpos - blockP;

commit 1a02a3b46128d714fc323e4400cc75a4ab3547f2

Fix old saves not getting their air-block maps approximated
Broken by 3028192c74e9, where I forgot that old saves existed.

src/client/GameSave.cpp
@@ -239,6 +239,7 @@ void GameSave::Expand(const std::vector<char> &data)
 void GameSave::setSize(Vec2<int> newBlockSize)
 {
 	blockSize = newBlockSize;
+	blockContent = blockSize.OriginRect();
 
 	particlesCount = 0;
 	particles = std::vector<Particle>(NPART);