d29ef71af78781e5a2d9c0ee60e46024b338c22c
Tamás Bálint Misius <lbphacker@gmail.com>
2026-06-01 08:18:58 +0200

    Declare some things in ElementDefs.h
    Strictly speaking, this is not an issue because these always get declared by the time this header ultimately gets included. It is very annoying to see warnings about undeclared types when looking at *headers* on their own, without the context of the translation unit they're included in, that in turn include this header.

--- src/simulation/ElementDefs.h
+++ src/simulation/ElementDefs.h
@@ -38,6 +38,10 @@ constexpr auto FLAG_SKIPMOVE      = UINT32_C(0x00000002); // skip movement for o
 constexpr auto FLAG_MOVABLE       = UINT32_C(0x00000008); // compatibility with old saves (moving SPNG), only applies to SPNG
 constexpr auto FLAG_PHOTDECO      = UINT32_C(0x00000008); // compatibility with old saves (decorated photons), only applies to PHOT. Having the same value as FLAG_MOVABLE is fine because they apply to different elements, and this saves space for future flags,
 
+class Parts;
+class Simulation;
+struct GraphicsFuncContext;
+struct Particle;
 
 #define UPDATE_FUNC_ARGS Simulation* sim, int i, int x, int y, int surround_space, int nt, Parts &parts, int pmap[YRES][XRES]
 #define UPDATE_FUNC_SUBCALL_ARGS sim, i, x, y, surround_space, nt, parts, pmap
@@ -88,4 +92,3 @@ constexpr bool InCellBounds(int x, int y)
 }
 
 struct playerst;
-class Parts;
