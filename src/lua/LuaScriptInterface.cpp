The-Powder-Toy/The-Powder-Toy

commit a715f5d71af0ee65a80d2e3b283e6b87384150f2

Use RGB for constants and gradients, other misc changes

src/lua/LuaScriptInterface.cpp
@@ -1865,19 +1865,10 @@ int LuaScriptInterface::simulation_decoColor(lua_State * l)
 		color = RGBA<uint8_t>::Unpack(luaL_optnumber(l, 1, 0xFFFF0000));
 	else
 	{
-		color.Red = luaL_optint(l, 1, 255);
-		color.Green = luaL_optint(l, 2, 255);
-		color.Blue = luaL_optint(l, 3, 255);
-		color.Alpha = luaL_optint(l, 4, 255);
-
-		if (color.Red < 0) color.Red = 0;
-		if (color.Red > 255) color.Red = 255;
-		if (color.Green < 0) color.Green = 0;
-		if (color.Green > 255) color.Green = 255;
-		if (color.Blue < 0) color.Blue = 0;
-		if (color.Blue > 255) color.Blue = 255;
-		if (color.Alpha < 0) color.Alpha = 0;
-		if (color.Alpha > 255) color.Alpha = 255;
+		color.Red   = std::clamp(luaL_optint(l, 1, 255), 0, 255);
+		color.Green = std::clamp(luaL_optint(l, 2, 255), 0, 255);
+		color.Blue  = std::clamp(luaL_optint(l, 3, 255), 0, 255);
+		color.Alpha = std::clamp(luaL_optint(l, 4, 255), 0, 255);
 	}
 	luacon_model->SetColourSelectorColour(ui::Colour(color.Red, color.Green, color.Blue, color.Alpha));
 	return 0;

commit 62d97a3c5f05b2d68044e750881ab7249f61e1b2

Fix Color element property returning uninitialized alpha values
Broken since a715f5d71af0, where the element property was originally a pixel (u32) but was replaced with RGB (3*u8), but the Lua API code that manages it wasn't updated accordingly. If that code had been type-safe (it wasn't, and it isn't today), this mistake would have been impossible. I'll have to see if I can do something about that.

src/lua/LuaScriptInterface.cpp
@@ -276,7 +276,7 @@ void LuaGetProperty(lua_State *L, StructProperty property, intptr_t propertyAddr
 			break;
 		}
 		case StructProperty::Colour:
-			lua_pushinteger(L, *((unsigned int*)propertyAddress));
+			lua_pushinteger(L, ((RGB*)propertyAddress)->Pack());
 			break;
 		case StructProperty::Removed:
 			lua_pushnil(L);
@@ -317,7 +317,7 @@ void LuaSetProperty(lua_State *L, StructProperty property, intptr_t propertyAddr
 			*((String*)propertyAddress) = tpt_lua_checkString(L, stackPos);
 			break;
 		case StructProperty::Colour:
-			*((unsigned int*)propertyAddress) = int32_truncate(luaL_checknumber(L, stackPos));
+			*((RGB*)propertyAddress) = RGB::Unpack(int32_truncate(luaL_checknumber(L, stackPos)));
 			break;
 		case StructProperty::Removed:
 			break;