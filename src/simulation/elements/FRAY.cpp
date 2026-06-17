a04df331931555c1bbc275276558347bd382e15f
Ksawi <66205430+Ksawi999@users.noreply.github.com>
2026-06-17 17:07:00 +0200

    Set default length to 11 to match old behavior

--- src/simulation/elements/FRAY.cpp
+++ src/simulation/elements/FRAY.cpp
@@ -54,7 +54,7 @@ static int update(UPDATE_FUNC_ARGS)
 	if (parts[i].tmp > 0)
 		curlen = parts[i].tmp;
 	else
-		curlen = 10;
+		curlen = 11;
 	for (auto rx = -1; rx <= 1; rx++)
 	{
 		for (auto ry = -1; ry <= 1; ry++)
