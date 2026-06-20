The-Powder-Toy/The-Powder-Toy

commit 259c4f00c378f11b1c895ed1b5a516c6ce61c7ed

Make Parts::lastActiveIndex more idiomatic
Also rename it to just "active". It now holds the size of the active range, rather than the index of the last item in the range, thereby fixing all usage sites which had unidiomatic code due to this value being off by one, such as having to use (auto i = 0; i <= lastActiveIndex; ++i) in loops and (data, data + lastActiveIndex + 1) in STL functions expecting ranges. The empty range of active particles (i.e. no active particles) can now also be represented properly.

This also fixes sim.resetTemp skipping the last active particle if it exists (it did the comparison wrong).

src/simulation/Simulation.h
@@ -38,7 +38,7 @@ struct Parts
 {
 	std::array<Particle, NPART> data;
 	// initialized in clear_sim
-	int lastActiveIndex;
+	int active;
 
 	operator const Particle *() const
 	{
@@ -59,8 +59,8 @@ struct Parts
 
 	Parts &operator =(const Parts &other)
 	{
-		std::copy(other.data.begin(), other.data.begin() + other.lastActiveIndex + 1, data.begin());
-		lastActiveIndex = other.lastActiveIndex;
+		std::copy(other.data.begin(), other.data.begin() + other.active, data.begin());
+		active = other.active;
 		return *this;
 	}
 

src/debug/DebugParts.cpp
@@ -18,7 +18,7 @@ void DebugParts::Draw()
 	Graphics * g = ui::Engine::Ref().g;
 
 	int x = 0, y = 0, lpx = 0, lpy = 0;
-	String info = String::Build(sim->parts.lastActiveIndex, "/", NPART, " (", Format::Precision((float)sim->parts.lastActiveIndex/(NPART)*100.0f, 2), "%)");
+	String info = String::Build(sim->parts.active, "/", NPART, " (", Format::Precision((float)sim->parts.active/(NPART)*100.0f, 2), "%)");
 	for (int i = 0; i < NPART; i++)
 	{
 		if (sim->parts[i].type)
@@ -26,7 +26,7 @@ void DebugParts::Draw()
 		else
 			g->AddPixel({ x, y }, 0x000000_rgb .WithAlpha(180));
 
-		if (i == sim->parts.lastActiveIndex)
+		if (i == sim->parts.active - 1)
 		{
 			lpx = x;
 			lpy = y;

src/graphics/Renderer.cpp
@@ -266,7 +266,7 @@ void Renderer::render_parts()
 			}
 	}
 	stats.foundParticles = 0;
-	for(i = 0; i<=sim->parts.lastActiveIndex; i++) {
+	for(i = 0; i < sim->parts.active; i++) {
 		if (sim->parts[i].type && sim->parts[i].type >= 0 && sim->parts[i].type < PT_NUM) {
 			t = sim->parts[i].type;
 
@@ -803,7 +803,7 @@ void Renderer::render_parts()
 							type = PT_PRTO;
 						else if (type == PT_PRTO)
 							type = PT_PRTI;
-						for (int z = 0; z <= sim->parts.lastActiveIndex; z++)
+						for (int z = 0; z < sim->parts.active; z++)
 						{
 							if (parts[z].type == type)
 							{
@@ -1408,7 +1408,7 @@ void Renderer::AdjustHdispLimit()
 		if (colorMode & COLOUR_HEAT)
 		{
 			auto &sd = SimulationData::CRef();
-			for (int i = 0; i <= sim->parts.lastActiveIndex; ++i)
+			for (int i = 0; i < sim->parts.active; ++i)
 			{
 				auto t = sim->parts[i].type;
 				if (t > 0 && t < PT_NUM)

src/lua/LuaSimulation.cpp
@@ -932,7 +932,7 @@ static int resetTemp(lua_State *L)
 	auto &sd = SimulationData::CRef();
 	auto &elements = sd.elements;
 	bool onlyConductors = luaL_optint(L, 1, 0);
-	for (int i = 0; i < sim->parts.lastActiveIndex; i++)
+	for (int i = 0; i < sim->parts.active; i++)
 	{
 		if (sim->parts[i].type && (!onlyConductors || !sd.IsHeatInsulator(sim->parts[i])))
 		{
@@ -1387,7 +1387,7 @@ static int brush(lua_State *L)
 static int partsClosure(lua_State *L)
 {
 	auto *lsi = GetLSI();
-	for (int i = lua_tointeger(L, lua_upvalueindex(1)); i <= lsi->sim->parts.lastActiveIndex; ++i)
+	for (int i = lua_tointeger(L, lua_upvalueindex(1)); i < lsi->sim->parts.active; ++i)
 	{
 		if (lsi->sim->parts[i].type)
 		{

src/simulation/Air.cpp
@@ -467,7 +467,7 @@ void Air::ApproximateBlockAirMaps()
 {
 	auto &sd = SimulationData::CRef();
 	auto &elements = sd.elements;
-	for (int i = 0; i <= sim.parts.lastActiveIndex; i++)
+	for (int i = 0; i < sim.parts.active; i++)
 	{
 		int type = sim.parts[i].type;
 		if (!type)

src/simulation/Editing.cpp
@@ -25,7 +25,7 @@ std::unique_ptr<Snapshot> Simulation::CreateSnapshot() const
 	snap->BlockAirH      .insert   (snap->BlockAirH      .begin(), &air->bmap_blockairh[0][0], &air->bmap_blockairh[0][0] + NCELL);
 	snap->FanVelocityX   .insert   (snap->FanVelocityX   .begin(), &fvx [0][0]      , &fvx [0][0] + NCELL);
 	snap->FanVelocityY   .insert   (snap->FanVelocityY   .begin(), &fvy [0][0]      , &fvy [0][0] + NCELL);
-	snap->Particles      .insert   (snap->Particles      .begin(), &parts  [0]      , &parts  [0] + parts.lastActiveIndex + 1);
+	snap->Particles      .insert   (snap->Particles      .begin(), &parts  [0]      , &parts  [0] + parts.active);
 	snap->PortalParticles.insert   (snap->PortalParticles.begin(), &portalp[0][0][0], &portalp[0][0][0] + CHANNELS * 8 * 80);
 	snap->WirelessData   .insert   (snap->WirelessData   .begin(), &wireless[0][0]  , &wireless[0][0] + CHANNELS * 2);
 	snap->stickmen       .insert   (snap->stickmen       .begin(), &fighters[0]     , &fighters[0] + MAX_FIGHTERS);
@@ -80,7 +80,7 @@ void Simulation::Restore(const Snapshot &snap)
 	signs = snap.signs;
 	frameCount = snap.FrameCount;
 	rng.state(snap.RngState);
-	parts.lastActiveIndex = NPART - 1;
+	parts.active = NPART;
 	RecalcFreeParticles(false);
 }
 
@@ -92,7 +92,7 @@ void Simulation::clear_area(int area_x, int area_y, int area_w, int area_h)
 	area_w = intersection.size.X;
 	area_h = intersection.size.Y;
 	float fx = area_x-.5f, fy = area_y-.5f;
-	for (int i = 0; i <= parts.lastActiveIndex; i++)
+	for (int i = 0; i < parts.active; i++)
 	{
 		if (parts[i].type)
 			if (parts[i].x >= fx && parts[i].x <= fx+area_w+1 && parts[i].y >= fy && parts[i].y <= fy+area_h+1)

src/simulation/Simulation.cpp
@@ -40,7 +40,7 @@ void Simulation::Load(const GameSave *save, bool includePressure, Vec2<int> bloc
 	};
 	std::vector<ExistingParticle> existingParticles;
 	auto pasteArea = RES.OriginRect() & RectSized(partP, save->blockSize * CELL);
-	for (int i = 0; i <= parts.lastActiveIndex; i++)
+	for (int i = 0; i < parts.active; i++)
 	{
 		if (parts[i].type)
 		{
@@ -138,10 +138,6 @@ void Simulation::Load(const GameSave *save, bool includePressure, Vec2<int> bloc
 		{
 			continue;
 		}
-		if (i > parts.lastActiveIndex)
-		{
-			parts.lastActiveIndex = i;
-		}
 		parts[i] = tempPart;
 
 
@@ -226,7 +222,7 @@ void Simulation::Load(const GameSave *save, bool includePressure, Vec2<int> bloc
 			parts[i].tmp3 = 0;
 		}
 	}
-	parts.lastActiveIndex = NPART-1;
+	parts.active = NPART;
 	force_stacking_check = true;
 	Element_PPIP_ppip_changed = 1;
 
@@ -986,12 +982,12 @@ int Simulation::parts_avg(int ci, int ni,int t)
 void Parts::Reset()
 {
 	memset(data.data(), 0, sizeof(Particle)*NPART);
-	lastActiveIndex = 0;
+	active = 0;
 }
 
 void Simulation::clear_sim(void)
 {
-	for (auto i = 0; i <= parts.lastActiveIndex; i++)
+	for (auto i = 0; i < parts.active; i++)
 	{
 		if (parts[i].type)
 		{
@@ -1008,10 +1004,7 @@ void Simulation::clear_sim(void)
 	memset(bmap, 0, sizeof(bmap));
 	memset(emap, 0, sizeof(emap));
 	parts.Reset();
-	for (int i = 0; i < NPART-1; i++)
-		parts[i].life = i+1;
-	parts[NPART-1].life = -1;
-	pfree = 0;
+	pfree = -1;
 	NUM_PARTS = 0;
 	memset(pmap, 0, sizeof(pmap));
 	memset(fvx, 0, sizeof(fvx));
@@ -1889,10 +1882,20 @@ int Simulation::create_part(int p, int x, int y, int t, int v)
 				return -1;
 			}
 		}
-		if (pfree == -1)
+		if (pfree != -1)
+		{
+			i = pfree;
+			pfree = parts[i].life;
+		}
+		else if (parts.active < NPART)
+		{
+			i = parts.active;
+			parts.active += 1;
+		}
+		else
+		{
 			return -1;
-		i = pfree;
-		pfree = parts[i].life;
+		}
 		NUM_PARTS += 1;
 	}
 	else
@@ -1913,8 +1916,6 @@ int Simulation::create_part(int p, int x, int y, int t, int v)
 
 		i = p;
 	}
-
-	if (i>parts.lastActiveIndex) parts.lastActiveIndex = i;
 
 	parts[i] = elements[t].DefaultProperties;
 	parts[i].type = t;
@@ -2214,7 +2215,7 @@ void Simulation::UpdateParticles(int start, int end)
 	//the main particle loop function, goes over all particles.
 	auto &sd = SimulationData::CRef();
 	auto &elements = sd.elements;
-	for (auto i = start; i < end && i <= parts.lastActiveIndex; i++)
+	for (auto i = start; i < end && i < parts.active; i++)
 	{
 		if (parts[i].type)
 		{
@@ -3249,10 +3250,6 @@ movedone:
 
 void Simulation::RecalcFreeParticles(bool do_life_dec)
 {
-	int x, y, t;
-	int lastPartUsed = 0;
-	int lastPartUnused = -1;
-
 	memset(pmap, 0, sizeof(pmap));
 	memset(pmap_count, 0, sizeof(pmap_count));
 	memset(photons, 0, sizeof(photons));
@@ -3261,81 +3258,82 @@ void Simulation::RecalcFreeParticles(bool do_life_dec)
 	auto &sd = SimulationData::CRef();
 	auto &elements = sd.elements;
 	//the particle loop that resets the pmap/photon maps every frame, to update them.
-	for (int i = 0; i <= parts.lastActiveIndex; i++)
+	for (int i = 0; i < parts.active; i++)
+	{
+		if (!parts[i].type)
+		{
+			continue;
+		}
+		auto t = parts[i].type;
+		auto x = int(parts[i].x+0.5f);
+		auto y = int(parts[i].y+0.5f);
+		bool inBounds = false;
+		if (x>=0 && y>=0 && x<XRES && y<YRES)
+		{
+			if (elements[t].Properties & TYPE_ENERGY)
+				photons[y][x] = PMAP(i, t);
+			else
+			{
+				// Particles are sometimes allowed to go inside INVS and FILT
+				// To make particles collide correctly when inside these elements, these elements must not overwrite an existing pmap entry from particles inside them
+				if (!pmap[y][x] || (t!=PT_INVIS && t!= PT_FILT))
+					pmap[y][x] = PMAP(i, t);
+				// (there are a few exceptions, including energy particles - currently no limit on stacking those)
+				if (t!=PT_THDR && t!=PT_EMBR && t!=PT_FIGH && t!=PT_PLSM)
+					pmap_count[y][x]++;
+			}
+			inBounds = true;
+		}
+		NUM_PARTS ++;
+
+		if (elementRecount && t >= 0 && t < PT_NUM && elements[t].Enabled)
+			elementCount[t]++;
+
+		//decrease particle life
+		if (do_life_dec && (!sys_pause || framerender))
+		{
+			if (t<0 || t>=PT_NUM || !elements[t].Enabled)
+			{
+				kill_part(i);
+				continue;
+			}
+
+			unsigned int elem_properties = elements[t].Properties;
+			if (parts[i].life>0 && (elem_properties&PROP_LIFE_DEC) && !(inBounds && bmap[y/CELL][x/CELL] == WL_STASIS && emap[y/CELL][x/CELL]<8))
+			{
+				// automatically decrease life
+				parts[i].life--;
+				if (parts[i].life<=0 && (elem_properties&(PROP_LIFE_KILL_DEC|PROP_LIFE_KILL)))
+				{
+					// kill on change to no life
+					kill_part(i);
+					continue;
+				}
+			}
+			else if (parts[i].life<=0 && (elem_properties&PROP_LIFE_KILL) && !(inBounds && bmap[y/CELL][x/CELL] == WL_STASIS && emap[y/CELL][x/CELL]<8))
+			{
+				// kill if no life
+				kill_part(i);
+				continue;
+			}
+		}
+	}
+	int newActive = 0;
+	auto *ppfree = &pfree;
+	for (int i = 0; i < parts.active; i++)
 	{
 		if (parts[i].type)
 		{
-			t = parts[i].type;
-			x = (int)(parts[i].x+0.5f);
-			y = (int)(parts[i].y+0.5f);
-			bool inBounds = false;
-			if (x>=0 && y>=0 && x<XRES && y<YRES)
+			for (auto j = newActive; j < i; ++j)
 			{
-				if (elements[t].Properties & TYPE_ENERGY)
-					photons[y][x] = PMAP(i, t);
-				else
-				{
-					// Particles are sometimes allowed to go inside INVS and FILT
-					// To make particles collide correctly when inside these elements, these elements must not overwrite an existing pmap entry from particles inside them
-					if (!pmap[y][x] || (t!=PT_INVIS && t!= PT_FILT))
-						pmap[y][x] = PMAP(i, t);
-					// (there are a few exceptions, including energy particles - currently no limit on stacking those)
-					if (t!=PT_THDR && t!=PT_EMBR && t!=PT_FIGH && t!=PT_PLSM)
-						pmap_count[y][x]++;
-				}
-				inBounds = true;
+				*ppfree = j;
+				ppfree = &parts[j].life;
 			}
-			lastPartUsed = i;
-			NUM_PARTS ++;
-
-			if (elementRecount && t >= 0 && t < PT_NUM && elements[t].Enabled)
-				elementCount[t]++;
-
-			//decrease particle life
-			if (do_life_dec && (!sys_pause || framerender))
-			{
-				if (t<0 || t>=PT_NUM || !elements[t].Enabled)
-				{
-					kill_part(i);
-					continue;
-				}
-
-				unsigned int elem_properties = elements[t].Properties;
-				if (parts[i].life>0 && (elem_properties&PROP_LIFE_DEC) && !(inBounds && bmap[y/CELL][x/CELL] == WL_STASIS && emap[y/CELL][x/CELL]<8))
-				{
-					// automatically decrease life
-					parts[i].life--;
-					if (parts[i].life<=0 && (elem_properties&(PROP_LIFE_KILL_DEC|PROP_LIFE_KILL)))
-					{
-						// kill on change to no life
-						kill_part(i);
-						continue;
-					}
-				}
-				else if (parts[i].life<=0 && (elem_properties&PROP_LIFE_KILL) && !(inBounds && bmap[y/CELL][x/CELL] == WL_STASIS && emap[y/CELL][x/CELL]<8))
-				{
-					// kill if no life
-					kill_part(i);
-					continue;
-				}
-			}
-		}
-		else
-		{
-			if (lastPartUnused<0) pfree = i;
-			else parts[lastPartUnused].life = i;
-			lastPartUnused = i;
+			newActive = i + 1;
 		}
 	}
-	if (lastPartUnused == -1)
-	{
-		pfree = (parts.lastActiveIndex>=(NPART-1)) ? -1 : parts.lastActiveIndex+1;
-	}
-	else
-	{
-		parts[lastPartUnused].life = (parts.lastActiveIndex>=(NPART-1)) ? -1 : parts.lastActiveIndex+1;
-	}
-	parts.lastActiveIndex = lastPartUsed;
+	*ppfree = -1;
+	parts.active = newActive;
 	if (elementRecount)
 		elementRecount = false;
 }
@@ -3344,7 +3342,7 @@ void Simulation::SimulateGoL()
 {
 	auto &builtinGol = SimulationData::builtinGol;
 	CGOL = 0;
-	for (int i = 0; i <= parts.lastActiveIndex; ++i)
+	for (int i = 0; i < parts.active; ++i)
 	{
 		auto &part = parts[i];
 		if (part.type != PT_LIFE)
@@ -3550,7 +3548,7 @@ void Simulation::CheckStacking()
 	}
 	if (excessive_stacking_found)
 	{
-		for (int i = 0; i <= parts.lastActiveIndex; i++)
+		for (int i = 0; i < parts.active; i++)
 		{
 			if (parts[i].type)
 			{
@@ -3774,7 +3772,7 @@ void Simulation::BeforeSim()
 		// update PPIP tmp?
 		if (Element_PPIP_ppip_changed)
 		{
-			for (int i = 0; i <= parts.lastActiveIndex; i++)
+			for (int i = 0; i < parts.active; i++)
 			{
 				if (parts[i].type==PT_PPIP)
 				{

src/simulation/elements/EMP.cpp
@@ -106,7 +106,7 @@ void Element_EMP_Trigger(Simulation *sim, int triggerCount)
 
 	float prob_randDLAY = Probability::binomial_gte1(triggerCount, 1.0f/70);
 
-	for (int r = 0; r <=sim->parts.lastActiveIndex; r++)
+	for (int r = 0; r < sim->parts.active; r++)
 	{
 		int t = parts[r].type;
 		auto rx = int(parts[r].x);

commit ae7a825e7f20e5f91d54b80b0154fe04bb8beab7

Fix cherenkov photon / gain photon reactions not working in many cases, broken since 259c4f00c378
These reactions checked Parts::MaxPartsReached and ended early. MaxPartsReached was broken, and would always return false if pfree was -1, even though there was still plenty of room after Parts::active (formerly lastActiveIndex) for spawning particles

src/simulation/Simulation.h
@@ -79,7 +79,7 @@ public:
 
 	bool MaxPartsReached() const
 	{
-		return pfree == -1;
+		return pfree == -1 && active >= NPART;
 	}
 };
 