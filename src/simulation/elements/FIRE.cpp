6600ad53ef06a68e05f5d2a3be282c5a7eda29e7
jacob1 <jfu614@gmail.com>
2026-06-20 13:44:33 -0400

    Revert "Remove Prop_Life_Kill from plsm and fire and modify existing life<=1 logic to also handle this. (#1008)"
    This reverts commit dc445bcde890fffee9c5ef63a77bc39a9716ad62.

--- src/simulation/elements/FIRE.cpp
+++ src/simulation/elements/FIRE.cpp
@@ -36,7 +36,7 @@ void Element::Element_FIRE()
 	HeatConduct = 88;
 	Description = "Ignites flammable materials. Heats air.";
 
-	Properties = TYPE_GAS|PROP_LIFE_DEC;
+	Properties = TYPE_GAS|PROP_LIFE_DEC|PROP_LIFE_KILL;
 	CarriesTypeIn = 1U << FIELD_CTYPE;
 
 	LowPressure = IPL;
@@ -76,11 +76,6 @@ int Element_FIRE_update(UPDATE_FUNC_ARGS)
 				parts[i].life = 0;
 				parts[i].ctype = PT_FIRE;
 			}
-			else
-			{
-				sim->kill_part(i);
-				return 1;
-			}
 		}
 		break;
 	case PT_FIRE:
@@ -98,11 +93,6 @@ int Element_FIRE_update(UPDATE_FUNC_ARGS)
 				//@ FIRE -> SMKE
 				sim->part_change_type(i,x,y,PT_SMKE);
 				parts[i].life = sim->rng.between(250, 269);
-			}
-			else
-			{
-				sim->kill_part(i);
-				return 1;
 			}
 		}
 		break;
--- src/simulation/elements/PLSM.cpp
+++ src/simulation/elements/PLSM.cpp
@@ -34,7 +34,7 @@ void Element::Element_PLSM()
 	HeatConduct = 5;
 	Description = "Plasma, extremely hot.";
 
-	Properties = TYPE_GAS|PROP_LIFE_DEC;
+	Properties = TYPE_GAS|PROP_LIFE_DEC|PROP_LIFE_KILL;
 	CarriesTypeIn = 1U << FIELD_CTYPE;
 
 	LowPressure = IPL;
